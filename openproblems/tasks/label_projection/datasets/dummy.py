59e7338ab43c3736a94d14a5e8a3e1a4224db84c
Scott Gigante <scottgigante@gmail.com>
2020-06-19 14:16:44 -0400

    fix dataset generation

--- openproblems/tasks/label_projection/datasets/dummy.py
+++ openproblems/tasks/label_projection/datasets/dummy.py
@@ -5,4 +5,5 @@ import anndata
 def dummy():
     adata = anndata.AnnData(np.random.uniform(0, 1, (100, 10)))
     adata.obs["labels"] = np.random.choice(2, 100, replace=True)
+    adata.obs["is_train"] = np.random.choice([True, False], 100, replace=True)
     return adata
