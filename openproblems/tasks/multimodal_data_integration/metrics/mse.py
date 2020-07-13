a1b59455eafd054fefaf4b4dd5b818b7c68df7ed
Scott Gigante <scottgigante@gmail.com>
2020-07-13 11:32:48 -0400

    fix norm

--- openproblems/tasks/multimodal_data_integration/metrics/mse.py
+++ openproblems/tasks/multimodal_data_integration/metrics/mse.py
@@ -8,10 +8,7 @@ def mse(adata):
     # mean and norm
     Z = np.vstack([X, Y])
     Z -= np.mean(Z, axis=0)
-    if sparse.issparse(Z):
-        Z /= sparse.linalg.norm(Z)
-    else:
-        Z /= np.linalg.norm(Z)
+    Z /= np.sqrt(np.sum(Z ** 2))
     # split back out
     X, Y = Z[: X.shape[0]], Z[X.shape[0] :]
     return np.mean(np.sum((X - Y) ** 2))
