a575725345ea60accaf2806cc0802b08f7ee254a
Scott Gigante <scottgigante@gmail.com>
2020-07-13 15:36:59 -0400

    fix caching

--- openproblems/data/utils.py
+++ openproblems/data/utils.py
@@ -5,12 +5,14 @@ from . import TEMPDIR
 
 
 @decorator
-def loader(func, *args, test=False, **kwargs):
-    filename = "openproblems_{}_test-{}.h5ad".format(hash(func), test)
+def loader(func, *args, **kwargs):
+    filename = "openproblems_{}-{}-{}.h5ad".format(
+        hash(func), hash(str(args)), hash(str(kwargs))
+    )
     filepath = os.path.join(TEMPDIR.name, filename)
     if os.path.isfile(filepath):
         return anndata.read_h5ad(filepath)
     else:
-        adata = func(test=test)
+        adata = func(*args, **kwargs)
         adata.write(filepath)
         return adata
