542faad1316fa15315664f377372c5d127f77090
Scott Gigante <scottgigante@gmail.com>
2020-07-13 18:35:01 -0400

    more specific git fetch

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,11 +1,13 @@
+set -x
+pip install -U ..[evaluate]
+python evaluate.py
 if [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
-    set -x
-    pip install -U ..[evaluate]
-    python evaluate.py
+    git remote set-url origin https://scottgigante:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git
+    git fetch --depth=1 origin master
     git checkout master
-    git set-url origin https://scottgigante:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git
-    git pull origin master
     git add ../results.md
+    git config user.name "Travis CI"
+    git config user.email "scottgigante@gmail.com"
     git commit -m "Travis CI: update results for ${TRAVIS_TAG}"
     git push origin master
 fi
