bac91671f4b094fa3b06b48f54e4154d5d0666b7
Scott Gigante <scottgigante@gmail.com>
2020-07-13 16:39:14 -0400

    fix git upload

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,6 +1,11 @@
 if [ "$TRAVIS_PYTHON_VERSION" = "3.6" ]; then
+    set -x
+    pip install -U ..[evaluate]
     python evaluate.py
+    git checkout master
+    git set-url origin https://scottgigante:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git
+    git pull origin master
     git add ../results.md
-    git commit -m "update results for ${TRAVIS_TAG}"
-    git push https://scottgigante:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git master
+    git commit -m "Travis CI: update results for ${TRAVIS_TAG}"
+    git push origin master
 fi
