fa96c5c666687baf7348fd636eff192375f2793c
Scott Gigante <scottgigante@gmail.com>
2020-07-13 10:58:18 -0400

    add auto deploy

---
+++ scripts/evaluate.sh
@@ -0,0 +1,6 @@
+if [ "$TRAVIS_PYTHON_VERSION" == "3.6" ]; then
+    python evaluate.py
+    git add ../results.md
+    git commit -m "update results for ${TRAVIS_TAG}"
+    git push https://scottgigante:${GITHUB_PASSWORD}@github.com/${TRAVIS_REPO_SLUG}.git master
+fi
