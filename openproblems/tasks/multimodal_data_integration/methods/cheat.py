157a48ab5253bcce8775b627c93e32174f1756ac
Scott Gigante <scottgigante@gmail.com>
2020-07-13 15:05:18 -0400

    make cheat memory efficient

--- openproblems/tasks/multimodal_data_integration/methods/cheat.py
+++ openproblems/tasks/multimodal_data_integration/methods/cheat.py
@@ -2,5 +2,5 @@ from sklearn.decomposition import TruncatedSVD
 
 
 def cheat(adata):
-    adata.obsm["aligned"] = adata.X
-    adata.obsm["mode2_aligned"] = adata.X
+    adata.obsm["aligned"] = adata.X[:, :200]
+    adata.obsm["mode2_aligned"] = adata.X[:, :200]
