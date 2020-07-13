85f3eeb63ec4ce48a66b89ea4c87c564866629bb
Scott Gigante <scottgigante@gmail.com>
2020-07-13 10:45:13 -0400

    add cheat

--- openproblems/tasks/multimodal_data_integration/methods/cheat.py
+++ openproblems/tasks/multimodal_data_integration/methods/cheat.py
@@ -1,7 +1,6 @@
 from sklearn.decomposition import TruncatedSVD
 
 
-def cheat(adata, n_svd=150):
-    X_pca = TruncatedSVD(n_svd).fit_transform(adata.X)
-    adata.obsm["aligned"] = X_pca
-    adata.uns["mode2"].obsm["aligned"] = X_pca
+def cheat(adata):
+    adata.obsm["aligned"] = adata.X
+    adata.uns["mode2"].obsm["aligned"] = adata.X
