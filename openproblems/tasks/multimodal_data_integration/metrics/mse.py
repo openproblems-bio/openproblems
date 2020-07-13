74fa526b8c182537b2863663f9c176fa09c10766
Scott Gigante <scottgigante@gmail.com>
2020-07-13 15:07:36 -0400

    just do dense

--- openproblems/tasks/multimodal_data_integration/metrics/mse.py
+++ openproblems/tasks/multimodal_data_integration/metrics/mse.py
@@ -12,13 +12,14 @@ def _square(X):
 
 
 def mse(adata):
-    X = adata.obsm["aligned"]
-    Y = adata.obsm["mode2_aligned"]
+    X = scprep.utils.toarray(data.obsm["aligned"])
+    Y = scprep.utils.toarray(data.obsm["mode2_aligned"])
+
     # mean and norm
-    vstack = sparse.vstack if sparse.issparse(X) and sparse.issparse(Y) else np.vstack
-    Z = vstack([X, Y])
+    Z = np.vstack([X, Y])
     Z -= np.mean(Z, axis=0)
     Z /= np.sqrt(np.sum(_square(Z)))
+
     # split back out
     X, Y = Z[: X.shape[0]], Z[X.shape[0] :]
     return np.mean(np.sum(_square(X - Y)))
