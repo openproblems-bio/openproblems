159a5be33792670ae5a6aa4a3da546a3567bdf94
Scott Gigante <scottgigante@gmail.com>
2020-07-14 12:40:24 -0400

    fix cheat method

--- openproblems/tasks/multimodal_data_integration/methods/cheat.py
+++ openproblems/tasks/multimodal_data_integration/methods/cheat.py
@@ -1,6 +1,9 @@
 from sklearn.decomposition import TruncatedSVD
 
 
-def cheat(adata):
-    adata.obsm["aligned"] = adata.X[:, :200]
-    adata.obsm["mode2_aligned"] = adata.X[:, :200]
+def cheat(adata, n_svd=300):
+    if min(adata.X.shape) <= n_svd:
+        n_svd = min(adata.X.shape) - 1
+    X_pca = TruncatedSVD(n_svd).fit_transform(adata.X)
+    adata.obsm["aligned"] = X_pca
+    adata.obsm["mode2_aligned"] = X_pca
