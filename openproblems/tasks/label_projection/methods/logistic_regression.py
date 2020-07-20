6b68d52116fc292b7f716a6bed113befe126978c
Your Name <malte.luecken@helmholtz-muenchen.de>
2020-07-20 15:11:15 +0200

    updated logistic regression function to work with anndata properly

--- openproblems/tasks/label_projection/methods/logistic_regression.py
+++ openproblems/tasks/label_projection/methods/logistic_regression.py
@@ -1,11 +1,13 @@
-import sklearn
-
+from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 def logistic_regression(adata):
-    classifier = sklearn.linear_model.LogisticRegression()
+    classifier = LogisticRegression()
 
     adata_train = adata[adata.obs["is_train"]]
-    adata_test = adata[~adata.obs["is_train"]]
+    adata_test = adata[~adata.obs["is_train"]].copy()
 
     classifier.fit(adata_train.X, adata_train.obs["labels"])
-    adata_test["labels_pred"] = classifier.predict(adata_test.X)
+    adata_test.obs["labels_pred"] = classifier.predict(adata_test.X)
+
+    adata.obs['labels_pred'] = [adata_test.obs['labels_pred'][idx] if idx in adata_test.obs_names else np.nan for idx in adata.obs_names]
