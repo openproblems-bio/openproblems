d1c9d77f188d95848ac1fff3b718b8e9da3d097a
Your Name <malte.luecken@helmholtz-muenchen.de>
2020-07-27 17:01:34 +0200

    added normalization functions

---
+++ openproblems/data/normalize.py
@@ -0,0 +1,23 @@
+import scanpy as sc
+from scIB.preprocessing import normalize
+
+
+def scran_pooling(adata):
+    """
+    This function scran-normalizes the data 
+    """
+
+    # Normalize via scran-pooling with own clustering at res=0.5
+    normalize(adata)
+    
+    # Make lightweight
+    del adata.raw
+    
+
+def cpm(adata):
+    adata.layers['counts'] = adata.X.copy()
+    sc.pp.normalize_total(adata, target_sum=1e6, key_added='size_factors')
+
+def log_cpm(adata):
+    cpm(adata)
+    sc.pp.log1p(adata)
