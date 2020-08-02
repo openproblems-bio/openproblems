d457d89a5a79b5e6cb9c6439cae2d1f9a257beb6
m0hammadl <mohammad.lotfollahi@helmholtz-muenchen.de>
2020-08-02 23:47:58 +0200

    add senis-muris loader

---
+++ openproblems/data/senis_muris.py
@@ -0,0 +1,10 @@
+import scanpy as sc
+from .utils import loader
+
+url = "https://ndownloader.figshare.com/files/24130931"
+@loader
+def load_senis_muris(test=False):
+    adata = sc.read(url=url)
+    if test:
+        sc.pp.subsample(adata,fraction=0.1)
+    return adata
