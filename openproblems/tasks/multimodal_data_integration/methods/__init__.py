5b7ed161447f002ec7042b690e9fa50d05884863
Scott Gigante <scottgigante@gmail.com>
2020-08-03 14:22:01 -0400

    Bugfix harmonic alignment import

--- openproblems/tasks/multimodal_data_integration/methods/__init__.py
+++ openproblems/tasks/multimodal_data_integration/methods/__init__.py
@@ -2,6 +2,6 @@ from .mnn import mnn_log_cpm, mnn_log_scran_pooling
 from .procrustes import procrustes
 from .cheat import cheat
 from .harmonic_alignment import (
-    harmonic_alignment_log_cpm,
+    harmonic_alignment_sqrt_cpm,
     harmonic_alignment_log_scran_pooling,
 )
