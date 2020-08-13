d6925796a07d42520ec09491430990a051903df1
Scott Gigante <scottgigante@gmail.com>
2020-08-13 09:55:00 -0400

    print results for website

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -40,14 +40,17 @@ def evaluate_task(task):
 def main():
     openproblems.test.utils.ignore_numba_warnings()
 
-    result = []
+    results = []
     for task in openproblems.TASKS:
-        result.append(evaluate_task(task))
+        result = evaluate_task(task).sort_values(["dataset", "metric", "value"])
+        with open("../website/content/results/{}.md".format(task), "w") as handle:
+            result.to_markdown(handle)
+        results.append(result)
 
-    result = pd.concat(result).sort_values(["task", "dataset", "metric", "value"])
+    results = pd.concat(results).sort_values(["task", "dataset", "metric", "value"])
     with open("../results.md", "w") as handle:
-        result.to_markdown(handle)
-    return result
+        results.to_markdown(handle)
+    return results
 
 
 if __name__ == "__main__":
