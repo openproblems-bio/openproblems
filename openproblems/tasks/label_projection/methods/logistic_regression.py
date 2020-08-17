12c580f081571e6189fc90ca55a715be50fdf416
Daniel Burkhardt <dan@vpn17225132246.its.yale.internal>
2020-08-17 16:42:58 -0400

    black

--- openproblems/tasks/label_projection/methods/logistic_regression.py
+++ openproblems/tasks/label_projection/methods/logistic_regression.py
@@ -12,19 +12,20 @@ def _logistic_regression(adata):
     adata_test = adata[~adata.obs["is_train"]].copy()
 
     # Dimensionality reduction & scaling
-    pc_op = PCA(n_components=100)
-    adata_train.obsm['pca'] = pc_op.fit_transform(adata_train.X.toarray())
-    adata_train.obsm['pca_scale'] = sklearn.preprocessing.scale(adata_train.obsm['pca'])
+    n_pca = np.min([adata.shape[0], adata.shape[1], 100])
+    pc_op = PCA(n_components=n_pca)
+    adata_train.obsm["pca"] = pc_op.fit_transform(adata_train.X.toarray())
+    adata_train.obsm["pca_scale"] = sklearn.preprocessing.scale(adata_train.obsm["pca"])
 
     # Fit to train data
-    classifier.fit(adata_train.obsm['pca_scale'], adata_train.obs["labels"])
+    classifier.fit(adata_train.obsm["pca_scale"], adata_train.obs["labels"])
 
     # Reduce and scale test data
-    adata_test.obsm['pca'] = adata_test.X @ pc_op.components_.T
-    adata_test.obsm['pca_scale'] = sklearn.preprocessing.scale(adata_test.obsm['pca'])
+    adata_test.obsm["pca"] = adata_test.X @ pc_op.components_.T
+    adata_test.obsm["pca_scale"] = sklearn.preprocessing.scale(adata_test.obsm["pca"])
 
     # Fit test data
-    adata_test.obs["labels_pred"] = classifier.predict(adata_test.obsm['pca_scale'])
+    adata_test.obs["labels_pred"] = classifier.predict(adata_test.obsm["pca_scale"])
 
     adata.obs["labels_pred"] = [
         adata_test.obs["labels_pred"][idx] if idx in adata_test.obs_names else np.nan
