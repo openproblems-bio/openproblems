7c27bb0039d834ceee02fec51c306f5f43dc001c
Scott Gigante <scottgigante@gmail.com>
2020-08-17 13:46:53 -0400

    add zebrafish back in
    Former-commit-id: 2c9ea34d19f7bb4cc7dcf347e92207d8182e26f0

---
+++ openproblems/tasks/label_projection/datasets/zebrafish.py
@@ -0,0 +1,18 @@
+import numpy as np
+from ....data.zebrafish import load_zebrafish
+
+
+def zebrafish_labels(test=False):
+    adata = load_zebrafish(test=test)
+    adata.obs["labels"] = adata.obs["cell_type"]
+    adata.obs["is_train"] = adata.obs["labels"] == "Schier"
+    return adata
+
+
+def zebrafish_random(test=False):
+    adata = load_zebrafish(test=test)
+    adata.obs["labels"] = adata.obs["cell_type"]
+    adata.obs["is_train"] = adata.obs["is_train"] = np.random.choice(
+        [True, False], adata.shape[0], replace=True
+    )
+    return adata
