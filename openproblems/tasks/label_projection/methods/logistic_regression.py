8c34478f947f5ddeadd22aa5b96d46ca3c8434ad
Scott Gigante <scottgigante@gmail.com>
2020-08-18 14:57:12 -0400

    scale without mean if sparse
    Former-commit-id: 57d60cdde10f73d734be224523652677e544e976

--- openproblems/tasks/label_projection/methods/logistic_regression.py
+++ openproblems/tasks/label_projection/methods/logistic_regression.py
@@ -1,13 +1,19 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+
 import numpy as np
+from scipy import sparse
+
 from ....tools.normalize import log_cpm, log_scran_pooling
 
 
 def _logistic_regression(adata):
     classifier = Pipeline(
-        [("scaler", StandardScaler()), ("regression", LogisticRegression())]
+        [
+            ("scaler", StandardScaler(with_mean=not sparse.issparse(adata.X))),
+            ("regression", LogisticRegression()),
+        ]
     )
 
     adata_train = adata[adata.obs["is_train"]]
