3385e7c59d67aedd70b168cc0796125d2ff4c746
Scott Gigante <scottgigante@gmail.com>
2020-08-31 12:22:35 -0400

    updated black
    Former-commit-id: 801d878ab9c81389e5f875732b4e1c0ebf11592d

--- website/themes/academic/scripts/list_language_packs.py
+++ website/themes/academic/scripts/list_language_packs.py
@@ -9,23 +9,23 @@
 import yaml
 from pathlib import Path
 
-LANG_PATH = Path(__file__).resolve().parent.parent.joinpath('data').joinpath('i18n')
-LANG_YAML = LANG_PATH.joinpath('languages.yaml')
+LANG_PATH = Path(__file__).resolve().parent.parent.joinpath("data").joinpath("i18n")
+LANG_YAML = LANG_PATH.joinpath("languages.yaml")
 
 
 # Iterate over languages.
 with open(LANG_YAML) as f:
-  master_map = yaml.safe_load(f)
+    master_map = yaml.safe_load(f)
 
-  # Print languages as a plaintext list.
-  # lang_list = [master_map[master_item] for master_item in master_map]
-  # print(', '.join(lang_list))
+    # Print languages as a plaintext list.
+    # lang_list = [master_map[master_item] for master_item in master_map]
+    # print(', '.join(lang_list))
 
-  # Print languages as a Markdown list.
-  i = 0
-  for master_item in master_map:
-    print(f'- **{master_map[master_item]}** ({master_item})')
-    i += 1
+    # Print languages as a Markdown list.
+    i = 0
+    for master_item in master_map:
+        print(f"- **{master_map[master_item]}** ({master_item})")
+        i += 1
 
 print("\n")
 print(f"{i} language packs found!")
