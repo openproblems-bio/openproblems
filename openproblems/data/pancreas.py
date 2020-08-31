632bb5314e4160ceba9ec2ba3c4eb658d5f8aa13
Scott Gigante <scottgigante@gmail.com>
2020-08-31 11:37:53 -0400

    black

--- openproblems/data/pancreas.py
+++ openproblems/data/pancreas.py
@@ -8,9 +8,7 @@ import scanpy as sc
 from .utils import loader
 from ..tools import normalize as n
 
-URL = (
-    "https://ndownloader.figshare.com/files/22891151"
-)
+URL = "https://ndownloader.figshare.com/files/22891151"
 
 
 @loader
@@ -20,10 +18,11 @@ def load_pancreas(test=False, preprocess=True):
         adata = load_pancreas(test=False)
 
         # Subsample pancreas data
-        cts = ['delta', 'gamma']
-        batches = ['inDrop2', 'smarter', 'celseq']
-        adata = adata[(adata.obs['batch'].isin(batches)) &
-                      (adata.obs['labels'].isin(cts)), :200].copy()
+        cts = ["delta", "gamma"]
+        batches = ["inDrop2", "smarter", "celseq"]
+        adata = adata[
+            (adata.obs["batch"].isin(batches)) & (adata.obs["labels"].isin(cts)), :200
+        ].copy()
         # Note: could also use 200-500 HVGs rather than 200 random genes
 
         return adata
@@ -36,19 +35,18 @@ def load_pancreas(test=False, preprocess=True):
 
             # Correct covariate naming and remove preprocessing
             prep_pancreas(adata)
-                
-        return adata
 
+        return adata
 
 
 def prep_pancreas(adata):
     # Rename categories
-    adata.obs['batch'] = adata.obs['tech'].copy()
-    adata.obs['labels'] = adata.obs['celltype'].copy()
-    
+    adata.obs["batch"] = adata.obs["tech"].copy()
+    adata.obs["labels"] = adata.obs["celltype"].copy()
+
     # Drop excess covariates
-    adata.obs = adata.obs.drop(columns=['tech', 'size_factors', 'celltype'])
+    adata.obs = adata.obs.drop(columns=["tech", "size_factors", "celltype"])
 
     # Remove processing
-    adata.X = adata.layers['counts']
-    del adata.layers['counts']
+    adata.X = adata.layers["counts"]
+    del adata.layers["counts"]
