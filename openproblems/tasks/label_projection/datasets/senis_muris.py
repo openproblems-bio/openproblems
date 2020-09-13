2398faaa0a3d886a6144de88f27c1c06273ce773
m0hammadl <mohammad.lotfollahi@helmholtz-muenchen.de>
2020-09-13 13:27:21 +0200

    add senis-muris task loaders

---
+++ openproblems/tasks/label_projection/datasets/senis_muris.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ....data.senis_muris import load_senis_muris
+
+
+def senis_muris_timepoint(test=False):
+    adata = load_senis_muris(test=test)
+    adata.obs["is_train"] = adata.obs["age"] != "3m"
+    return adata
+
+
+def senis_muris_random(test=False):
+    adata = load_senis_muris(test=test)
+    adata.obs["is_train"] = np.random.choice(
+        [True, False], adata.shape[0], replace=True
+    )
+    return adata
