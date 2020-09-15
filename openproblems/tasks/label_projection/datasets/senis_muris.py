7ecd90bc5f24a2799b77f67ec37153310f356e55
Scott Gigante <scottgigante@gmail.com>
2020-09-15 10:27:39 -0400

    label dataset

--- openproblems/tasks/label_projection/datasets/senis_muris.py
+++ openproblems/tasks/label_projection/datasets/senis_muris.py
@@ -1,13 +1,16 @@
 import numpy as np
 from ....data.senis_muris import load_senis_muris
+from ....tools.decorators import dataset
 
 
+@dataset("Tabula Senis (by timepoint)")
 def senis_muris_timepoint(test=False):
     adata = load_senis_muris(test=test)
     adata.obs["is_train"] = adata.obs["age"] != "3m"
     return adata
 
 
+@dataset("Tabula Senis (random split)")
 def senis_muris_random(test=False):
     adata = load_senis_muris(test=test)
     adata.obs["is_train"] = np.random.choice(
