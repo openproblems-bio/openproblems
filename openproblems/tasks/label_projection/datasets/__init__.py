dccbc2432afb9a90e21ebe7ec6bba159dacf38a2
Scott Gigante <scottgigante@users.noreply.github.com>
2020-09-15 08:48:46 -0400

    Fix typo

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -1,3 +1,3 @@
 from .zebrafish import zebrafish_labels, zebrafish_random
 from .pancreas import pancreas_batch, pancreas_random
-from .senis_muris import senis_muris_timepoint, senis_muris_batch
+from .senis_muris import senis_muris_timepoint, senis_muris_random
