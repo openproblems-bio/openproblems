70fa9cb2e6a1570474b429bcf0b3d2767db84e1d
Scott Gigante <scottgigante@gmail.com>
2020-09-21 15:31:35 -0400

    use fastmnn instead of mnnpy

--- openproblems/tasks/multimodal_data_integration/methods/mnn.py
+++ openproblems/tasks/multimodal_data_integration/methods/mnn.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scprep
+
 from sklearn.decomposition import TruncatedSVD
 from ....tools.normalize import log_cpm, log_scran_pooling
 from ....tools.decorators import method
@@ -6,20 +8,31 @@ from ....tools.utils import check_version
 
 
 def _mnn(adata, n_svd=100):
-    import mnnpy
+    import rpy2.robjects as ro
+    from rpy2.robjects import numpy2ri
+
+    numpy2ri.activate()
+    scprep.run.install_bioconductor("batchelor")
 
     if min(adata.X.shape) <= n_svd:
         n_svd = min(adata.X.shape) - 1
     if min(adata.obsm["mode2"].shape) <= n_svd:
         n_svd = min(adata.obsm["mode2"].shape) - 1
-    # TODO: also normalize obsm["mode2"]
     X_pca = TruncatedSVD(n_svd).fit_transform(adata.X)
     Y_pca = TruncatedSVD(n_svd).fit_transform(adata.obsm["mode2"])
-    (X_corrected, Y_corrected), _, _ = mnnpy.mnn_correct(
-        X_pca, Y_pca, var_index=np.arange(n_svd), do_concatenate=False
-    )
-    adata.obsm["aligned"] = X_corrected
-    adata.obsm["mode2_aligned"] = Y_corrected
+
+    ro.globalenv["expr"] = np.vstack([X_pca, Y_pca]).T
+    ro.globalenv["batch"] = np.concatenate(
+        [np.full(X_pca.shape[0], 1), np.full(Y_pca.shape[0], 2)]
+    ).tolist()
+
+    ro.r("batch <- as.integer(batch)")
+    ro.r("sce <- fastMNN(expr, batch = batch)")
+    ro.r("out <- assay(sce, 'reconstructed')")
+
+    XY_corrected = numpy2ri.rpy2py(ro.r("as.matrix(out)")).T
+    adata.obsm["aligned"] = XY_corrected[: X_pca.shape[0]]
+    adata.obsm["mode2_aligned"] = XY_corrected[X_pca.shape[0] :]
 
 
 @method(
@@ -27,8 +40,8 @@ def _mnn(adata, n_svd=100):
     paper_name="Batch effects in single-cell RNA-sequencing data are corrected by matching mutual nearest neighbors",
     paper_url="https://www.nature.com/articles/nbt.4091",
     paper_year=2018,
-    code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
+    code_url="https://github.com/LTLA/batchelor",
+    code_version=check_version("rpy2"),
 )
 def mnn_log_cpm(adata, n_svd=100):
     log_cpm(adata)
@@ -41,8 +54,8 @@ def mnn_log_cpm(adata, n_svd=100):
     paper_name="Batch effects in single-cell RNA-sequencing data are corrected by matching mutual nearest neighbors",
     paper_url="https://www.nature.com/articles/nbt.4091",
     paper_year=2018,
-    code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
+    code_url="https://github.com/LTLA/batchelor",
+    code_version=check_version("rpy2"),
 )
 def mnn_log_scran_pooling(adata, n_svd=100):
     log_scran_pooling(adata)
