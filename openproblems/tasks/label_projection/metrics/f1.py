4e80fa2204b2273377a906f0568847a8dc78fbd3
Scott Gigante <scottgigante@gmail.com>
2020-10-05 16:43:05 -0400

    fix classification
    Former-commit-id: 7d108187824443ac6b6e9d9f90343fc586bcd461

--- openproblems/tasks/label_projection/metrics/f1.py
+++ openproblems/tasks/label_projection/metrics/f1.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder
 
 from ....tools.decorators import metric
 
@@ -6,6 +7,11 @@ from ....tools.decorators import metric
 @metric(metric_name="F1 score", maximize=True)
 def f1(adata):
     test_data = adata[~adata.obs["is_train"]]
+
+    encoder = LabelEncoder().fit(adata.obs["labels"])
+    test_data.obs["labels"] = encoder.transform(test_data.obs["labels"])
+    test_data.obs["labels_pred"] = encoder.transform(test_data.obs["labels_pred"])
+
     return f1_score(
         test_data.obs["labels"], test_data.obs["labels_pred"], average="weighted"
     )
