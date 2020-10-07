9e341d0d95fb592036136c497846eb595b7167a3
Scott Gigante <scottgigante@gmail.com>
2020-10-07 17:16:47 -0400

    install R and method specific requirements
    Former-commit-id: 68c3566aa1041580e33d9113cfbfac4f680ad748

--- setup.py
+++ setup.py
@@ -52,12 +52,8 @@ setup(
         "doc": doc_requires,
         "methods": methods_requires,
         "evaluate": evaluate_requires + methods_requires,
-        "multimodal_data_integration-harmonic_alignment_sqrt_cpm": [
-            "harmonicalignment @ git+https://github.com/KrishnaswamyLab/harmonic-alignment#subdirectory=python"
-        ],
-        "multimodal_data_integration-harmonic_alignment_log_scran_pooling": [
-            "harmonicalignment @ git+https://github.com/KrishnaswamyLab/harmonic-alignment#subdirectory=python"
-        ],
+        "multimodal_data_integration-harmonic_alignment_sqrt_cpm": harmonic_alignment_requires,
+        "multimodal_data_integration-harmonic_alignment_log_scran_pooling": harmonic_alignment_requires,
     },
     test_suite="nose2.collector.collector",
     long_description=readme,
