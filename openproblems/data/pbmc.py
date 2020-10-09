f24fb718b1115ca85130a45f2e56fddb00075d22
Florian Wagner <florian.wagner@hey.com>
2020-10-09 14:59:18 -0500

    set up basic task structure

---
+++ openproblems/data/pbmc.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+
+import numpy as np
+import scprep
+import scanpy as sc
+
+from .utils import loader
+from ..tools import normalize as n
+
+@loader
+def load_pbmc(test=False):
+    """Downloads PBMC data from figshare"""
+    URL = "https://ndownloader.figshare.com/files/24974465"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = os.path.join(tempdir, "pbmc.h5ad")
+        scprep.io.download.download_url(URL, filepath)
+        adata = anndata.read_h5ad(filepath)
+
+    if test:
+        sc.pp.subsample(adata, n_obs=1000)
+        adata = adata[:, :1000]
+
+    return adata
---
+++ openproblems/tasks/denoising/methods/__init__.py
@@ -0,0 +1,1 @@
+from .no_denoising import no_denoising
