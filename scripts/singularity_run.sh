e457b3734d666711be7557cfd6fb1ea4559388ff
Scott Gigante <scottgigante@gmail.com>
2020-10-09 14:32:17 -0400

    try a different way
    Former-commit-id: 26ff44e1bcbf4bbcf4bd0c701d04d3bca353c5ea

---
+++ scripts/singularity_run.sh
@@ -0,0 +1,8 @@
+WORKDIR=$1
+SCRIPT=$2
+TASKNAME=$3
+FUN=$4
+ARRAY=( $@ )
+LEN=${#ARRAY[@]}
+ARGS=${ARRAY[@]:4:$LEN}
+pip install --no-cache-dir --user -q -U ${WORKDIR}/..[${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $TASKNAME $FUN ${ARGS}
