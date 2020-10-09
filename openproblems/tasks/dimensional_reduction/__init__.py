0725a6d864a557870528f04c048da4da223df1af
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2020-10-09 16:21:10 -0400

    Update __init__.py

--- openproblems/tasks/dimensional_reduction/__init__.py
+++ openproblems/tasks/dimensional_reduction/__init__.py
@@ -6,4 +6,3 @@ _task_name = "Multimodal Data Integration"
 DATASETS = utils.get_callable_members(datasets)
 METHODS = utils.get_callable_members(methods)
 METRICS = utils.get_callable_members(metrics)
-
