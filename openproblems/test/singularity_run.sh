4d2dc394bad3c584092f66303763133512feefe6
Scott Gigante <scottgigante@gmail.com>
2020-10-09 15:59:18 -0400

    fix install path

--- openproblems/test/singularity_run.sh
+++ openproblems/test/singularity_run.sh
@@ -6,4 +6,4 @@ FUN=$4
 ARRAY=( $@ )
 LEN=${#ARRAY[@]}
 ARGS=${ARRAY[@]:4:$LEN}
-pip install --no-cache-dir --user -q -U ${WORKDIR}/..[${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $SCRIPT $TASKNAME $FUN ${ARGS}
+pip install --no-cache-dir --user -q -U ${WORKDIR}/../..[${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $SCRIPT $TASKNAME $FUN ${ARGS}
