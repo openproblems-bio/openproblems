d2d4524cecf7551a1f91bc6e6fd865e7a197dc5a
Florian Wagner <florian.wagner@hey.com>
2020-10-10 13:22:07 -0500

    replace 1k PBMC dataset with 10k PBMC dataset again and fix load_pbmc() function to reduce number of genes to 1000 wduring testing
    Former-commit-id: c08d12de3ccc715169a62cb23de1479cb7f59ca4

--- openproblems/data/pbmc.py
+++ openproblems/data/pbmc.py
@@ -12,7 +12,7 @@ from ..tools import normalize as n
 @loader
 def load_pbmc(test=False):
     """Downloads PBMC data from figshare"""
-    URL = "https://ndownloader.figshare.com/files/24974582"
+    URL = "https://ndownloader.figshare.com/files/24974465"
 
     with tempfile.TemporaryDirectory() as tempdir:
         filepath = os.path.join(tempdir, "pbmc.h5ad")
@@ -20,7 +20,8 @@ def load_pbmc(test=False):
         adata = sc.read_h5ad(filepath)
 
     if test:
-        sc.pp.subsample(adata, n_obs=1000)
+        sc.pp.subsample(adata, n_obs=100)
         adata = adata[:, :1000]
 
     return adata
+ 
