1d048b49505bd830ee474205dfffa2f6de88b0be
Florian Wagner <florian.wagner@hey.com>
2020-10-10 10:36:37 -0500

    fix formatting
    Former-commit-id: 8cfb771c933aa3a6f5a11f285d5b91ae4033ecf4

--- openproblems/tasks/denoising/methods/no_denoising.py
+++ openproblems/tasks/denoising/methods/no_denoising.py
@@ -2,14 +2,15 @@ from scanpy import AnnData
 
 from ....tools.decorators import method
 
+
 @method(
     method_name="No denoising",
     paper_name="N/A",
-    #paper_url="https://books.google.com/books?id=64JYAwAAQBAJ",
-    #paper_year=2013,
-    #code_url="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html",
+    # paper_url="https://books.google.com/books?id=64JYAwAAQBAJ",
+    # paper_year=2013,
+    # code_url="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html",
     code_version=check_version("scanpy"),
 )
 def no_denoising(adata: AnnData) -> None:
     """Do nothing."""
-    adata.obsm['denoised'] = adata.obsm['train']
+    adata.obsm["denoised"] = adata.obsm["train"]
--- openproblems/tasks/denoising/metrics/mse.py
+++ openproblems/tasks/denoising/metrics/mse.py
@@ -1,17 +1,17 @@
 @metric(metric_name="Mean-squared error", maximize=False)
 def mse(adata):
 
-    test_data = AnnData(X=adata.obsm['test'], obs=adata.obs, var=adata.var)
-    denoised_data = AnnData(X=adata.obsm['denoised'], obs=adata.obs, var=adata.var)
+    test_data = AnnData(X=adata.obsm["test"], obs=adata.obs, var=adata.var)
+    denoised_data = AnnData(X=adata.obsm["denoised"], obs=adata.obs, var=adata.var)
 
     # scaling and transformation
     target_sum = np.median(test_data.X.sum(axis=1))
 
     sc.pp.normalize_total(test_data, target_sum)
     sc.pp.log1p(test_data)
-    
+
     sc.pp.normalize_total(denoised_data, target_sum)
     sc.pp.log1p(denoised_data)
-    
+
     error = mean_squared_error(test_data.X, denoised_data.X)
     return error
