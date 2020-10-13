3befa38d0eba784ae67a4530e1eebb94e32558e2
Scott Gigante <scottgigante@gmail.com>
2020-10-13 15:38:45 -0400

    fix shell scripts

--- scripts/singularity_run.sh
+++ scripts/singularity_run.sh
@@ -3,7 +3,5 @@ WORKDIR=$1
 SCRIPT=$2
 TASKNAME=$3
 FUN=$4
-ARRAY=( $@ )
-LEN=${#ARRAY[@]}
-ARGS=${ARRAY[@]:4:$LEN}
+ARGS=${@:5}
 pip install --no-cache-dir --user -q -U ${WORKDIR}/..[${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $SCRIPT $TASKNAME $FUN ${ARGS}
