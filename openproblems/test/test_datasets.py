b274a2cb8bf76980afc060c2f98900be1e656f82
Scott Gigante <scottgigante@gmail.com>
2020-10-13 22:48:14 -0400

    warn instead of error on dense data
    Former-commit-id: 0cdefe1b63cf4f44255d948e6855185b1c3c828e

--- openproblems/test/test_datasets.py
+++ openproblems/test/test_datasets.py
@@ -1,6 +1,7 @@
 import anndata
 import parameterized
 import openproblems
+import warnings
 from scipy import sparse
 from openproblems.test import utils
 
@@ -21,7 +22,14 @@ def test_task(dataset, task, test):
     assert isinstance(adata, anndata.AnnData)
     assert adata.shape[0] > 0
     assert adata.shape[1] > 0
-    assert sparse.issparse(adata.X)
+    if not sparse.issparse(adata.X):
+        warnings.warn(
+            "{}-{}: Dense data will raise an error in openproblems v1.0".format(
+                task.__name__.split(".")[-1], dataset.__name__
+            ),
+            FutureWarning,
+        )
+    # assert sparse.issparse(adata.X)
     assert adata.X.sum(axis=0).min() > 0
     assert adata.X.sum(axis=1).min() > 0
     if test:
