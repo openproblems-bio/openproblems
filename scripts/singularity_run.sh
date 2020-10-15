1d9ed446e10212561b8804c23738ec1d10d5d3cc
Scott Gigante <scottgigante@gmail.com>
2020-10-15 00:21:31 -0400

    install test
    Former-commit-id: 368bffcf761df5efaa41756266c8d7925fe9c0ed

--- scripts/singularity_run.sh
+++ scripts/singularity_run.sh
@@ -4,4 +4,4 @@ SCRIPT=$2
 TASKNAME=$3
 FUN=$4
 ARGS=${@:5}
-pip install --no-cache-dir --user -q -U ${WORKDIR}/..[test,${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $SCRIPT $TASKNAME $FUN ${ARGS}
+pip install --no-cache-dir --user -q -U ${WORKDIR}/..[${TASKNAME}-${FUN}] && cd ${WORKDIR} && python3 $SCRIPT $TASKNAME $FUN ${ARGS}
