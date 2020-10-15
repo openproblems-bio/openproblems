f70ba6bcefc8ec40ab7200483d55810c8d140548
Scott Gigante <scottgigante@gmail.com>
2020-10-15 15:00:33 -0400

    cache singularity images

--- openproblems/test/test_methods.py
+++ openproblems/test/test_methods.py
@@ -11,7 +11,7 @@ from openproblems.test import utils
 utils.ignore_warnings()
 
 TESTDIR = os.path.dirname(os.path.abspath(__file__))
-CACHEDIR = os.path.join(tempfile.gettempdir(), ".singularity")
+CACHEDIR = os.path.join(os.environ["HOME"], ".singularity")
 os.environ["SINGULARITY_CACHEDIR"] = CACHEDIR
 os.environ["SINGULARITY_PULLFOLDER"] = CACHEDIR
 
