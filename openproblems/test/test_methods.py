8844a69aef57d73e00389dfe56603c04d4eba30d
Scott Gigante <scottgigante@gmail.com>
2020-10-28 18:16:02 -0400

    print age output
    Former-commit-id: dd07a335941a57e73df908bb4bfb1683f321499d

--- openproblems/test/test_methods.py
+++ openproblems/test/test_methods.py
@@ -1,4 +1,3 @@
-import unittest
 import parameterized
 import tempfile
 import os
@@ -54,9 +53,13 @@ def image_requires_docker(image):
             push_timestamp = int(handle.read().strip())
     except FileNotFoundError:
         push_timestamp = 0
+    print("{}: push age {}".format(image, push_timestamp))
     image_age = utils.git_file_age(dockerfile)
+    print("{}: git age {}".format(dockerfile, image_age))
     for req in requirements:
-        image_age = max(image_age, utils.git_file_age(req))
+        req_age = utils.git_file_age(req)
+        print("{}: git age {}".format(req, req_age))
+        image_age = max(image_age, req_age)
     if push_timestamp > utils.git_file_age(dockerfile):
         return False
     else:
