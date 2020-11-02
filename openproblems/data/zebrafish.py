8c6e85a813c20c219610e210828277c91d13f0d4
Scott Gigante <scottgigante@gmail.com>
2020-11-02 17:40:34 -0500

    clean up
    Former-commit-id: 8e6feba945983fec5667d66819bd5ba53a5c52a8

--- openproblems/data/zebrafish.py
+++ openproblems/data/zebrafish.py
@@ -6,12 +6,14 @@ import scprep
 
 from . import utils
 
+URL = (
+    "https://ndownloader.figshare.com/files/24566651?private_link=e3921450ec1bd0587870"
+)
+
 
 @utils.loader
 def load_zebrafish(test=False):
-    """Downloads zebrafish data from figshare"""
-    URL = "https://ndownloader.figshare.com/files/24566651?private_link=e3921450ec1bd0587870"
-
+    """Download zebrafish data from figshare."""
     with tempfile.TemporaryDirectory() as tempdir:
         filepath = os.path.join(tempdir, "zebrafish.h5ad")
         scprep.io.download.download_url(URL, filepath)
