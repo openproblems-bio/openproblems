e75631df7a96177f10287122d09e04ab9051ffbe
Scott Gigante <scottgigante@gmail.com>
2020-11-09 16:42:09 -0500

    adaptive tech selection
    Former-commit-id: 71c3964699f0b6965c45fab089bf309cdb50aa68

--- openproblems/tasks/label_projection/datasets/pancreas.py
+++ openproblems/tasks/label_projection/datasets/pancreas.py
@@ -9,7 +9,7 @@ def pancreas_batch(test=False):
     adata.obs["labels"] = adata.obs["celltype"]
 
     # Assign training/test
-    test_batches = ["inDrop4", "smartseq2"]
+    test_batches = adata.obs["tech"].dtype.categories[[-3, -1]]
     adata.obs["is_train"] = [
         False if adata.obs["tech"][idx] in test_batches else True
         for idx in adata.obs_names
