9f4242e882d2c3a7674f9e88fbf8bfa2a136a970
Scott Gigante <scottgigante@gmail.com>
2020-11-09 12:49:58 -0500

    remove redundant code
    Former-commit-id: e2ce414a77084bf6e96758bbca873268df3a78a3

--- openproblems/data/multimodal/scicar/base.py
+++ openproblems/data/multimodal/scicar/base.py
@@ -15,18 +15,11 @@ def load_scicar(
     atac_url,
     atac_cells_url,
     atac_genes_url,
-    with_peak=False,
 ):
-    if not with_peak:
-        rna_cells = pd.read_csv(rna_cells_url, low_memory=False)["sample"]
-        rna_genes = pd.read_csv(rna_genes_url, low_memory=False)["gene_id"]
-        atac_cells = pd.read_csv(atac_cells_url, low_memory=False)["sample"]
-        atac_genes = pd.read_csv(atac_genes_url, low_memory=False, index_col=0)["peak"]
-    else:
-        rna_genes = pd.read_csv(rna_genes_url, low_memory=False, index_col=0)
-        atac_genes = pd.read_csv(atac_genes_url, low_memory=False, index_col=1)
-        rna_cells = pd.read_csv(rna_cells_url, low_memory=False, index_col=0)
-        atac_cells = pd.read_csv(atac_cells_url, low_memory=False, index_col=0)
+    rna_genes = pd.read_csv(rna_genes_url, low_memory=False, index_col=0)
+    atac_genes = pd.read_csv(atac_genes_url, low_memory=False, index_col=1)
+    rna_cells = pd.read_csv(rna_cells_url, low_memory=False, index_col=0)
+    atac_cells = pd.read_csv(atac_cells_url, low_memory=False, index_col=0)
 
     with tempfile.TemporaryDirectory() as tempdir:
         rna_file = os.path.join(tempdir, "rna.mtx.gz")
@@ -36,44 +29,22 @@ def load_scicar(
         scprep.io.download.download_url(atac_url, atac_file)
         atac_data = scprep.io.load_mtx(atac_file, cell_axis="col").tocsr()
 
-    print(atac_genes)
+    adata = create_joint_adata(
+        rna_data,
+        atac_data,
+        X_index=rna_cells.index,
+        X_columns=rna_genes.index,
+        Y_index=atac_cells.index,
+        Y_columns=atac_genes.index,
+    )
 
-    if not with_peak:
-        adata = create_joint_adata(
-            rna_data,
-            atac_data,
-            X_index=rna_cells,
-            X_columns=rna_genes,
-            Y_index=atac_cells,
-            Y_columns=atac_genes,
-        )
-        adata = filter_joint_data_empty_cells(adata)
-    else:
-        rna_data, rna_cells_index = scprep.filter.filter_empty_cells(
-            rna_data, rna_cells.index
-        )
-        atac_data, atac_cells_index = scprep.filter.filter_empty_cells(
-            atac_data, atac_cells.index
-        )
+    # merge obs and var
+    adata.obs = rna_cells.loc[adata.obs.index]
+    adata.var = rna_genes
+    for key in atac_cells.columns:
+        adata.obs[key] = atac_cells[key]
+    for key in atac_genes.columns:
+        adata.uns["mode2_var_{}".format(key)] = atac_genes[key]
 
-        common_cells = np.intersect1d(rna_cells_index, atac_cells_index)
-
-        rna_subset = np.isin(rna_cells_index, common_cells)
-        rna_cells, rna_data = rna_cells.loc[rna_subset], rna_data[rna_subset]
-        rna_order = np.argsort(rna_cells.index)
-        rna_cells, rna_data = rna_cells.iloc[rna_order], rna_data[rna_order]
-
-        atac_subset = np.isin(atac_cells_index, common_cells)
-        atac_cells, atac_data = atac_cells.loc[atac_subset], atac_data[atac_subset]
-        atac_order = np.argsort(atac_cells.index)
-        atac_cells, atac_data = atac_cells.iloc[atac_order], atac_data[atac_order]
-
-        adata = anndata.AnnData(
-            rna_data,
-            obs=rna_cells,
-            var=rna_genes,
-        )
-        adata.obsm["mode2"] = atac_data
-        adata.uns["mode2_obs"] = atac_cells.to_numpy()
-        adata.uns["mode2_var"] = atac_genes.iloc[:, 1:].to_numpy()
+    adata = filter_joint_data_empty_cells(adata)
     return adata
--- openproblems/tasks/chromatin_potential/datasets/scicar_chrom.py
+++ openproblems/tasks/chromatin_potential/datasets/scicar_chrom.py
@@ -4,4 +4,4 @@ from ....data.multimodal import scicar
 
 @dataset("sciCAR Mouse Kidney with cell clusters")
 def scicar_mouse_kidney(test=False):
-    return scicar.load_scicar_mouse_kidney(test=test, with_peak=True)
+    return scicar.load_scicar_mouse_kidney(test=test)
