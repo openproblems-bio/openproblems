5c9771ee0c25dfa03d1c53f6bef99c75a4fd3cea
Scott Gigante <scottgigante@gmail.com>
2020-11-17 14:35:09 -0500

    more docs

--- openproblems/tasks/label_projection/methods/logistic_regression.py
+++ openproblems/tasks/label_projection/methods/logistic_regression.py
@@ -49,7 +49,8 @@ def _logistic_regression(adata, max_iter=1000, n_pca=100):
     paper_name="Applied Logistic Regression",
     paper_url="https://books.google.com/books?id=64JYAwAAQBAJ",
     paper_year=2013,
-    code_url="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html",
+    code_url="https://scikit-learn.org/stable/modules/generated/"
+    "sklearn.linear_model.LogisticRegression.html",
     code_version=check_version("scikit-learn"),
 )
 def logistic_regression_log_cpm(adata):
@@ -62,7 +63,8 @@ def logistic_regression_log_cpm(adata):
     paper_name="Applied Logistic Regression",
     paper_url="https://books.google.com/books?id=64JYAwAAQBAJ",
     paper_year=2013,
-    code_url="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html",
+    code_url="https://scikit-learn.org/stable/modules/generated/"
+    "sklearn.linear_model.LogisticRegression.html",
     code_version=check_version("scikit-learn"),
     image="openproblems-r-base",
 )
