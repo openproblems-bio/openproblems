60e87abde399d34ac0928a03f8fa99c9886640d0
Scott Gigante <scottgigante@gmail.com>
2020-11-23 14:20:43 -0500

    more documentation

--- openproblems/test/run_test_metric.py
+++ openproblems/test/run_test_metric.py
@@ -7,17 +7,22 @@ from openproblems.test import utils
 utils.ignore_warnings()
 
 
-def test_method(task, data_path, metric):
+def test_metric(task, data_path, metric):
+    """Test a metric applied to a specific dataset run through a specific method."""
     adata = anndata.read_h5ad(data_path)
     m = metric(adata)
     assert isinstance(m, numbers.Number)
 
 
 def main(task_name, metric_name, data_path):
+    """Run the test.
+
+    This test is run via a subprocess call in test_metrics.py.
+    """
     openproblems.data.no_cleanup()
     task = getattr(openproblems.tasks, task_name)
     metric = getattr(task.metrics, metric_name)
-    test_method(task, data_path, metric)
+    test_metric(task, data_path, metric)
 
 
 if __name__ == "__main__":
