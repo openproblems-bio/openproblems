2621f87f7f06b352cb948b04338b2da7fb87dab7
mvinyard <mvinyard@broadinstitute.org>
2020-11-27 07:25:27 +0000

    intermediate fix to init
    Former-commit-id: 2040a3e60b5f7c024be802cbb7ab78dc886450da

--- openproblems/tasks/dimensional_reduction/datasets/__init__.py
+++ openproblems/tasks/dimensional_reduction/datasets/__init__.py
@@ -1,2 +1,4 @@
-from .10x_pbmc import tenx_5k_pbmc
-from .citeseq import citeseq_cbmc
+
+from .citeseq import load_citeseq_cbmc
+from .human_blood_nestorowa2016 import human_blood_nestorowa2016
+from .10x_5k_pbmc import tenx_5k_pbmc
