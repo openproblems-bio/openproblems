59ff94471cbd024568bb2a9bc259e6b514ebae5a
mvinyard <mvinyard@broadinstitute.org>
2020-11-27 07:10:37 +0000

    added checks.py as well as check func to metrics and fixed API
    Former-commit-id: be299f3272c52f042b73062105ebee9bf83893e4

--- openproblems/tasks/dimensional_reduction/metrics/pearsons_chi_squared.py
+++ openproblems/tasks/dimensional_reduction/metrics/pearsons_chi_squared.py
@@ -1,5 +1,6 @@
 
 from ....tools.decorators import metric
+from ..checks import check_method
 
 
 @metric(metric_name="Pearson's chi squared test", maximize=True)
@@ -8,11 +9,13 @@ def chi_squared(adata, method):
     """
     Perform Pearson's chi squared test to estimate the goodness of fit of dimensional reduction.
     """
-
-    from scipy.stats import chisquare
-
-    adata.obsp["chi_squared"] = chisquare(adata.obsm["dimensional_reduction_method"], f_exp=adata.X.astype(array))
     
-    ### need to add a summation method to this
-    
-    return float(adata.uns["chi_squared_sum"])
+    if check_method(adata) == True:
+
+        from scipy.stats import chisquare
+
+        adata.obsp["chi_squared"] = chisquare(adata.obsm["dimensional_reduction_method"], f_exp=adata.X.astype(array))
+
+        ### need to add a summation method to this
+
+        return float(adata.uns["chi_squared_sum"])
