dddd5fbbf234724d9bb017d34d77bea13c086bf8
Scott Gigante <scottgigante@gmail.com>
2020-12-04 14:14:18 -0500

    configure logging

--- openproblems/__init__.py
+++ openproblems/__init__.py
@@ -2,7 +2,9 @@ import logging as _logging
 from . import tasks, utils, tools, patch
 from .version import __version__
 
+_logging.basicConfig()
 log = _logging.getLogger("openproblems")
+
 TASKS = utils.get_members(tasks)
 
 patch.patch_anndata()
