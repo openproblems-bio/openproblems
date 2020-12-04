4db08956126d2b6acc5d99493d4513569dce0d7c
Scott Gigante <scottgigante@gmail.com>
2020-12-04 15:02:55 -0500

    install nose2
    Former-commit-id: 8c2c804a71c3054ab2dc14bf2386e7a36a38296a

--- test/singularity_run.sh
+++ test/singularity_run.sh
@@ -10,7 +10,7 @@ CODEDIR=$(dirname $WORKDIR)
 
 python3 -m pip install --upgrade "pip<=21.0"
 python3 -m pip install --use-deprecated=legacy-resolver --upgrade-strategy=only-if-needed --no-cache-dir -U $CODEDIR
-python3 -m pip install --use-deprecated=legacy-resolver -U coverage
+python3 -m pip install --use-deprecated=legacy-resolver -U coverage nose2
 
 cd ${WORKDIR}
 python3 -m coverage run --parallel --source=${CODEDIR}/openproblems $SCRIPT $TASKNAME $FUN ${ARGS}
