cda02d45a46fbd7ed642702c66f8708350a20aa8
Scott Gigante <scottgigante@gmail.com>
2020-12-04 14:59:43 -0500

    less quiet

--- test/singularity_run.sh
+++ test/singularity_run.sh
@@ -9,8 +9,8 @@ ARGS=${ARRAY[@]:4:$LEN}
 CODEDIR=$(dirname $WORKDIR)
 
 python3 -m pip install --upgrade "pip<=21.0"
-python3 -m pip install --use-deprecated=legacy-resolver --upgrade-strategy=only-if-needed --no-cache-dir -qq -U $CODEDIR
-python3 -m pip install --use-deprecated=legacy-resolver -qq -U coverage
+python3 -m pip install --use-deprecated=legacy-resolver --upgrade-strategy=only-if-needed --no-cache-dir -U $CODEDIR
+python3 -m pip install --use-deprecated=legacy-resolver -U coverage
 
 cd ${WORKDIR}
 python3 -m coverage run --parallel --source=${CODEDIR}/openproblems $SCRIPT $TASKNAME $FUN ${ARGS}
