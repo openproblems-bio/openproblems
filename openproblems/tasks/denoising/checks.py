6bbe1641ef64ad00bfbc7d8da484da08ade7fde0
Scott Gigante <scottgigante@gmail.com>
2020-12-07 10:26:00 -0500

    lint

--- openproblems/tasks/denoising/checks.py
+++ openproblems/tasks/denoising/checks.py
@@ -1,12 +1,11 @@
-import numpy as np
-
-
 def check_dataset(adata):
+    """Check that dataset output fits expected API."""
     assert "train" in adata.obsm
     assert "test" in adata.obsm
     return True
 
 
 def check_method(adata):
+    """Check that method output fits expected API."""
     assert "denoised" in adata.obsm
     return True
