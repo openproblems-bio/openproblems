124a9d1188ad63a36d2aaf09f6b7c8ffe07455e7
Scott Gigante <scottgigante@gmail.com>
2020-12-07 10:20:47 -0500

    rename 10x to tenx
    Former-commit-id: a9c8a678d54a120695ab52eb7fe62c3fb4ee9838

--- openproblems/tasks/dimensional_reduction/datasets/10x_pbmc.py
+++
@@ -1,7 +0,0 @@
-from ....data.10x_5k_pbmc import load_10x_5k_pbmc
-from ....tools.decorators import dataset
-
-
-@dataset("10X PBMCs v3 Chemistry")
-def tenx_5k_pbmc(test=False):
-    return load_10x_5k_pbmc(test=test)
