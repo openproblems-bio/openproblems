fc414aaba87f28007ab9f58277b60c10239c7003
Scott Gigante <scottgigante@gmail.com>
2020-12-07 10:32:08 -0500

    fix remaining 10x
    Former-commit-id: a81848543688c57a5b68500ae51a80c5fadddaf9

--- openproblems/tasks/dimensional_reduction/datasets/__init__.py
+++ openproblems/tasks/dimensional_reduction/datasets/__init__.py
@@ -1,4 +1,3 @@
-
 from .citeseq import load_citeseq_cbmc
 from .human_blood_nestorowa2016 import human_blood_nestorowa2016
-from .10x_5k_pbmc import tenx_5k_pbmc
+from .tenx_5k_pbmc import tenx_5k_pbmc
