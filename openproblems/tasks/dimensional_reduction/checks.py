794abbbdb536560d37a65e1c5a5806f3225e2cb6
Scott Gigante <scottgigante@gmail.com>
2020-12-07 11:40:35 -0500

    fix checks

--- openproblems/tasks/dimensional_reduction/checks.py
+++ openproblems/tasks/dimensional_reduction/checks.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 
+def check_dataset(adata):
+    """Check that dataset output fits expected API."""
+    return True
+
+
 def check_method(adata):
-
+    """Check that method output fits expected API."""
     assert np.sum(adata.obsm["X_emb"]) > 0
-
     return True
