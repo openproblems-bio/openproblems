9eafacf68a48b157b1a501e540a340ca4a20bdd2
Scott Gigante <scottgigante@gmail.com>
2021-02-17 11:35:04 -0500

    import new tasks

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -1,3 +1,6 @@
+from . import batch_integration_embed
+from . import batch_integration_features
+from . import batch_integration_graph
 from . import chromatin_potential
 from . import denoising
 from . import dimensional_reduction
