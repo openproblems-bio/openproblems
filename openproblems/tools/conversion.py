98046934e3863ac15436f55fd85de9e417afdfe6
Scott Gigante <scottgigante@gmail.com>
2021-03-05 14:12:46 -0500

    cleaner caller ref
    Former-commit-id: 6b800d90e0819a553cbaef95c2048ccbc0dabdd2

--- openproblems/tools/conversion.py
+++ openproblems/tools/conversion.py
@@ -1,8 +1,9 @@
 import os
 import scprep
+import inspect
 
 
-def r_function(filename, caller):
+def r_function(filename):
     """Convert a .R file to a Python function.
 
     This code takes a .R file written as the body of a function.
@@ -21,9 +22,11 @@ def r_function(filename, caller):
     fun : scprep.run.RFunction
         Python callable evaluating the R code
     """
+    curr_frame = inspect.currentframe()
+    prev_frame = inspect.getframeinfo(curr_frame.f_back)
     assert filename.endswith(".R")
-    filepath = os.path.join(os.path.dirname(caller), filename)
-    with open(filepath, "w") as handle:
+    filepath = os.path.join(os.path.dirname(prev_frame.filename), filename)
+    with open(filepath, "r") as handle:
         r_code = handle.read()
 
     r_fun = "function evaluate(sce) {{ {code} }}".format(code=r_code)
