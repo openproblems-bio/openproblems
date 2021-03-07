c0012a9b22a3c670e27a34e5dc143f02c077fad9
Scott Gigante <scottgigante@gmail.com>
2021-03-06 20:28:05 -0500

    better hashing

--- openproblems/api/hash.py
+++ openproblems/api/hash.py
@@ -1,15 +1,19 @@
+from ..tools.utils import check_version
 from . import utils
 
+import hashlib
 import importlib
 import os
 import subprocess
+
+_MODULE = type(os)
 
 
 def get_module(fun):
     """Get the Python module in which a function is defined."""
     if hasattr(fun, "__wrapped__"):
         fun = fun.__wrapped__
-    return importlib.import_module(fun.__module__)
+    return fun.__module__
 
 
 def git_hash(file):
@@ -26,11 +30,49 @@ def git_hash(file):
         return p.stdout.decode()
 
 
+def get_context(obj, context=None):
+    if context is None:
+        context = dict()
+    if isinstance(obj, _MODULE):
+        module_name = obj.__name__
+    else:
+        try:
+            module_name = get_module(obj)
+        except AttributeError:
+            # doesn't belong to a module
+            return context
+    if module_name in context:
+        # already done
+        pass
+    elif not module_name.startswith("openproblems"):
+        # use version number of external libs
+        version = check_version(module_name.split(".")[0])
+        if version != "ModuleNotFound":
+            context[module_name] = version
+    else:
+        module = importlib.import_module(module_name)
+        context[module_name] = git_hash(module.__file__)
+        for member_name in dir(module):
+            member = getattr(module, member_name)
+            if callable(member) or isinstance(member, _MODULE):
+                context = get_context(member, context=context)
+    return context
+
+
+def hash_dict(context):
+    """Convert a SHA256 hash for a Python dictionary of strings."""
+    hash = hashlib.sha256()
+    for key in sorted(context.keys()):
+        hash.update(bytes(key, encoding="utf-8"))
+        hash.update(bytes(context[key], encoding="utf-8"))
+    return hash.hexdigest()
+
+
 def get_hash(task_name, function_type, function_name):
     """Get the git commit hash associated with a function."""
     fun = utils.get_function(task_name, function_type, function_name)
-    module = get_module(fun)
-    hash = git_hash(module.__file__)
+    context = get_context(fun)
+    hash = hash_dict(context)
     return hash
 
 
