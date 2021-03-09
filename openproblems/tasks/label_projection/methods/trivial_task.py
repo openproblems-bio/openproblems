945778c8f5ca517b3ccfb8e7403de37f808dbaf2
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2021-03-09 10:33:56 -0500

    Update trivial_task.py

--- openproblems/tasks/label_projection/methods/trivial_task.py
+++ openproblems/tasks/label_projection/methods/trivial_task.py
@@ -8,7 +8,7 @@ import numpy as np
     paper_url="https://github.com/dburkhardt",
     paper_year=2021,
     code_url="https://github.com/dburkhardt",
-    code_version="1.0.0",
+    code_version="1.0.1",
 )
 def trivial_method(adata):
     adata.obs["labels_pred"] = np.full(shape=adata.n_obs, fill_value='unknown')
