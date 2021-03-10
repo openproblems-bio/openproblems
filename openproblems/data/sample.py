b79130e9930ce7e1b53e23543fb72375f207b382
Scott Gigante <scottgigante@gmail.com>
2021-03-10 13:45:48 -0500

    fix anndata constructor
    Former-commit-id: 36b9799a1e7624b482f0887cc2161f58724cb8fc

--- openproblems/data/sample.py
+++ openproblems/data/sample.py
@@ -13,10 +13,12 @@ def load_sample_data(test=True):
     """Create a simple dataset to use for testing in multimodal applications."""
     assert test
 
-    genes = pd.read_csv(rna_genes_url, low_memory=False, index_col=0).index[:500]
-    cells = pd.read_csv(rna_cells_url, low_memory=False, index_col=0).index
+    genes = pd.read_csv(rna_genes_url, low_memory=False, index_col=0).iloc[:500]
+    cells = pd.read_csv(rna_cells_url, low_memory=False, index_col=0).iloc[:200]
 
-    rna_data = scipy.sparse.csr_matrix(np.random.poisson(2, (len(cells), len(genes))))
+    rna_data = scipy.sparse.csr_matrix(
+        np.random.poisson(2, (cells.shape[0], genes.shape[0]))
+    )
 
-    adata = anndata.AnnData(rna_data, index=cells, columns=genes)
+    adata = anndata.AnnData(rna_data, obs=cells, var=genes)
     return adata
