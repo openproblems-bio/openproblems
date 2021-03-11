cafded93f52b21665e133fe3bd3261d7c9fc6c4c
Daniel Strobl <daniel.strobl@tum.de>
2021-03-11 14:58:42 +0100

    fix scvi

--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/scvi.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/scvi.py
@@ -16,8 +16,10 @@ def scvi_full_unscaled(adata):
     from scIB.integration import runScvi
     from scIB.preprocessing import reduce_data
 
+    adata.obs.rename(columns={"labels": "lab"})  # ugly fix for scvi conversion error
     adata = runScvi(adata, "batch")
     reduce_data(adata, use_rep="X_emb")
+    adata.obs.rename(columns={"lab": "labels"})  # ugly fix for scvi conversion error
     # Complete the result in-place
     return adata
 
@@ -36,9 +38,11 @@ def scvi_hvg_unscaled(adata):
     from scIB.preprocessing import hvg_batch
     from scIB.preprocessing import reduce_data
 
+    adata.obs.rename(columns={"labels": "lab"})  # ugly fix for scvi conversion error
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
     adata = runScvi(adata, "batch")
     reduce_data(adata, use_rep="X_emb")
+    adata.obs.rename(columns={"lab": "labels"})  # ugly fix for scvi conversion error
     return adata
 
 
@@ -57,10 +61,12 @@ def scvi_hvg_scaled(adata):
     from scIB.preprocessing import reduce_data
     from scIB.preprocessing import scale_batch
 
+    adata.obs.rename(columns={"labels": "lab"})  # ugly fix for scvi conversion error
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
     adata = scale_batch(adata, "batch")
     adata = runScvi(adata, "batch")
     reduce_data(adata, use_rep="X_emb")
+    adata.obs.rename(columns={"lab": "labels"})  # ugly fix for scvi conversion error
     return adata
 
 
@@ -78,7 +84,9 @@ def scvi_full_scaled(adata):
     from scIB.preprocessing import reduce_data
     from scIB.preprocessing import scale_batch
 
+    adata.obs.rename(columns={"labels": "lab"})  # ugly fix for scvi conversion error
     adata = scale_batch(adata, "batch")
     adata = runScvi(adata, "batch")
     reduce_data(adata, use_rep="X_emb")
+    adata.obs.rename(columns={"lab": "labels"})  # ugly fix for scvi conversion error
     return adata
