8c3bf67e18368e1d0ecdc9f7f003d2eb7918a584
adamgayoso <adamgayoso@users.noreply.github.com>
2021-03-29 16:58:21 -0700

    enforce batch metadata in dataset

--- openproblems/tasks/label_projection/api.py
+++ openproblems/tasks/label_projection/api.py
@@ -6,6 +6,7 @@ import numpy as np
 def check_dataset(adata):
     """Check that dataset output fits expected API."""
     assert "labels" in adata.obs
+    assert "batch" in adata.obs
     assert "is_train" in adata.obs
     assert np.sum(adata.obs["is_train"]) > 0
     assert np.sum(~adata.obs["is_train"]) > 0
@@ -21,6 +22,7 @@ def check_method(adata):
 def sample_dataset():
     """Create a simple dataset to use for testing methods in this task."""
     adata = load_sample_data()
+    adata.obs["batch"] = np.random.choice(5, adata.shape[0], replace=True).astype(str)
     adata.obs["labels"] = np.random.choice(5, adata.shape[0], replace=True).astype(str)
     adata.obs["is_train"] = np.random.choice(
         [True, False], adata.shape[0], replace=True, p=[0.8, 0.2]
