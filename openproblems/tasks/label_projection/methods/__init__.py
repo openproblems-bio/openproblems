9e01da52218afb53fd39cb44be64ecd73528610f
Nikolay Markov <nikolai.markov@icloud.com>
2021-03-29 22:28:21 -0500

    Add majority vote simple baseline

--- openproblems/tasks/label_projection/methods/__init__.py
+++ openproblems/tasks/label_projection/methods/__init__.py
@@ -2,5 +2,6 @@ from .knn_classifier import knn_classifier_log_cpm
 from .knn_classifier import knn_classifier_scran
 from .logistic_regression import logistic_regression_log_cpm
 from .logistic_regression import logistic_regression_scran
+from .majority_vote import majority_vote
 from .mlp import mlp_log_cpm
 from .mlp import mlp_scran
