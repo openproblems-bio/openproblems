ef7c72f1e99fe42e999e57dc6fe1e995c018ee1e
Ben DeMeo <bdemeo@mit.edu>
2021-03-30 12:15:38 -0400

    added densmap and densne

---
+++ openproblems/tasks/dimensionality_reduction/methods/densmap.py
@@ -0,0 +1,16 @@
+from ....tools.decorators import method
+from ....tools.utils import check_version
+
+import densmap
+
+@method(
+    method_name="densMAP",
+    paper_name="Assessing single-cell transcriptomic variability through density-preserving data visualization",
+    paper_url="https://www.nature.com/articles/s41587-020-00801-7",
+    paper_year=2021,
+    code_url="https://github.com/hhcho/densvis",
+    code_version='8efe0a2',
+)
+def densmap(adata):
+    adata.obsm['X_emb'] = densmap.densMAP(final_dens=False).fit_transform(adata.X)
+    return adata
