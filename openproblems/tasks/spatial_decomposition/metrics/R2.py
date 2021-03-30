6f67c71fa0a0fd468e467268e8469d438f1e023a
almaan <almaan@kth.se>
2021-03-30 08:39:57 +0200

    black

--- openproblems/tasks/spatial_decomposition/metrics/R2.py
+++ openproblems/tasks/spatial_decomposition/metrics/R2.py
@@ -11,8 +11,7 @@ def R2(adata):
     prop_true = adata.obsm["proportions_true"].values
     prop_pred = adata.obsm["proportions_pred"].values
 
-    r2_score = sklearn.metrics.r2_score(prop_true,
-                                        prop_pred,
-                                        sample_weight=None,
-                                        multioutput='uniform_average')
+    r2_score = sklearn.metrics.r2_score(
+        prop_true, prop_pred, sample_weight=None, multioutput="uniform_average"
+    )
     return r2_score
