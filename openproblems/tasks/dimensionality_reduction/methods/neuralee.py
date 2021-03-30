d5c3807e97db25fd31276a764d8ec2e723c5bee0
Michal Klein <michal.klein@protonmail.com>
2021-03-30 18:00:15 +0200

    Implement method

--- openproblems/tasks/dimensionality_reduction/methods/neuralee.py
+++ openproblems/tasks/dimensionality_reduction/methods/neuralee.py
@@ -4,13 +4,34 @@ from anndata import AnnData
 
 
 @method(
-    method_name="",
-    paper_name="",
-    paper_url="",
+    method_name="NeuralEE (CPU)",
+    paper_name=" NeuralEE: A GPU-Accelerated Elastic Embedding "
+    "Dimensionality Reduction Method for "
+    "Visualizing Large-Scale scRNA-Seq Data ",
+    paper_url="https://pubmed.ncbi.nlm.nih.gov/33193561/",
     paper_year=2020,
-    code_url="",
+    code_url="https://github.com/HiBearME/NeuralEE",
     code_version=check_version("neuralee"),
     image="openproblems-python-extras",
 )
 def neuralee(adata: AnnData) -> AnnData:
+    from neuralee.dataset import GeneExpressionDataset
+    from neuralee.embedding import NeuralEE
+
+    import torch
+
+    dataset = GeneExpressionDataset(adata.X)
+    # this follows "recommended" preprocessing taken from example notebooks, e.g.:
+    # https://github.com/HiBearME/NeuralEE/blob/master/tests/notebooks/retina_dataset.ipynb
+    dataset.log_shift()  # log(x + 1)
+    dataset.subsample_genes(500)  # filter genes according to variance
+    dataset.standardscale()
+    # 10% of cells as batch to the estimate affinity matrix
+    dataset.affinity_split(N_small=0.1)
+
+    NEE = NeuralEE(dataset, d=2, device=torch.device("cpu"))
+    res = NEE.fine_tune(verbose=False)
+
+    adata.obsm["X_emb"] = res["X"]
+
     return adata
