a58e49cedc505d7774cf3d12b1b627a86f1e3d13
Valentine Svensson <v@nxn.se>
2021-03-30 21:07:09 +0000

    Added reference to example label noise dataset in datasets __init__.py

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .pancreas import pancreas_batch
 from .pancreas import pancreas_random
+from .pancreas import pancreas_random_label_noise
 from .zebrafish import zebrafish_labels
 from .zebrafish import zebrafish_random
