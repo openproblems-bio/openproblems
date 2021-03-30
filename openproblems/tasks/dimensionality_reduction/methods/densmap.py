d05f49fe7c26ada3b0d5fd718d4a9940f65bacd9
Ben DeMeo <bdemeo@mit.edu>
2021-03-30 14:41:52 -0400

    umap-learn implementation

--- openproblems/tasks/dimensionality_reduction/methods/densmap.py
+++ openproblems/tasks/dimensionality_reduction/methods/densmap.py
@@ -7,12 +7,12 @@ from ....tools.decorators import method
     " density-preserving data visualization",
     paper_url="https://www.nature.com/articles/s41587-020-00801-7",
     paper_year=2021,
-    code_url="https://github.com/hhcho/densvis",
+    code_url="https://github.com/lmcinnes/umap",
     code_version="8efe0a2",
     image="openproblems-python-extras",
 )
 def densmap(adata):
-    import densmap as dens
+    import umap
 
-    adata.obsm["X_emb"] = dens.densMAP(final_dens=False).fit_transform(adata.X)
+    adata.obsm["X_emb"] = umap.UMAP(densmap=True, random_state=42).fit_transform(adata.X)
     return adata  # return with embedding
