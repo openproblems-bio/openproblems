882a327a24feee1a07d6f521fb840bf10b082ab0
almaan <almaan@kth.se>
2021-03-31 08:09:37 +0200

    add RCTD

--- openproblems/tasks/spatial_decomposition/methods/rctd.py
+++ openproblems/tasks/spatial_decomposition/methods/rctd.py
@@ -1,19 +1,33 @@
+from ....tools.conversion import r_function
 from ....tools.decorators import method
-from ....tools.normalize import log_cpm
 from ....tools.utils import check_version
 
-# import rctd, this is an R tool so need to have also R file
+import numpy as np
+
+_rctd = r_function("rctd.R")
 
 
 @method(
-    method_name="Rctd",
+    method_name="RCTD",
     paper_name="Robust decomposition of cell type mixtures in spatial transcriptomics",
     paper_url="https://www.nature.com/articles/s41587-021-00830-w",
     paper_year=2020,
-    code_url="https://github.com/dmcable/RCTD",
+    code_url="https://github.com/almaan/RCTD",
     code_version=check_version("rctd"),
 )
-def stereoscope_log_cpm(adata):
-    log_cpm(adata)
-    # do something
-    return
+def rctd(adata):
+    # exctract single cell reference data
+    sc_adata = adata.uns["sc_reference"].copy()
+    # remove single cell reference from original anndata
+    del adata.uns["sc_reference"]
+    # set spatial coordinates for the single cell data
+    sc_adata.obsm["spatial"] = np.ones((sc_adata.shape[0], 2))
+    # concatenate single cell and spatial data, r_function only accepts one argument
+    adata = adata.concatenate(
+        sc_adata, batch_key="modality", batch_categories=["sp", "sc"]
+    )
+    # remove single cell reference anndata to reduce memory use
+    del sc_adata
+    # run RCTD
+    adata = _rctd(adata)
+    return adata
