fe3664d2cbddd602e7b6324c6c86592aa2c5c9b1
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2021-03-30 21:04:26 -0400

    Update alra.py
    Change to utilize alra.R support script.
    
    Former-commit-id: cca19ec5a326bbfc9b875e85041c7f7d132009e0

--- openproblems/tasks/denoising/methods/alra.py
+++ openproblems/tasks/denoising/methods/alra.py
@@ -1,27 +1,14 @@
+from ....tools.conversion import r_function
 from ....tools.decorators import method
+from ....tools.normalize import log_cpm
+from ....tools.normalize import log_scran_pooling
 from ....tools.utils import check_version
-
+#should be imported by docker
 import numpy as np
 import scprep
 
-_alra = scprep.run.RFunction(
-    setup="""
-    library(SingleCellExperiment)
-    library(Matrix)
-    library(rsvd)
-    source('https://raw.githubusercontent.com/KlugerLab/ALRA/master/alra.R')
+_alra = r_function("alra.R")
 
-    """,
-    args="sce",
-    body="""
-    assay(sce, "X") <- as(assay(sce, "X"), "CsparseMatrix")
-    reducedDim(sce, "train") <- as(reducedDim(sce, "train"), "CsparseMatrix")
-    completed <- alra(as.matrix(reducedDim(sce, "train")))
-    normCompleted <- completed[[3]]
-    reducedDim(sce, "train") <- as.matrix(normCompleted)
-    sce
-    """,
-)
 
 
 @method(
@@ -34,17 +21,13 @@ _alra = scprep.run.RFunction(
     image="openproblems-r-extras",
 )
 def alra(adata):
-    X, libsize = scprep.normalize.library_size_normalize(
+    #libsize and sqrt norm
+    #overwrite obsm['train'] with normalized version
+    adata.obsm["train"], libsize = scprep.normalize.library_size_normalize(
         adata.obsm["train"], rescale=1, return_library_size=True
     )
-    X = scprep.transform.sqrt(
-        X
-    )  # note that log transform is used in the examples of alra on github
-    adata.obsm[
-        "train"
-    ] = X  # we know that the scprep's .run.Rfunction uses anndata.2ri, where reducedDim(d, "PCA")=adata.obsm["X_PCA"], but that these dim-reduction equivalents
-    # do not encompass any given key hold true across all contexts. Thus, we use X_PCA for now to ensure compatability.
-    Y = _alra(X)
+
+    Y = _alra(adata)["train"]
     Y = scprep.utils.matrix_transform(Y, np.square)
     Y = scprep.utils.matrix_vector_elementwise_multiply(Y, libsize, axis=0)
     adata.obsm["denoised"] = Y
