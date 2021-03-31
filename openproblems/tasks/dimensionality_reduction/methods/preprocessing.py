96b348390d400e08a50bcb7545a7e1f5010a98b8
Luke Zappia <lazappi@users.noreply.github.com>
2021-03-31 13:53:47 +0200

    Add preprocessing.py for dim red methods
    This file should contain standard pre-processing functions used to
    create variations on different methods

---
+++ openproblems/tasks/dimensionality_reduction/methods/preprocessing.py
@@ -0,0 +1,10 @@
+import scanpy as sc
+from ....tools.normalize import log_cpm
+
+
+def preprocess_scanpy(adata):
+
+    log_cpm(adata)
+    sc.pp.highly_variable_genes(adata, n_top_genes=1000)
+    sc.tl.pca(adata, n_comps=50, svd_solver="arpack")
+    adata.obsm["X_input"] = adata.obsm["X_pca"]
