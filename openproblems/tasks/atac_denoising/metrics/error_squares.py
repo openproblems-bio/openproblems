693534fa9097e5e12a57dcb639f13edd92316951
Dominik <dominik.otto@gmail.com>
2021-03-30 23:32:42 -0700

    more general metric task-atac-denoising

--- openproblems/tasks/atac_denoising/metrics/error_squares.py
+++ openproblems/tasks/atac_denoising/metrics/error_squares.py
@@ -5,5 +5,6 @@ from sklearn.metrics import mean_squared_error
 @metric(metric_name="Mean error squares", maximize=False)
 def mean_error_squares(adata):
     return mean_squared_error(
-        adata.obsm["mode2_denoised"].data, adata.obsm["mode2"].data
+        adata.obsm["mode2_denoised"].todense(),
+        adata.obsm["mode2"].todense(),
     )
