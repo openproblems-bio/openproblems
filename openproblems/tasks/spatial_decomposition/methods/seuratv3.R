76db36c975dda4ae28f99c046d30ffdb452a40b1
Alma Andersson <kangarooblood@gmail.com>
2021-03-31 18:09:51 +0200

    Seurat (#8)
    Co-authored-by: almaan <almaan@kth.se>

---
+++ openproblems/tasks/spatial_decomposition/methods/seuratv3.R
@@ -0,0 +1,45 @@
+library(Seurat)
+
+# R base for seuratv3.py
+sce <- as.Seurat(sce, counts = "X", data = NULL)
+
+# extract single cell reference data
+sce_sc <- sce[, sce@meta.data$modality == "sc"]
+sce_sp <- sce[, sce@meta.data$modality == "sp"]
+
+# Normalize and do dimred for spatial data
+sce_sp <- SCTransform(sce_sp, assay = "RNA", verbose = FALSE)
+
+sce_sp <- RunPCA(sce_sp, assay = "SCT", verbose = FALSE)
+sce_sp <- RunUMAP(sce_sp, reduction = "pca", dims = 1:30)
+
+# Normalize and do dimred for single cell data
+sce_sc <- SCTransform(sce_sc, ncells = 3000, verbose = FALSE)
+sce_sc <- RunPCA(sce_sc, verbose = FALSE)
+sce_sc <- RunUMAP(sce_sc, dims = 1:30)
+
+# find anchors (MNN's to compute adjustmen vectors)
+anchors <- FindTransferAnchors(
+  reference = sce_sc,
+  query = sce_sp,
+  normalization.method = "SCT"
+)
+
+# transfer labels from single cell data to spatial
+predictions_assay <- TransferData(
+  anchorset = anchors,
+  refdata = as.factor(as.character(sce_sc@meta.data$label)),
+  prediction.assay = TRUE,
+  weight.reduction = sce_sp[["pca"]]
+)
+
+# format data and return results
+predictions <- GetAssayData(predictions_assay, slot = "data")
+predictions <- predictions[!(rownames(predictions) == "max"), ]
+rownames(predictions) <- paste0("xCT_", rownames(predictions))
+colnames(predictions) <- colnames(sce_sp)
+predictions <- as.data.frame(t(predictions))
+sce_sp@meta.data <- cbind(sce_sp@meta.data, predictions)
+sce <- as.SingleCellExperiment(sce_sp)
+
+sce
