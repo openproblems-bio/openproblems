45763153edc5dc8640a701ce065bcfc4978d80e5
Dominik <dominik.otto@izi.fraunhofer.de>
2021-04-01 12:53:59 -0700

    use openproblems naming convention

--- openproblems/tasks/atac_denoising/metrics/mse.py
+++ openproblems/tasks/atac_denoising/metrics/mse.py
@@ -2,8 +2,8 @@ from ....tools.decorators import metric
 from sklearn.metrics import mean_squared_error
 
 
-@metric(metric_name="Mean error squares", maximize=False)
-def mean_error_squares(adata):
+@metric(metric_name="Mean-squared error", maximize=False)
+def mse(adata):
     return mean_squared_error(
         adata.obsm["mode2_denoised"].todense(),
         adata.obsm["mode2"].todense(),
