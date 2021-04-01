3915798dbf368fef958962b37b9cf5e81e14ea35
Nikolay Markov <nikolai.markov@icloud.com>
2021-04-01 15:38:55 -0500

    Remove zebrafish random dataset

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -4,4 +4,3 @@ from .pancreas import pancreas_batch
 from .pancreas import pancreas_random
 from .pancreas import pancreas_random_label_noise
 from .zebrafish import zebrafish_labels
-from .zebrafish import zebrafish_random
--- openproblems/tasks/label_projection/datasets/zebrafish.py
+++ openproblems/tasks/label_projection/datasets/zebrafish.py
@@ -1,8 +1,6 @@
 from ....data.zebrafish import load_zebrafish
 from ....tools.decorators import dataset
 
-import numpy as np
-
 
 @dataset("Zebrafish (by labels)")
 def zebrafish_labels(test=False):
@@ -10,15 +8,4 @@ def zebrafish_labels(test=False):
     adata.obs["labels"] = adata.obs["cell_type"]
     adata.obs["batch"] = adata.obs["lab"]
     adata.obs["is_train"] = adata.obs["lab"] == adata.obs["lab"][0]
-    return adata
-
-
-@dataset("Zebrafish (random split)")
-def zebrafish_random(test=False):
-    adata = load_zebrafish(test=test)
-    adata.obs["labels"] = adata.obs["cell_type"]
-    adata.obs["batch"] = adata.obs["lab"]
-    adata.obs["is_train"] = np.random.choice(
-        [True, False], adata.shape[0], replace=True
-    )
     return adata
