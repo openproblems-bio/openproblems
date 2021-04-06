9f2b99e8962bce1b38170d901c9e4e95b14e038e
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-06 20:05:15 +0200

    clean up cbmc script
    Former-commit-id: b34b28d8f811a7271c6ad5fd59877507c620d06a

--- src/modality_alignment/datasets/citeseq_cbmc/script.py
+++ src/modality_alignment/datasets/citeseq_cbmc/script.py
@@ -1,24 +1,31 @@
 ## VIASH START
+# The code between the 'VIASH START' and 'VIASH END' gets stripped away before 
+# execution. Here you can put anything that helps the prototyping of your script.
 par = {
     "input_rna": "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE100866&format=file&file=GSE100866%5FCBMC%5F8K%5F13AB%5F10X%2DRNA%5Fumi%2Ecsv%2Egz",
     "input_adt": "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE100866&format=file&file=GSE100866%5FCBMC%5F8K%5F13AB%5F10X%2DADT%5Fumi%2Ecsv%2Egz",
     "output": "output.h5ad",
     "test": False
 }
-resources_dir = "..."
+resources_dir = "../../resources/utils.py"
 ## VIASH END
 
-print("Running imports")
+print("Importing libraries")
+import scprep
+
+# adding resources dir to system path
 import sys
 sys.path.append(resources_dir)
+
+# importing helper functions from common utils.py file in resources dir
 from utils import create_joint_adata
 from utils import filter_joint_data_empty_cells
 from utils import subset_joint_data
-import scprep
 
 print("(Down)loading expression datasets from GEO")
 sys.stdout.flush()
 
+# par["input_rna"] can be the path to a local file, or a url
 rna_data = scprep.io.load_csv(
     par["input_rna"], cell_axis="col", compression="gzip", sparse=True, chunksize=1000
 )
