6f0d17bdc76e9050ce9164ca6586ab1c502f7dea
Daniel Burkhardt <burkhardt.d.b@gmail.com>
2021-04-07 14:38:04 -0400

    Update preprocessing
    Former-commit-id: 2799e7a97148443f412c09ae182b16da8a66ca53

--- src/modality_alignment/resources/preprocessing.py
+++ src/modality_alignment/resources/preprocessing.py
@@ -1,41 +1,51 @@
+import anndata
+import functools
 import scanpy as sc
 import scprep
 
-#_scran = scprep.run.RFunction(
-#    setup="library('scran')",
-#    args="sce, min.mean=0.1",
-#    body="""
-#    sce <- computeSumFactors(sce, min.mean=min.mean, assay.type="X")
-#    sizeFactors(sce)
-#    """,
-#)
-#
-#
-#def log_scran_pooling(adata):
-#    """Normalize data with scran via rpy2."""
-#    scprep.run.install_bioconductor("scran")
-#    adata.obs["size_factors"] = _scran(adata)
-#    adata.X = scprep.utils.matrix_vector_elementwise_multiply(
-#        adata.X, adata.obs["size_factors"], axis=0
-#    )
-#    sc.pp.log1p(adata)
+def normalizer(func, *args, **kwargs):
+    """Decorate a normalization function."""
+
+    @functools.wraps(func)
+    def normalize(adata, *args, obsm=None, obs=None, var=None, **kwargs):
+        # log.debug("Running {} normalization".format(func.__name__))
+        assert isinstance(adata, anndata.AnnData)
+
+        if obsm is not None:
+            cache_name = "{}_{}".format(obsm, func.__name__)
+            if cache_name in adata.obsm:
+                adata.obsm[obsm] = adata.obsm[cache_name]
+            else:
+                obs = adata.uns[obs] if obs else adata.obs
+                var = adata.uns[var] if var else adata.var
+                adata_temp = anndata.AnnData(adata.obsm[obsm], obs=obs, var=var)
+                func(adata_temp, *args, **kwargs)
+                adata.obsm[obsm] = adata.obsm[cache_name] = adata_temp.X
+        else:
+            if func.__name__ in adata.layers:
+                adata.X = adata.layers[func.__name__]
+            else:
+                func(adata, *args, **kwargs)
+                adata.layers[func.__name__] = adata.X
+
+    return normalize
 
 def _cpm(adata):
     adata.layers["counts"] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=1e6, key_added="size_factors")
 
-
+@normalizer
 def cpm(adata):
     """Normalize data to counts per million."""
     _cpm(adata)
 
-
+@normalizer
 def log_cpm(adata):
     """Normalize data to log counts per million."""
     _cpm(adata)
     sc.pp.log1p(adata)
 
-
+@normalizer
 def sqrt_cpm(adata):
     """Normalize data to sqrt counts per million."""
     _cpm(adata)
