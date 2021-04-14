93dc754fa7bf09b8fbc4fb470e557a50c14a6886
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-14 16:55:59 +0200

    refactor extract scores
    Former-commit-id: fc0ed288e74761922fef6aca3fdce0fbec494bd1

--- src/utils/extract_scores/script.R
+++ src/utils/extract_scores/script.R
@@ -1,19 +1,27 @@
 ## VIASH START
 par <- list(
-  input = list.files("out_bash/modality_alignment/metrics/", full.names = TRUE),
+  input = list.files("work", full.names = TRUE, pattern = "*.h5ad"),
   output = "out_bash/modality_alignment/scores.tsv"
 )
-inp <- par$input[[1]]
+inp <- par$input[[2]]
 ## VIASH END
 
 cat("Loading dependencies\n")
 library(anndata, warn.conflicts = FALSE)
 options(tidyverse.quiet = TRUE)
 library(tidyverse)
+library(assertthat)
 
 cat("Reading input h5ad files")
 scores <- map_df(par$input, function(inp) {
+  cat("Reading '", inp, "'\n", sep = "")
   ad <- read_h5ad(inp)
+
+  assert_that("dataset_name" %in% names(ad$uns))
+  assert_that("method_name" %in% names(ad$uns))
+  assert_that("metric_name" %in% names(ad$uns))
+  assert_that("metric_value" %in% names(ad$uns))
+
   as_tibble(ad$uns[c("dataset_name", "method_name", "metric_name", "metric_value")])
 })
 
