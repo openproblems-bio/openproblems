90a196989f5138b3f24530dfeb39d479fa3573f9
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-14 16:55:50 +0200

    refactor scot
    Former-commit-id: b1a1148f0ca1768711331149e50662e9a86d4e62

--- src/modality_alignment/methods/scot/scot.py
+++ src/modality_alignment/methods/scot/scot.py
@@ -8,43 +8,46 @@ par = {
 resources_dir = "../../resources/utils.py"
 ## VIASH END
 
+print("Loading dependencies")
 import scanpy as sc
 import sklearn.decomposition
-import sys
-sys.path.append(resources_dir)
+from SCOT import SCOT
 
 # importing helper functions from common preprocessing.py file in resources dir
+import sys
+sys.path.append(resources_dir)
 from preprocessing import log_cpm
 from preprocessing import sqrt_cpm
 
-def _scot(adata, n_svd=100, balanced=False):
-    from SCOT import SCOT
 
-    # PCA reduction
-    n_svd = min([n_svd, min(adata.X.shape) - 1, min(adata.obsm["mode2"].shape) - 1])
-    X_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata.X)
-    Y_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata.obsm["mode2"])
+print("Reading input h5ad file")
+adata = sc.read_h5ad(par["input"])
 
-    # Initialize SCOT
-    scot = SCOT(X_pca, Y_pca)
+print("Normalising mode 1")
+sqrt_cpm(adata)
 
-    # call the unbalanced alignment
-    # From https://github.com/rsinghlab/SCOT/blob/master/examples/unbalanced_GW_SNAREseq.ipynb # noqa: 501
-    X_new_unbal, y_new_unbal = scot.align(
-        k=50, e=1e-3, rho=0.0005, normalize=True, balanced=balanced
-    )
-    adata.obsm["aligned"] = X_new_unbal
-    adata.obsm["mode2_aligned"] = y_new_unbal
+print("Normalising mode 2")
+log_cpm(adata, obsm="mode2", obs="mode2_obs", var="mode2_var")
 
-    return adata
 
-if __name__ == "__main__":
-    adata = sc.read_h5ad(par["input"])
-    # Normalize mode1
-    sqrt_cpm(adata)
-    # Normalize mode2
-    log_cpm(adata, obsm="mode2", obs="mode2_obs", var="mode2_var")
-    # run scot
-    _scot(adata, n_svd=par["n_svd"], balanced=par["balanced"])
-    # Write output to file
-    adata.write_h5ad(par["output"], compression=9)
+print("Performing PCA reduction")
+n_svd = min([par["n_svd"], min(adata.X.shape) - 1, min(adata.obsm["mode2"].shape) - 1])
+X_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata.X)
+Y_pca = sklearn.decomposition.TruncatedSVD(n_svd).fit_transform(adata.obsm["mode2"])
+
+print("Initialize SCOT")
+scot = SCOT(X_pca, Y_pca)
+
+print("Call the unbalanced alignment")
+# From https://github.com/rsinghlab/SCOT/blob/master/examples/unbalanced_GW_SNAREseq.ipynb # noqa: 501
+X_new_unbal, y_new_unbal = scot.align(
+    k=50, e=1e-3, rho=0.0005, normalize=True, balanced=par["balanced"]
+)
+
+print()
+adata.obsm["aligned"] = X_new_unbal
+adata.obsm["mode2_aligned"] = y_new_unbal
+
+print("Write output to file")
+adata.uns["method_name"] = "scot"
+adata.write(par["output"], compression="gzip")
