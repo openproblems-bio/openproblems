3dc8d68cca3afad03f21647f09f1320748465954
Robrecht Cannoodt <rcannood@gmail.com>
2021-04-26 21:29:50 +0200

    add sample component
    Former-commit-id: b917f59cb143055a330b3e4a7e31d6853c0a95d8

---
+++ src/modality_alignment/datasets/sample_dataset/test.py
@@ -0,0 +1,30 @@
+import os
+from os import path
+import subprocess
+
+import scanpy as sc
+import pandas
+import numpy as np
+
+print(">> Running sample_dataset")
+
+out = subprocess.check_output([
+    "./sample_dataset",
+    "--output", "output.h5ad"
+]).decode("utf-8")
+
+print(">> Checking whether file exists")
+assert path.exists("output.h5ad")
+
+print(">> Check that dataset output fits expected API")
+adata = sc.read_h5ad("output.h5ad")
+assert "mode2" in adata.obsm
+assert "mode2_obs" in adata.uns
+assert "mode2_var" in adata.uns
+assert np.all(adata.obs.index == adata.uns["mode2_obs"])
+assert len(adata.uns["mode2_var"]) == adata.obsm["mode2"].shape[1]
+
+# check dataset id
+assert "dataset_id" in adata.uns
+
+print(">> All tests passed successfully")
