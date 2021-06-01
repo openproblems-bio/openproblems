27b5a9b1694f1590de92f6686e091d864a435943
Robrecht Cannoodt <rcannood@gmail.com>
2021-06-01 12:47:14 +0200

    fix config orders
    Former-commit-id: ea8d592674b5892144ad549ab14fa99d7d16e866

--- src/modality_alignment/workflows/run_bash.sh
+++ src/modality_alignment/workflows/run_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run this prior to executing this script:
-# bin/project_build -q 'modality_alignment|utils'
+# bin/viash_build -q 'modality_alignment|utils'
 
 # get the root of the directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
--- src/modality_alignment/workflows/run_nextflow.sh
+++ src/modality_alignment/workflows/run_nextflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run this prior to executing this script:
-# bin/project_build -q 'modality_alignment|utils'
+# bin/viash_build -q 'modality_alignment|utils'
 
 # get the root of the directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
@@ -15,6 +15,8 @@ export NXF_VER=21.04.1
 bin/nextflow \
   run . \
   -main-script src/modality_alignment/workflows/main.nf \
-  -resume \
+  -c src/modality_alignment/workflows/nextflow.config \
   --output output/modality_alignment
+  
+# -resume \
 
