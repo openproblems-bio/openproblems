9830586f120d7440c0044a65e7e158c5e8ba6a47
Michaela Mueller <mumichae@in.tum.de>
2021-07-20 22:22:34 +0200

    added asw_label
    Former-commit-id: 991dc274080eebdd45bf5922d2a2f64f9a39ced0

---
+++ src/batch_integration/embedding/metrics/asw_label/test.py
@@ -0,0 +1,30 @@
+from os import path
+import subprocess
+import pandas as pd
+import numpy as np
+
+np.random.seed(42)
+
+metric = 'asw_label'
+metric_file = metric + '.tsv'
+
+print(">> Running script")
+out = subprocess.check_output([
+    "./" + metric,
+    "--adata", 'pancreas_mnn.h5ad',
+    "--output", metric_file
+]).decode("utf-8")
+
+print(">> Checking whether file exists")
+assert path.exists(metric_file)
+
+print(">> Check that score makes sense")
+result = pd.read_table(metric_file)
+assert result.shape == (1, 4)
+score = result.loc[0, 'value']
+print(score)
+
+assert 0 < score < 1
+assert score == 0.6171182170510292
+
+print(">> All tests passed successfully")
