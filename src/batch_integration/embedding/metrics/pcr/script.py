1a99227c20369f7f8ce1f112890b7063ce7ae2dd
Michaela Mueller <mumichae@in.tum.de>
2021-07-20 23:30:49 +0200

    added PCR
    Former-commit-id: 245778883f6f91a409757bcf448f1333118a9b69

---
+++ src/batch_integration/embedding/metrics/pcr/script.py
@@ -0,0 +1,41 @@
+## VIASH START
+par = {
+    'adata': './src/batch_integration/embedding/resources/mnn_pancreas.h5ad',
+    'output': './src/batch_integration/embedding/resources/cc_score_pancreas_mnn.tsv',
+    'debug': True
+}
+## VIASH END
+
+print('Importing libraries')
+import pprint
+import scanpy as sc
+from scIB.metrics import pcr_comparison
+
+if par['debug']:
+    pprint.pprint(par)
+
+OUTPUT_TYPE = 'embedding'
+METRIC = 'pcr'
+
+adata_file = par['adata']
+output = par['output']
+
+print('Read adata')
+adata = sc.read(adata_file)
+adata_int = adata.copy()
+name = adata.uns['name']
+
+print('compute score')
+score = pcr_comparison(
+    adata,
+    adata_int,
+    embed='X_emb',
+    covariate='batch',
+    verbose=False
+)
+
+with open(output, 'w') as file:
+    header = ['dataset', 'output_type', 'metric', 'value']
+    entry = [name, OUTPUT_TYPE, METRIC, score]
+    file.write('\t'.join(header) + '\n')
+    file.write('\t'.join([str(x) for x in entry]))
