d45bba062c91d07c87edbf156c85e84be038dbad
Michaela Mueller <mumichae@in.tum.de>
2021-12-16 20:30:39 +0100

    add Combat
    Former-commit-id: 54f55e81629e70ddb5091a89c5b9a94f603120c7

---
+++ src/batch_integration/graph/methods/combat/script.py
@@ -0,0 +1,54 @@
+## VIASH START
+par = {
+    'adata': './src/batch_integration/resources/datasets_pancreas.h5ad',
+    'output': './src/batch_integration/resources/pancreas_bbknn.h5ad',
+    'hvg': True,
+    'scaling': True,
+    'debug': True
+}
+## VIASH END
+
+print('Importing libraries')
+from pprint import pprint
+import scanpy as sc
+from scib.integration import combat
+
+if par['debug']:
+    pprint(par)
+
+adata_file = par['adata']
+output = par['output']
+hvg = par['hvg']
+scaling = par['scaling']
+
+print('Read adata')
+adata = sc.read(adata_file)
+
+if hvg:
+    print('Select HVGs')
+    adata = adata[:, adata.var['highly_variable']]
+
+if scaling:
+    print('Scale')
+    adata.X = adata.layers['logcounts_scaled']
+else:
+    adata.X = adata.layers['logcounts']
+
+print('Integrate')
+adata = combat(adata, batch='batch')
+
+print('Postprocess data')
+sc.pp.pca(
+    adata,
+    n_comps=50,
+    use_highly_variable=True,
+    svd_solver='arpack',
+    return_info=True
+)
+sc.pp.neighbors(adata, use_rep='X_pca')
+
+print('Save HDF5')
+adata.uns['hvg'] = hvg
+adata.uns['scaled'] = scaling
+
+adata.write(output, compression='gzip')
