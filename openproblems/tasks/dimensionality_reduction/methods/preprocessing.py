c83789f4d2aec63984aac7e916b304fcf654305b
Luke Zappia <lazappi@users.noreply.github.com>
2022-03-07 17:20:32 +0100

    Subset object in preprocess_logCPM_1kHVG()

--- openproblems/tasks/dimensionality_reduction/methods/preprocessing.py
+++ openproblems/tasks/dimensionality_reduction/methods/preprocessing.py
@@ -8,5 +8,6 @@ def preprocess_logCPM_1kHVG(adata: AnnData) -> None:
 
     log_cpm(adata)
     sc.pp.highly_variable_genes(adata, n_top_genes=1000)
+    adata = adata[:, adata.var["highly_variable"]]
     sc.tl.pca(adata, n_comps=50, svd_solver="arpack")
     adata.obsm["X_input"] = adata.obsm["X_pca"]
