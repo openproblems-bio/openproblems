8d174ab44cc918c87a25fa7017b106aec915aa1a
Daniel Strobl <danielstrobl@gmail.com>
2022-03-09 21:52:31 +0100

    scib api change opt_louvain

--- openproblems/tasks/_batch_integration/batch_integration_graph/metrics/nmi.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/metrics/nmi.py
@@ -7,7 +7,7 @@ from .....tools.decorators import metric
     image="openproblems-python-batch-integration",  # only if required
 )
 def nmi(adata):
-    from scib.clustering import opt_louvain
+    from scib.metrics.clustering import opt_louvain
     from scib.metrics import nmi
 
     opt_louvain(
