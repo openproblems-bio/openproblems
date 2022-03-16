e16df05cc2f93ddf5f17bf3f8b6f3189d36961cb
Luke Zappia <lazappi@users.noreply.github.com>
2022-03-16 15:22:39 +0100

    Jamboree dimensionality reduction metrics (#317)
    * Add skeleton methods
    
    * Implement metrics
    
    * Return min. values if invalid emb
    
    * Add method skeleton
    
    * Implement method
    
    * Explicitly add umap-learn
    
    * Use different way, add test
    
    * Add license from original UMAP
    
    * Add license, preprocessning, fix types
    
    * Fix preprocessing
    
    * pre-commit
    
    Co-authored-by: Michal Klein <michal.klein@protonmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
    Former-commit-id: 9ea9416a9c2ad25e2afacce93b5f2923a487cf1f

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -75,3 +75,43 @@ def test_trustworthiness_sparse(tempdir, image):
 
     assert isinstance(m, float)
     assert 0 <= m <= 1
+
+
+@parameterized.parameterized.expand(
+    [
+        (
+            utils.TEMPDIR.name,
+            openproblems.tasks.dimensionality_reduction.metrics.density_preservation.metadata[  # noqa: E501
+                "image"
+            ],
+        )
+    ],
+    name_func=utils.name.name_test,
+)
+@utils.docker.docker_test
+def test_density_preservation_matches_densmap(tempdir, image):
+    from openproblems.tasks.dimensionality_reduction.metrics.density import _K
+    from openproblems.tasks.dimensionality_reduction.metrics.density import _SEED
+    from scipy.stats import pearsonr
+    from umap import UMAP
+
+    import numpy as np
+
+    task = openproblems.tasks.dimensionality_reduction
+    metric = openproblems.tasks.dimensionality_reduction.metrics.density_preservation
+
+    adata = task.api.sample_dataset()
+    adata = task.api.sample_method(adata)
+    openproblems.log.debug(
+        "Testing {} metric from {} task".format(metric.__name__, task.__name__)
+    )
+
+    (emb, ro, re) = UMAP(
+        n_neighbors=_K, random_state=_SEED, densmap=True, output_dens=True
+    ).fit_transform(adata.X)
+    expected = pearsonr(ro, re)[0]
+
+    adata.obsm["X_emb"] = emb
+    actual = metric(adata)
+
+    np.testing.assert_allclose(expected, actual)
