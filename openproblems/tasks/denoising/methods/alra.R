718dd938046437dcfff42a4af936ffc7d8e37655
Scott Gigante <scott.gigante@immunai.com>
2022-03-17 12:52:38 +0200

    split up alra oneliner

--- openproblems/tasks/denoising/methods/alra.R
+++ openproblems/tasks/denoising/methods/alra.R
@@ -5,10 +5,12 @@ library(rsvd)
 
 # save the matrix of the obsm variable train
 
-train_matrix <- reducedDim(sce, "train")
+train_matrix <- t(as.matrix(reducedDim(sce, "train")))
 
-# this is the format used in the alraSeurat2.R example on KlugerLab/ALRA/ repo
-reducedDim(sce, "train") <- Matrix(t(alra(t(as.matrix(train_matrix)))[[3]]))
+alra_output <- alra(train_matrix)
+alra_mat <- alra_output$A_norm_rank_k_cor_sc
+
+reducedDim(sce, "train") <- Matrix(t(alra_mat))
 
 # return
 
