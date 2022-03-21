624ed58dcc848769c90f22633bbc2aff55cecda1
Daniel Strobl <danielstrobl@gmail.com>
2022-03-21 21:00:17 +0100

    isort next try

--- openproblems/tasks/_batch_integration/batch_integration_graph/metrics/nmi.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/metrics/nmi.py
@@ -7,9 +7,8 @@ from .....tools.decorators import metric
     image="openproblems-python-batch-integration",  # only if required
 )
 def nmi(adata):
-    from scib.metrics.clustering import opt_louvain
-
-    from scib.metrics import nmi  # isort:skip
+    from scib.metrics.clustering import opt_louvain # isort:skip
+    from scib.metrics import nmi
 
     opt_louvain(
         adata,
