2092d72b2d03bc5c8f1e45726a0362b873782456
Daniel Strobl <danielstrobl@gmail.com>
2022-03-21 14:50:44 +0100

    fastmnn dgcmatrix output

--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/fastmnn_feature.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/fastmnn_feature.py
@@ -11,13 +11,13 @@ _fastmnn_feature = scprep.run.RFunction(
     setup="""
             library(SingleCellExperiment)
             library(batchelor)
+            library(Seurat)
         """,
     args="sobj, batch, hvg=2000",
     body="""
         expr <- assay(sobj, 'counts')
         sce <- fastMNN(expr, batch = colData(sobj)[[batch]])
-        assay(sobj, "counts") <- assay(sce, "reconstructed")
-        sobj
+        as(assay(sce, "reconstructed"), "dgCMatrix")
         """,
 )
 
@@ -34,7 +34,7 @@ _fastmnn_feature = scprep.run.RFunction(
 def fastmnn_feature_full_unscaled(adata):
     from scib.preprocessing import reduce_data
 
-    adata = _fastmnn_feature(adata, "batch")
+    adata.X = _fastmnn_feature(adata, "batch")
     reduce_data(adata, umap=False)
     # Complete the result in-place
     return adata
@@ -54,7 +54,7 @@ def fastmnn_feature_hvg_unscaled(adata):
     from scib.preprocessing import reduce_data
 
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
-    adata = _fastmnn_feature(adata, "batch")
+    adata.X = _fastmnn_feature(adata, "batch")
     reduce_data(adata, umap=False)
     return adata
 
@@ -75,7 +75,7 @@ def fastmnn_feature_hvg_scaled(adata):
 
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
     adata = scale_batch(adata, "batch")
-    adata = _fastmnn_feature(adata, "batch")
+    adata.X = _fastmnn_feature(adata, "batch")
     reduce_data(adata, umap=False)
     return adata
 
@@ -94,6 +94,6 @@ def fastmnn_feature_full_scaled(adata):
     from scib.preprocessing import reduce_data
 
     adata = scale_batch(adata, "batch")
-    adata = _fastmnn_feature(adata, "batch")
+    adata.X = _fastmnn_feature(adata, "batch")
     reduce_data(adata, umap=False)
     return adata
