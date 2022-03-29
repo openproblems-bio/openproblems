428b4c4e8172b7dc9bc1a6bb9ad0ce10d0e70d0f
Scott Gigante <scott.gigante@immunai.com>
2022-03-29 18:05:13 -0400

    more tests for the CLI

--- test/test_cli.py
+++ test/test_cli.py
@@ -108,6 +108,21 @@ def test_image_metrics(task, metric):
     _test_image(task, "--metrics", metric)
 
 
+def test_hash_basic():
+    assert main(["--test-hash"]) is None
+
+
+def test_version():
+    assert main(["--version"]) == openproblems.__version__
+
+
+def test_help(capsys):
+    assert main([]) is None
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert "Open Problems for Single Cell Analysis command-line interface" in captured.out
+
+
 @parameterized.parameterized.expand(
     [
         ("label_projection", "--datasets", "pancreas_batch"),
