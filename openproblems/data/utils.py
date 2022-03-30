2e5254e3193748bf79a04376eb4a59c047123fef
Scott Gigante <scott.gigante@immunai.com>
2022-03-30 11:12:00 -0400

    store adata.X in adata.layers['counts']

--- openproblems/data/utils.py
+++ openproblems/data/utils.py
@@ -48,6 +48,7 @@ def loader(func, *args, **kwargs):
         log.debug("Downloading {}({}, {}) dataset".format(func.__name__, args, kwargs))
         adata = func(*args, **kwargs)
         adata.__from_cache__ = False
+        adata.layers["counts"] = adata.X
         try:
             os.mkdir(TEMPDIR)
         except OSError:
