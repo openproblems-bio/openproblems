45864a5615dc13b86c25dbcb77454e319f2098bc
Nikolay Markov <nikolai.markov@icloud.com>
2022-04-01 21:08:30 -0500

    Revert "Remove zebrafish random dataset"
    This reverts commit 3915798dbf368fef958962b37b9cf5e81e14ea35.

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -4,3 +4,4 @@ from .pancreas import pancreas_batch
 from .pancreas import pancreas_random
 from .pancreas import pancreas_random_label_noise
 from .zebrafish import zebrafish_labels
+from .zebrafish import zebrafish_random
