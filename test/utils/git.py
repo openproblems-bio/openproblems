f6b6abb9e828b9588f693c2fcaa8796e07dbf4f0
Scott Gigante <scott.gigante@immunai.com>
2022-04-06 22:10:59 -0400

    make sure we fetch the base repo to compare against

--- test/utils/git.py
+++ test/utils/git.py
@@ -5,7 +5,7 @@ import openproblems
 import os
 
 
-def git_file_age(filename, branch=None):
+def git_file_age(filename):
     """Get the age of a file's last git commit."""
     git_age = (
         run.run(
@@ -21,27 +21,54 @@ def git_file_age(filename, branch=None):
         return int(git_age)
 
 
+@functools.lru_cache(None)
+def git_setup_remote_base():
+    """Set up the `base` remote to point to the main repo."""
+    remotes = run.run(["git", "remote"], return_stdout=True).strip().split("\n")
+    if "base" not in remotes:
+        run.run(
+            [
+                "git",
+                "remote",
+                "add",
+                "base",
+                "https://github.com/openproblems-bio/openproblems",
+            ],
+            return_stdout=True,
+        )
+    run.run(["git", "fetch", "base", "main"])
+
+
 def git_has_diff(filename):
+    """Returns true if the filename has changed relative to base/main."""
     if isinstance(filename, str):
         filename = [filename]
     diffstat = run.run(
-        ["git", "diff", "origin/main", "--shortstat", "--"] + filename,
+        ["git", "diff", "base/main", "--shortstat", "--"] + filename,
         return_stdout=True,
     )
     return len(diffstat) > 0
 
 
 def task_modified(task):
+    """Check if the task has changed relative to base/main."""
     task_dir = os.path.dirname(task.__file__)
     return git_has_diff(task_dir)
 
 
 def core_modified():
+    """Check if anything outside `tasks` has changed relative to base/main."""
     return git_has_diff([".", ":^openproblems/tasks"])
 
 
 @functools.lru_cache(None)
 def list_modified_tasks():
+    """List tasks for which testing must be run.
+
+    Return all tasks if the core repo has changed,
+    otherwise just those that have changed relative to base/main.
+    """
+    git_setup_remote_base()
     if core_modified():
         return openproblems.TASKS
 
