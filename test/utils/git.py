260081064e030e859ba51bf2eacf096f955b1bdc
Scott Gigante <scott.gigante@immunai.com>
2022-04-07 09:19:00 -0400

    test everything on base/main HEAD

--- test/utils/git.py
+++ test/utils/git.py
@@ -43,6 +43,7 @@ def git_has_diff(filename):
     """Returns true if the filename has changed relative to base/main."""
     if isinstance(filename, str):
         filename = [filename]
+    git_setup_remote_base()
     diffstat = run.run(
         ["git", "diff", "base/main", "--shortstat", "--"] + filename,
         return_stdout=True,
@@ -69,15 +70,30 @@ def core_modified():
     return git_has_diff([".", ":^*.md", ":^website"] + task_exclusions)
 
 
+def git_rev_parse(branch):
+    """Get the current commit of a branch"""
+    return run.run(
+        ["git", "rev-parse", branch],
+        return_stdout=True,
+    ).strip()
+
+
+def is_main_head():
+    """Check if we are currently on base/main"""
+    git_setup_remote_base()
+    return git_rev_parse("base/main") == git_rev_parse("HEAD")
+
+
 @functools.lru_cache(None)
 def list_modified_tasks():
     """List tasks for which testing must be run.
 
     Return all tasks if the core repo has changed,
     otherwise just those that have changed relative to base/main.
+
+    If we are currently at the HEAD of base/main, test all tasks.
     """
-    git_setup_remote_base()
-    if core_modified():
+    if core_modified() or is_main_head():
         return openproblems.TASKS
 
     return [task for task in openproblems.TASKS if task_modified(task)]
