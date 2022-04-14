d72be16c80f2e8808d26165df8688481eda1ddfc
Scott Gigante <scott.gigante@immunai.com>
2022-04-14 10:03:17 -0400

    exclude unbalanced SCOT for now

--- openproblems/tasks/multimodal_data_integration/methods/__init__.py
+++ openproblems/tasks/multimodal_data_integration/methods/__init__.py
@@ -4,6 +4,4 @@ from .mnn import mnn_log_cpm
 from .mnn import mnn_log_scran_pooling
 from .procrustes import procrustes
 from .scot import scot_log_scran_pooling_balanced
-from .scot import scot_log_scran_pooling_unbalanced
 from .scot import scot_sqrt_cpm_balanced
-from .scot import scot_sqrt_cpm_unbalanced
