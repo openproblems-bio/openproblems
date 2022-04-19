3104532e872d8b228481aeb1c6084a43bb177b22
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-04-19 18:27:09 -0400

    simplify ValueError to avoid errors

--- openproblems/tasks/denoising/methods/alra.py
+++ openproblems/tasks/denoising/methods/alra.py
@@ -36,6 +36,5 @@ def alra(adata, test=False):
     Y = scprep.utils.matrix_vector_elementwise_multiply(Y, libsize, axis=0)
     adata.obsm["denoised"] = Y.tocsr()
     if np.shape(adata.obsm["train"]) != np.shape(Y.tocsr()):
-        raise ValueError(('The dimensions are not the same. Train is of shape' + str(np.shape(adata.obsm["train"])) + ' and denoised is of shape ' + str(np.shape(Y.tocsr())))
-
+        raise ValueError(('The dimensions are not the same.'))
     return adata
