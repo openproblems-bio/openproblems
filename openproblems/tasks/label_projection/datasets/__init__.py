780881cbe4c436b3395700d813b59e1798a854d4
Nikolay Markov <nikolai.markov@icloud.com>
2022-04-21 14:49:13 -0500

    Add tabula_muris_senis_lung_random dataset to label_projection

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -5,3 +5,4 @@ from .pancreas import pancreas_random
 from .pancreas import pancreas_random_label_noise
 from .zebrafish import zebrafish_labels
 from .zebrafish import zebrafish_random
+from .tabula_muris_senis import tabula_muris_senis_lung_random
