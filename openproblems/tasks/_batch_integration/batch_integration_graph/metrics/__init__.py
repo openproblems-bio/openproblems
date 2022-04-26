08bf563f8b034d664ba7870a11e3d6718a71e9d3
Daniel Strobl <danielstrobl@gmail.com>
2022-04-26 16:52:12 +0200

    stub metric

---
+++ openproblems/tasks/_batch_integration/batch_integration_graph/metrics/__init__.py
@@ -0,0 +1,1 @@
+from ari import ari
---
+++ openproblems/tasks/_batch_integration/batch_integration_graph/metrics/ari.py
@@ -0,0 +1,32 @@
+from .....tools.decorators import metric
+
+"""
+The Rand index compares the overlap of two clusterings;
+it considers both correct clustering overlaps while also counting correct
+disagreements between two clusterings.
+Similar to NMI, we compared the cell-type labels with the NMI-optimized
+Louvain clustering computed on the integrated dataset.
+The adjustment of the Rand index corrects for randomly correct labels.
+An ARI of 0 or 1 corresponds to random labeling or a perfect match, respectively.
+We also used the scikit-learn (v.0.22.1) implementation of the ARI.
+"""
+
+
+@metric(
+    metric_name="ARI",
+    maximize=True,
+    image="openproblems-python-batch-integration",  # only if required
+)
+def ari(adata):
+    from scib.metrics import ari
+    from scib.metrics.clustering import opt_louvain
+
+    opt_louvain(
+        adata,
+        label_key="labels",
+        cluster_key="cluster",
+        plot=False,
+        inplace=True,
+        force=True,
+    )
+    return ari(adata, group1="cluster", group2="labels")
