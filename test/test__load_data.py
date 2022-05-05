c6eec47d8d9938d5cdb92ddb7b8c5f85e65628a6
Scott Gigante <scott.gigante@immunai.com>
2022-05-05 12:11:06 -0400

    tighter diff for testing

--- test/test__load_data.py
+++ test/test__load_data.py
@@ -8,34 +8,36 @@ import utils.warnings
 utils.warnings.ignore_warnings()
 
 
-@parameterized.parameterized.expand(
-    [
-        (
-            task.__name__.split(".")[-1],
-            dataset.__name__,
-            test,
-            utils.TEMPDIR.name,
-            dataset.metadata["image"],
-        )
-        for task in utils.git.list_modified_tasks()
-        for dataset in task.DATASETS
-        for test in [True]
-    ],
-    name_func=utils.name.name_test,
-    skip_on_empty=True,
-)
-@utils.docker.docker_test(retries=2)
-def test_load_dataset(task_name, dataset_name, test, tempdir, image):
-    """Test loading and caching of a dataset."""
-    import utils.asserts
-    import utils.cache
+if len(utils.git.list_modified_tasks()) > 0:
 
-    task = getattr(openproblems.tasks, task_name)
-    dataset = getattr(task.datasets, dataset_name)
-    adata = dataset(test=test)
-    utils.asserts.assert_finite(adata.X)
-    utils.asserts.assert_array_equal(adata.X, adata.layers["counts"])
-    adata2 = dataset(test=test)
-    assert adata2.shape == adata.shape
-    assert adata2.__from_cache__
-    utils.cache.save(adata, tempdir, task, dataset, test=test)
+    @parameterized.parameterized.expand(
+        [
+            (
+                task.__name__.split(".")[-1],
+                dataset.__name__,
+                test,
+                utils.TEMPDIR.name,
+                dataset.metadata["image"],
+            )
+            for task in utils.git.list_modified_tasks()
+            for dataset in task.DATASETS
+            for test in [True]
+        ],
+        name_func=utils.name.name_test,
+        skip_on_empty=True,
+    )
+    @utils.docker.docker_test(retries=2)
+    def test_load_dataset(task_name, dataset_name, test, tempdir, image):
+        """Test loading and caching of a dataset."""
+        import utils.asserts
+        import utils.cache
+
+        task = getattr(openproblems.tasks, task_name)
+        dataset = getattr(task.datasets, dataset_name)
+        adata = dataset(test=test)
+        utils.asserts.assert_finite(adata.X)
+        utils.asserts.assert_array_equal(adata.X, adata.layers["counts"])
+        adata2 = dataset(test=test)
+        assert adata2.shape == adata.shape
+        assert adata2.__from_cache__
+        utils.cache.save(adata, tempdir, task, dataset, test=test)
