416023bbe0c7ee11e55e57d466c56c27a42ec38c
Scott Gigante <scott.gigante@immunai.com>
2022-05-12 15:05:40 -0400

    put everything back in

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -1,7 +1,6 @@
 from . import denoising
-
-# from . import dimensionality_reduction
-# from . import label_projection
-# from . import multimodal_data_integration
-# from . import regulatory_effect_prediction
-# from ._batch_integration import batch_integration_graph
+from . import dimensionality_reduction
+from . import label_projection
+from . import multimodal_data_integration
+from . import regulatory_effect_prediction
+from ._batch_integration import batch_integration_graph
