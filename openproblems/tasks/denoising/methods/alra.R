7c52f57039e83065ad9340f39426b2b3b2a74083
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-05-14 16:40:45 -0400

    Remove the retry from R

--- openproblems/tasks/denoising/methods/alra.R
+++ openproblems/tasks/denoising/methods/alra.R
@@ -6,20 +6,7 @@ source("https://raw.githubusercontent.com/wes-lewis/ALRA/master/alra.R")
 # fetch the matrix of the obsm variable train_norm
 train_matrix <- t(as.matrix(sce))
 
-# sometimes ALRA randomly fails with a decomposition error; retry
-alra_output <- NULL
-attempt <- 0
-while (is.null(alra_output)) {
-  if (attempt < 4) {
-    try(
-      alra_output <- alra(train_matrix)
-    )
-  } else {
-    # final attempt
-    alra_output <- alra(train_matrix)
-  }
-  attempt <- attempt + 1
-}
+alra_output <- alra(train_matrix)
 
 alra_mat <- alra_output$A_norm_rank_k_cor_sc
 
