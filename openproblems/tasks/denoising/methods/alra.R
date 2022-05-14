4153803640d76d1eaacd454243584616ecfb034e
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-05-14 14:39:21 -0400

    Retry ALRA on failure

--- openproblems/tasks/denoising/methods/alra.R
+++ openproblems/tasks/denoising/methods/alra.R
@@ -1,16 +1,23 @@
 library(SingleCellExperiment)
-library(Matrix)
+
+# we explicitly cast to as.matrix, so unless as.matrix fails we don't need
+# ALRA to check the class type of train_matrix. therefore, use fork of
+# ALRA that doesn't check type:
 source("https://raw.githubusercontent.com/wes-lewis/ALRA/master/alra.R")
-library(rsvd)
 
-# save the matrix of the obsm variable train
-
+# fetch the matrix of the obsm variable train_norm
 train_matrix <- t(as.matrix(reducedDim(sce, "train_norm")))
 
-# cast to as.matrix, so unless as.matrix fails we don't need ALRA to check
-# the class type of train_matrix. therefore, use version of ALRA that
-# doesn't check type: https://github.com/wes-lewis/ALRA/blob/master/alra.R
-alra_output <- alra(as.matrix(train_matrix))
+# sometimes ALRA randomly fails with a decomposition error; retry
+alra_output <- NULL
+attempt <- 0
+while( is.null(alra_output) && attempt <= 3 ) {
+  try(
+    alra_output <- alra(train_matrix)
+  )
+  attempt <- attempt + 1
+}
+
 alra_mat <- alra_output$A_norm_rank_k_cor_sc
 
 reducedDim(sce, "denoised") <- as.matrix(t(alra_mat))
