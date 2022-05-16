ba06cf71b564eb23823a662341055dc5ac2be231
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-05-16 15:10:42 -0400

    Fix ALRA (#353)
    * Create alra.py
    
    Add alra.py (includes existing bug).
    
    * pre-commit
    
    * import alra
    
    * pre-commit
    
    * set alra version
    
    * split up alra oneliner
    
    * debug
    
    * fix syntax error
    
    * pre-commit
    
    * use dgCMatrix
    
    * output is stored in obsm
    
    * remove prints
    
    * pre-commit
    
    * Update alra.py
    
    add to_csr() to fix coo matrix error
    
    * fix csr casting
    
    * Update alra.py
    
    try adding custom exception to catch shape mismatch from ALRA
    
    * pre-commit
    
    * add ValueError
    
    * pre-commit
    
    * simplify ValueError to avoid errors
    
    * pre-commit
    
    * cast to array for MSE
    
    Now getting an error in MSE--seems like this was already the case with earlier code, but attempting to fix regardless!
    
    * pre-commit
    
    * separate error line functions
    
    Seems something about ALRA is failing tests. Separate out obsm call to get cleaner traceback
    
    * Remove to_array()
    
    * pre-commit
    
    * try casting to a matrix one more time
    
    * notate that wes' ALRA fork must be used instead
    
    * pre-commit
    
    * source from wes' code
    
    * fix URL
    
    * shorten line lengths
    
    * Check output is ndarray
    
    * Fix typo
    
    * Return dense data
    
    * don't need tocsr now that the data is dense
    
    * Return directly to denoised
    
    * code cleanup
    
    * Revert debugging
    
    * Don't edit adata.obsm['train']
    
    * access train_norm
    
    * Add warning about editing adata.obsm['train']
    
    * pre-commit
    
    * check train and test are not modified
    
    * pre-commit
    
    * Retry ALRA on failure
    
    * pre-commit
    
    * Switch t(as.matrix()) order
    
    * Check dense data
    
    * Return sparse data
    
    * Check input data is sparse
    
    * Fix typo
    
    * pre-commit
    
    * Don't send the full AnnData to R
    
    * Expect sparse input, dense array output
    
    * train and test must be floats
    
    * Convert back to float
    
    * Fail on final attempt
    
    * put the retry inside python
    
    * Remove the retry from R
    
    * pre-commit
    
    * layers['counts'] might not be sparse
    
    * pre-commit
    
    * Log error each time
    
    * import logging
    
    * pre-commit
    
    * Better way to check matrices
    
    * pre-commit
    
    * fix array equal comparison
    
    * add explicit comment
    
    * More explicit toarray
    
    * Can't check for untouched train/test
    
    * Don't import scprep
    
    * Just use a fixed target_sum
    
    * Sample data should match API
    
    * pre-commit
    
    * flake8
    
    * no_denoising still needs to densify
    
    * convert to csc
    
    * pre-commit
    
    * Convert to csr
    
    * conversion of sparse doesn't work, try anndata
    
    * accept sce
    
    * pre-commit
    
    * Convert to dense
    
    * pre-commit
    
    * Convert to dense
    
    * pre-commit
    
    * Try `.tocsr()`
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Scott Gigante <scott.gigante@immunai.com>
    Co-authored-by: Scott Gigante <scottgigante@users.noreply.github.com>
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>

--- openproblems/tasks/denoising/methods/alra.py
+++ openproblems/tasks/denoising/methods/alra.py
@@ -1,14 +1,11 @@
 from ....tools.conversion import r_function
 from ....tools.decorators import method
 
-# should be imported by docker
-import numpy as np
-import scprep
-
-# from ....tools.utils import check_version
-
+import logging
 
 _alra = r_function("alra.R")
+
+log = logging.getLogger("openproblems")
 
 
 @method(
@@ -17,19 +14,37 @@ _alra = r_function("alra.R")
     paper_url="https://www.biorxiv.org/content/10.1101/397588v1",
     paper_year=2018,
     code_url="https://github.com/KlugerLab/ALRA",
-    # code_version=check_version("scprep"),
+    code_version="v1.0.0",
     image="openproblems-r-extras",
 )
 def alra(adata, test=False):
+    import numpy as np
+    import rpy2.rinterface_lib.embedded
+    import scprep
+
     # libsize and sqrt norm
-    # overwrite obsm['train'] with normalized version
-    adata.obsm["train"] = scprep.utils.matrix_transform(adata.obsm["train"], np.sqrt)
-    adata.obsm["train"], libsize = scprep.normalize.library_size_normalize(
-        adata.obsm["train"], rescale=1, return_library_size=True
+    adata.obsm["train_norm"] = scprep.utils.matrix_transform(
+        adata.obsm["train"], np.sqrt
     )
+    adata.obsm["train_norm"], libsize = scprep.normalize.library_size_normalize(
+        adata.obsm["train_norm"], rescale=1, return_library_size=True
+    )
+    adata.obsm["train_norm"] = adata.obsm["train_norm"].tocsr()
     # run alra
-    # _alra takes adata returns adata, edits "train"
-    Y = _alra(adata)["train"]
+    # _alra takes sparse array, returns dense array
+    Y = None
+    attempts = 0
+    while Y is None:
+        try:
+            Y = _alra(adata)
+        except rpy2.rinterface_lib.embedded.RRuntimeError as e:
+            if attempts < 10:
+                log.warning("alra.R failed")
+                log.warning(str(e))
+                attempts += 1
+            else:
+                raise
+
     # transform back into original space
     Y = scprep.utils.matrix_transform(Y, np.square)
     Y = scprep.utils.matrix_vector_elementwise_multiply(Y, libsize, axis=0)
