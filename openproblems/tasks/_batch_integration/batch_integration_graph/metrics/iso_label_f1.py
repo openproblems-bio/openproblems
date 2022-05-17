09b7cd64df2d300be67aac6d89eced1f798eed30
Daniel Strobl <danielstrobl@gmail.com>
2022-05-17 13:33:16 +0000

    line wrapping

--- openproblems/tasks/_batch_integration/batch_integration_graph/metrics/iso_label_f1.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/metrics/iso_label_f1.py
@@ -2,8 +2,9 @@ from .....tools.decorators import metric
 
 """
 We developed two isolated label scores to evaluate how well the data integration methods
-dealt with cell identity labels shared by few batches. Specifically, we identified isolated
-cell labels as the labels present in the least number of batches in the integration task.
+dealt with cell identity labels shared by few batches. Specifically, we identified
+isolated cell labels as the labels present in the least number of batches in the
+integration task.
 The score evaluates how well these isolated labels separate from other cell identities.
 We implemented the isolated label metric in two versions:
 (1) the best clustering of the isolated label (F1 score) and
@@ -15,10 +16,11 @@ The F1 score is a weighted mean of precision and recall given by the equation:
 𝐹1=2×(precision×recall)/(precision+recall).
 
 It returns a value between 0 and 1,
-where 1 shows that all of the isolated label cells and no others are captured in the cluster.
-For the isolated label ASW score, we compute the ASW of isolated versus nonisolated labels
-on the PCA embedding (ASW metric above) and scale this score to be between 0 and 1.
-The final score for each metric version consists of the mean isolated score of all isolated labels.
+where 1 shows that all of the isolated label cells and no others are captured in
+the cluster. For the isolated label ASW score, we compute the ASW of isolated
+versus nonisolated labels on the PCA embedding (ASW metric above) and scale this
+score to be between 0 and 1. The final score for each metric version consists of
+the mean isolated score of all isolated labels.
 """
 
 
