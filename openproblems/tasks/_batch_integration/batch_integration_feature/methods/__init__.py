c904d30e3efd7349a9eb95f755f561c4a6a03bbe
Daniel Strobl <danielstrobl@gmail.com>
2022-05-18 15:54:17 +0200

    initial commit

--- openproblems/tasks/_batch_integration/batch_integration_feature/methods/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_feature/methods/__init__.py
@@ -7,21 +7,10 @@ from ...batch_integration_graph.methods.combat import combat_hvg_unscaled
 # from ...batch_integration_graph.methods.desc import desc_full_unscaled
 # from ...batch_integration_graph.methods.desc import desc_hvg_scaled
 # from ...batch_integration_graph.methods.desc import desc_hvg_unscaled
-from ...batch_integration_graph.methods.fastmnn_embed import fastmnn_embed_full_scaled
-from ...batch_integration_graph.methods.fastmnn_embed import fastmnn_embed_full_unscaled
-from ...batch_integration_graph.methods.fastmnn_embed import fastmnn_embed_hvg_scaled
-from ...batch_integration_graph.methods.fastmnn_embed import fastmnn_embed_hvg_unscaled
-
 # from ...batch_integration_graph.methods.fastmnn_feature import fastmnn_feature_full_scaled
 # from ...batch_integration_graph.methods.fastmnn_feature import fastmnn_feature_full_unscaled
 # from ...batch_integration_graph.methods.fastmnn_feature import fastmnn_feature_hvg_scaled
 # from ...batch_integration_graph.methods.fastmnn_feature import fastmnn_feature_hvg_unscaled
-from ...batch_integration_graph.methods.harmony import harmony_full_scaled
-from ...batch_integration_graph.methods.harmony import harmony_full_unscaled
-from ...batch_integration_graph.methods.harmony import harmony_hvg_scaled
-from ...batch_integration_graph.methods.harmony import harmony_hvg_unscaled
-from ...batch_integration_graph.methods.liger import liger_full_unscaled
-from ...batch_integration_graph.methods.liger import liger_hvg_unscaled
 from ...batch_integration_graph.methods.mnn import mnn_full_scaled
 from ...batch_integration_graph.methods.mnn import mnn_full_unscaled
 from ...batch_integration_graph.methods.mnn import mnn_hvg_scaled
@@ -35,18 +24,6 @@ from ...batch_integration_graph.methods.mnn import mnn_hvg_unscaled
 # from ...batch_integration_graph.methods.saucie_full import saucie_feature_full_unscaled
 # from ...batch_integration_graph.methods.saucie_full import saucie_feature_hvg_scaled
 # from ...batch_integration_graph.methods.saucie_full import saucie_feature_hvg_unscaled
-from ...batch_integration_graph.methods.scanorama_embed import (
-    scanorama_embed_full_scaled,
-)
-from ...batch_integration_graph.methods.scanorama_embed import (
-    scanorama_embed_full_unscaled,
-)
-from ...batch_integration_graph.methods.scanorama_embed import (
-    scanorama_embed_hvg_scaled,
-)
-from ...batch_integration_graph.methods.scanorama_embed import (
-    scanorama_embed_hvg_unscaled,
-)
 from ...batch_integration_graph.methods.scanorama_full import (
     scanorama_feature_full_scaled,
 )
@@ -60,15 +37,11 @@ from ...batch_integration_graph.methods.scanorama_full import (
     scanorama_feature_hvg_unscaled,
 )
 
-from ...batch_integration_graph.methods.scanvi import scanvi_full_unscaled
-from ...batch_integration_graph.methods.scanvi import scanvi_hvg_unscaled
 
 # from ...batch_integration_graph.methods.scgen import scgen_full_scaled
 # from ...batch_integration_graph.methods.scgen import scgen_full_unscaled
 # from ...batch_integration_graph.methods.scgen import scgen_hvg_scaled
 # from ...batch_integration_graph.methods.scgen import scgen_hvg_unscaled
-from ...batch_integration_graph.methods.scvi import scvi_full_unscaled
-from ...batch_integration_graph.methods.scvi import scvi_hvg_unscaled
 
 # from ...batch_integration_graph.methods.seurat_full import seurat_full_scaled
 # from ...batch_integration_graph.methods.seurat_full import seurat_full_unscaled
