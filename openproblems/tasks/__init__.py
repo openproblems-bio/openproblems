d9a9df551dacab3dd62e0fef57d86483778a4acb
Daniel Strobl <danielstrobl@gmail.com>
2022-05-23 09:08:17 +0200

    batch integration embed in init py

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -1,6 +1,8 @@
+import imp
 from . import denoising
 from . import dimensionality_reduction
 from . import label_projection
 from . import multimodal_data_integration
 from . import regulatory_effect_prediction
 from ._batch_integration import batch_integration_graph
+from ._batch_integration import batch_integration_embed
