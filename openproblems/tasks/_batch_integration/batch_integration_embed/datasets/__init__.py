77dfeccc89607f3e8e3ddadb648c9c9cb7db05aa
Daniel Strobl <danielstrobl@gmail.com>
2022-05-23 10:18:25 +0200

    wrong data import

--- openproblems/tasks/_batch_integration/batch_integration_embed/datasets/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_embed/datasets/__init__.py
@@ -1,2 +1,2 @@
-from ...batch_integration_graph.datasets.pancreas import immune_batch
+from ...batch_integration_graph.datasets.immune import immune_batch
 from ...batch_integration_graph.datasets.pancreas import pancreas_batch
