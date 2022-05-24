b7501ac375b8a7334ec75f13863ceb688de27e18
Daniel Strobl <danielstrobl@gmail.com>
2022-05-24 11:41:16 +0200

    remove wrong import

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -5,5 +5,3 @@ from . import multimodal_data_integration
 from . import regulatory_effect_prediction
 from ._batch_integration import batch_integration_embed
 from ._batch_integration import batch_integration_graph
-
-import imp
