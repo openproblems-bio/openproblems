fb658790ed1b50ca125a3266cb68693690d56a89
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-05-27 17:40:13 +0200

    add example script for data loader
    Former-commit-id: 0ff175b1c67245f23300df0ec815a0cedd795afb

---
+++ src/common/data_loader/run_example.sh
@@ -0,0 +1,4 @@
+bin/viash run src/common/data_loader/config.vsh.yaml -- \
+  --output src/common/data_loader/resources/pancreas.h5ad \
+  --url https://ndownloader.figshare.com/files/24539828 \
+  --name pancreas --lognorm_available adata.X
