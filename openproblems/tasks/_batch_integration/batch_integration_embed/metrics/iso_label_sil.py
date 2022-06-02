c1c63173dce53bde16a9ee2f9f68f17ae19a69de
MalteDLuecken <m.d.luecken@gmail.com>
2022-06-02 14:45:44 +0200

    Update iso_label_sil.py
    adapt iso label ASW description

--- openproblems/tasks/_batch_integration/batch_integration_embed/metrics/iso_label_sil.py
+++ openproblems/tasks/_batch_integration/batch_integration_embed/metrics/iso_label_sil.py
@@ -1,24 +1,17 @@
 from .....tools.decorators import metric
 
 """
-We developed two isolated label scores to evaluate how well the data integration methods
-dealt with cell identity labels shared by few batches. Specifically, we identified
-isolated cell labels as the labels present in the least number of batches in the
-integration task. The score evaluates how well these isolated labels separate from other
-cell identities.
+In the scIB package two isolated label scores are available. These metrics evaluate how 
+well the data integration methods dealt with cell identity labels shared by few batches. 
+Specifically, isolated cell labels are defined as the labels present in the least number
+ of batches in the integration task. The score evaluates how well these isolated labels 
+separate from other cell identities.
 
-We implemented the isolated label metric in two versions: (1) the best clustering of the
-isolated label (F1 score) and (2) the global ASW of the isolated label. For the cluster-
-based score, we first optimize the cluster assignment of the isolated label using the F1
-score across louvain clustering resolutions ranging from 0.1 to 2 in resolution steps of
-0.1. The optimal F1 score for the isolated label is then used as the metric score. The
-F1 score is a weighted mean of precision and recall given by the equation:
-𝐹1=2×(precision×recall)/(precision+recall).
-
-It returns a value between 0 and 1, where 1 shows that all of the isolated label cells
-and no others are captured in the cluster. For the isolated label ASW score, we compute
-the ASW of isolated versus nonisolated labels on the PCA embedding (ASW metric above)
-and scale this score to be between 0 and 1. The final score for each metric version
+The isolated label metric is available in two versions: (1) the best clustering of the
+isolated label (F1 score) and (2) the global ASW of the isolated label. This function 
+contains the latter version. The isolated label ASW score is obtained by computing the
+ASW of isolated versus non-isolated labels on the PCA embedding (ASW metric above) and
+scaling this score to be between 0 and 1. The final score for each metric version
 consists of the mean isolated score of all isolated labels.
 """
 
