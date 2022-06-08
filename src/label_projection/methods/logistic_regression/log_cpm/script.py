4c057e481966dba28158a92bbf0794c76987161a
Vinicius Saraiva <chagas.vis@gmail.com>
2022-06-08 09:17:08 -0300

    feat: logistic_regression
    Former-commit-id: 979d40e6ae60cfcbd22fad4f8e728a4cf71546d9

--- src/label_projection/methods/logistic_regression/log_cpm/script.py
+++ src/label_projection/methods/logistic_regression/log_cpm/script.py
@@ -1,7 +1,7 @@
 ## VIASH START
 par = {
     'input': '../../../data/test_data_preprocessed.h5ad',
-    'output': 'output.mlplogcpm.h5ad'
+    'output': 'output.knnscran.h5ad'
 }
 ## VIASH END
 resources_dir = "../../../../common/tools/"
@@ -14,17 +14,16 @@ sys.path.append(meta['resources_dir'])
 import scanpy as sc
 from normalize import log_cpm
 from utils import classifier
-import sklearn.neural_network
+import sklearn.linear_model
 
 print("Load input data")
 adata = sc.read(par['input'])
 
 print("Run classifier")
-hidden_layer_sizes = tuple(par['hidden_layer_sizes'])
-max_iter = par['max_iter']
 adata = log_cpm(adata)
-adata = classifier(adata, estimator=sklearn.neural_network.MLPClassifier, hidden_layer_sizes=hidden_layer_sizes, max_iter=max_iter)
-adata.uns["method_id"] = "mlp_log_cpm"
+max_iter = par['max_iter']
+adata = classifier(adata, estimator=sklearn.linear_model.LogisticRegression, max_iter=max_iter)
+adata.uns["method_id"] = "logistic_regression_log_cpm"
 
 print("Write data")
 adata.write(par['output'], compression="gzip")
--- src/label_projection/methods/logistic_regression/scran/script.py
+++ src/label_projection/methods/logistic_regression/scran/script.py
@@ -1,7 +1,7 @@
 ## VIASH START
 par = {
     'input': '../../../data/test_data_preprocessed.h5ad',
-    'output': 'output.mlplogcpm.h5ad'
+    'output': 'output.mlpscran.h5ad'
 }
 ## VIASH END
 resources_dir = "../../../../common/tools/"
@@ -12,19 +12,18 @@ sys.path.append(resources_dir)
 sys.path.append(utils_dir)
 sys.path.append(meta['resources_dir'])
 import scanpy as sc
-from normalize import log_cpm
+from normalize import log_scran_pooling
 from utils import classifier
-import sklearn.neural_network
+import sklearn.linear_model
 
 print("Load input data")
 adata = sc.read(par['input'])
 
 print("Run classifier")
-hidden_layer_sizes = tuple(par['hidden_layer_sizes'])
 max_iter = par['max_iter']
-adata = log_cpm(adata)
-adata = classifier(adata, estimator=sklearn.neural_network.MLPClassifier, hidden_layer_sizes=hidden_layer_sizes, max_iter=max_iter)
-adata.uns["method_id"] = "mlp_log_cpm"
+adata = log_scran_pooling(adata)
+adata = classifier(adata, estimator=sklearn.linear_model.LogisticRegression, max_iter=max_iter)
+adata.uns["method_id"] = "logistic_regression_scran"
 
 print("Write data")
 adata.write(par['output'], compression="gzip")
