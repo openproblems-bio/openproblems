36968623be5f7e7c5bc4802a8e8612efa1af8e4b
Scott Gigante <scott.gigante@immunai.com>
2022-06-13 13:30:04 -0400

    replace __from_cache__ with .uns['_from_cache']

--- openproblems/tasks/_batch_integration/batch_integration_graph/datasets/immune.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/datasets/immune.py
@@ -7,7 +7,6 @@ import scanpy as sc
 @dataset(dataset_name="Immune (by batch)", image="openproblems")
 def immune_batch(test=False):
     adata = load_immune(test)
-    from_cache = adata.__from_cache__
     adata.obs["labels"] = adata.obs["final_annotation"]
 
     sc.pp.filter_genes(adata, min_counts=1)
@@ -21,6 +20,4 @@ def immune_batch(test=False):
     adata.obsm["X_uni"] = adata.obsm["X_pca"]
 
     sc.pp.neighbors(adata, use_rep="X_uni", key_added="uni")
-
-    adata.__from_cache__ = from_cache
     return adata
