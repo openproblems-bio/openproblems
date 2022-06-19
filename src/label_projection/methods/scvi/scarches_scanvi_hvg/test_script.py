328a85fbf80a318e5cb14c4336fd43d85fac1c34
Vinicius Saraiva <chagas.vis@gmail.com>
2022-06-19 15:52:41 -0300

    feat: scvi_methods - scarches_scanvi_hvg
    Former-commit-id: 5be853764979da380a027a4cdb56b47b88b1a116

---
+++ src/label_projection/methods/scvi/scarches_scanvi_hvg/test_script.py
@@ -0,0 +1,29 @@
+import subprocess
+import scanpy as sc
+from os import path
+
+INPUT = "toy_preprocessed_data.h5ad"
+OUTPUT = "output.scarchesallgenes.h5ad"
+
+print(">> Running script as test")
+out = subprocess.check_output([
+    "./scarches_scanvi_hvg",
+    '--n_hidden', "32",
+    '--n_layers', "1",
+    '--n_latent', "10",
+    '--span', "0.8",
+    '--n_top_genes', "2000",
+    '--max_epochs', "1",
+    '--limit_train_batches', "10",
+    '--limit_val_batches', "10",
+    "--input", INPUT,
+    "--output", OUTPUT
+]).decode("utf-8")
+
+print(">> Checking if output file exists")
+assert path.exists(OUTPUT)
+
+print(">> Checking if predictions were added")
+adata = sc.read_h5ad(OUTPUT)
+assert "labels_pred" in adata.obs
+assert "scarches_scanvi_hvg" == adata.uns["method_id"]
