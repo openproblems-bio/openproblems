58fab2af37d56729d38ed3e8df73ffcbaed63122
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-06-21 12:18:11 -0400

    Set `max_epochs` on `test`

--- openproblems/tasks/spatial_decomposition/methods/destvi.py
+++ openproblems/tasks/spatial_decomposition/methods/destvi.py
@@ -11,9 +11,14 @@ from .._utils import split_sc_and_sp
     code_url="https://github.com/YosefLab/scvi-tools",
     image="openproblems-python-extras",
 )
-def destvi_raw(adata, test=False):
+def destvi_raw(adata, test=False, max_epochs=None):
     from scvi.model import CondSCVI
     from scvi.model import DestVI
+    
+    if test:
+        max_epochs = max_epochs or 10
+    else:
+        max_epochs = max_epochs or 2500
 
     adata_sc, adata_sp = split_sc_and_sp(adata)
     CondSCVI.setup_anndata(adata_sc, labels_key="label", layer=None)
@@ -22,7 +27,7 @@ def destvi_raw(adata, test=False):
     DestVI.setup_anndata(adata_sp, layer=None)
 
     st_model = DestVI.from_rna_model(adata_sp, sc_model)
-    st_model.train(max_epochs=2500)
+    st_model.train(max_epochs=max_epochs)
     adata_sp.obsm["proportions_pred"] = st_model.get_proportions()
     adata.uns["method_code_version"] = check_version("scvi-tools")
     return adata_sp
