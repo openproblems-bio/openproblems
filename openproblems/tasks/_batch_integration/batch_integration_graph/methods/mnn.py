4fe7b85950847c45fce6d3c6953450748c1d67f2
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-06-22 12:17:03 -0400

    Refactor `batch_integration_graph` (#445)
    * remove bbknn_full_unscaled
    
    * refactor batch_integration_graph to use r_function and test arg
    
    * shorten test timeout
    
    * Revert "shorten test timeout"
    
    This reverts commit 4f3ff0643be4656c26e8d9e0cc428473b9e245d2 [formerly ddbfdaeb7a5ad6989802525c7e7cb8cdd41571fd].
    
    * bump scib
    
    * just use annoy==1.16.3
    
    * Convert strings to categoricals before running scanorama
    
    * fix method names
    
    Former-commit-id: b5eca97e049fe84571e5ee331cbf3e9a122eb5d0

--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/mnn.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/mnn.py
@@ -1,88 +1,55 @@
-# from ....tools.normalize import log_cpm
 from .....tools.decorators import method
 from .....tools.utils import check_version
 
+import functools
 
-@method(
-    method_name="MNN",
-    paper_name="""Batch effects in single-cell RNA-sequencing
-               data are corrected by matching mutual nearest neighbors""",
+_mnn_method = functools.partial(
+    method,
+    paper_name="Batch effects in single-cell RNA-sequencing "
+    "data are corrected by matching mutual nearest neighbors",
     paper_url="https://www.nature.com/articles/nbt.4091",
     paper_year=2018,
     code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
     image="openproblems-python-batch-integration",
 )
-def mnn_full_unscaled(adata, test=False):
+
+
+def _mnn(adata):
     from scib.integration import runMNN
     from scib.preprocessing import reduce_data
 
     adata = runMNN(adata, "batch")
     reduce_data(adata, umap=False)
-    # Complete the result in-place
+    adata.uns["method_code_version"] = check_version("mnnpy")
     return adata
 
 
-@method(
-    method_name="MNN (hvg/unscaled)",
-    paper_name="""Batch effects in single-cell RNA-sequencing
-               data are corrected by matching mutual nearest neighbors""",
-    paper_url="https://www.nature.com/articles/nbt.4091",
-    paper_year=2018,
-    code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
-    image="openproblems-python-batch-integration",
-)
+@_mnn_method(method_name="MNN (full/unscaled)")
+def mnn_full_unscaled(adata, test=False):
+    return _mnn(adata)
+
+
+@_mnn_method(method_name="MNN (hvg/unscaled)")
 def mnn_hvg_unscaled(adata, test=False):
     from ._utils import hvg_batch
-    from scib.integration import runMNN
-    from scib.preprocessing import reduce_data
 
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
-    adata = runMNN(adata, "batch")
-    reduce_data(adata, umap=False)
-    return adata
+    return _mnn(adata)
 
 
-@method(
-    method_name="MNN (hvg/scaled)",
-    paper_name="""Batch effects in single-cell RNA-sequencing
-               data are corrected by matching mutual nearest neighbors""",
-    paper_url="https://www.nature.com/articles/nbt.4091",
-    paper_year=2018,
-    code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
-    image="openproblems-python-batch-integration",
-)
+@_mnn_method(method_name="MNN (hvg/scaled)")
 def mnn_hvg_scaled(adata, test=False):
     from ._utils import hvg_batch
     from ._utils import scale_batch
-    from scib.integration import runMNN
-    from scib.preprocessing import reduce_data
 
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
     adata = scale_batch(adata, "batch")
-    adata = runMNN(adata, "batch")
-    reduce_data(adata, umap=False)
-    return adata
+    return _mnn(adata)
 
 
-@method(
-    method_name="MNN (full/scaled)",
-    paper_name="""Batch effects in single-cell RNA-sequencing
-               data are corrected by matching mutual nearest neighbors""",
-    paper_url="https://www.nature.com/articles/nbt.4091",
-    paper_year=2018,
-    code_url="https://github.com/chriscainx/mnnpy",
-    code_version=check_version("mnnpy"),
-    image="openproblems-python-batch-integration",
-)
+@_mnn_method(method_name="MNN (full/scaled)")
 def mnn_full_scaled(adata, test=False):
     from ._utils import scale_batch
-    from scib.integration import runMNN
-    from scib.preprocessing import reduce_data
 
     adata = scale_batch(adata, "batch")
-    adata = runMNN(adata, "batch")
-    reduce_data(adata, umap=False)
-    return adata
+    return _mnn(adata)
--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/scanvi.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/scanvi.py
@@ -1,18 +1,20 @@
 from .....tools.decorators import method
 from .....tools.utils import check_version
 
+import functools
 
-@method(
-    method_name="scanvi",
-    paper_name="Probabilistic harmonization and annotation of single‐cell\
-                transcriptomics data with deep generative models",
+_scanvi_method = functools.partial(
+    method,
+    paper_name="Probabilistic harmonization and annotation of single‐cell "
+    "transcriptomics data with deep generative models",
     paper_url="https://www.embopress.org/doi/full/10.15252/msb.20209620",
     paper_year=2021,
     code_url="https://github.com/YosefLab/scvi-tools",
-    code_version=check_version("scvi"),
-    image="openproblems-python-batch-integration",  # only if required
+    image="openproblems-python-batch-integration",
 )
-def scanvi_full_unscaled(adata, test=False):
+
+
+def _scanvi(adata):
     from scanpy.preprocessing import neighbors
     from scib.integration import runScanvi
 
@@ -24,31 +26,18 @@ def scanvi_full_unscaled(adata, test=False):
     adata.obs.rename(
         columns={"lab": "labels"}, inplace=True
     )  # ugly fix for scvi conversion error
+    adata.uns["method_code_version"] = check_version("scvi")
     return adata
 
 
-@method(
-    method_name="Scanvi (hvg/unscaled)",
-    paper_name="Probabilistic harmonization and annotation of single‐cell\
-                transcriptomics data with deep generative models",
-    paper_url="https://www.embopress.org/doi/full/10.15252/msb.20209620",
-    paper_year=2021,
-    code_url="https://github.com/YosefLab/scvi-tools",
-    code_version=check_version("scvi"),
-    image="openproblems-python-batch-integration",  # only if required
-)
+@_scanvi_method(method_name="scANVI (full/unscaled)")
+def scanvi_full_unscaled(adata, test=False):
+    return _scanvi(adata)
+
+
+@_scanvi_method(method_name="scANVI (hvg/unscaled)")
 def scanvi_hvg_unscaled(adata, test=False):
     from ._utils import hvg_batch
-    from scanpy.preprocessing import neighbors
-    from scib.integration import runScanvi
 
-    adata.obs.rename(
-        columns={"labels": "lab"}, inplace=True
-    )  # ugly fix for scvi conversion error
     adata = hvg_batch(adata, "batch", target_genes=2000, adataOut=True)
-    adata = runScanvi(adata, "batch", "lab")
-    neighbors(adata, use_rep="X_emb")
-    adata.obs.rename(
-        columns={"lab": "labels"}, inplace=True
-    )  # ugly fix for scvi conversion error
-    return adata
+    return _scanvi(adata)
