c7cd8913b18692d0155cc3cfd476e7b160bd5a1c
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-06-23 11:26:15 -0400

    Temporarily remove steroscope

--- openproblems/tasks/spatial_decomposition/methods/__init__.py
+++ openproblems/tasks/spatial_decomposition/methods/__init__.py
@@ -5,5 +5,5 @@ from .nusvr import nusvr_sklearn
 from .random import random_proportion_assignment
 from .rctd import rctd
 from .seuratv3 import seuratv3
-from .stereoscope import stereoscope_raw
+# from .stereoscope import stereoscope_raw
 from .vanillanmf import nmf
