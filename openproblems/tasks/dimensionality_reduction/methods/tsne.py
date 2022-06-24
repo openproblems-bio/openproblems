8515821aaaadbbb7cae886a07c192e3536779af2
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-06-24 11:20:31 -0400

    Add phate with sqrt potential (#430)
    * add phate with sqrt potential
    
    * fix tsne version and params
    
    * Set gamma in `phate_sqrt`

--- openproblems/tasks/dimensionality_reduction/methods/tsne.py
+++ openproblems/tasks/dimensionality_reduction/methods/tsne.py
@@ -12,12 +12,12 @@ import scanpy as sc
     paper_year=2008,
     code_url="https://scikit-learn.org/stable/modules/generated/"
     "sklearn.manifold.TSNE.html#sklearn.manifold.TSNE",
-    code_version=check_version("MulticoreTSNE"),
     image="openproblems-python-extras",
 )
 def tsne_logCPM_1kHVG(adata, test: bool = False, n_pca=50):
     adata = log_cpm_hvg(adata)
-    sc.tl.pca(adata, n_comps=50, svd_solver="arpack")
+    sc.tl.pca(adata, n_comps=n_pca, svd_solver="arpack")
     sc.tl.tsne(adata, use_rep="X_pca", n_pcs=n_pca)
     adata.obsm["X_emb"] = adata.obsm["X_tsne"]
+    adata.uns["method_code_version"] = check_version("MulticoreTSNE")
     return adata
