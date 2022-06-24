e9497d8c7df9e82f10025704bcfe575bb1982604
Scott Gigante <scott.gigante@immunai.com>
2022-06-24 13:10:50 -0400

    Revert "rename _batch_integration to batch_integration"
    This reverts commit c184488b2de6c00dc01edcf4bd241165ab68d5be.

--- openproblems/tasks/__init__.py
+++ openproblems/tasks/__init__.py
@@ -3,4 +3,4 @@ from . import dimensionality_reduction
 from . import label_projection
 from . import multimodal_data_integration
 from . import regulatory_effect_prediction
-from .batch_integration import batch_integration_graph
+from ._batch_integration import batch_integration_graph
