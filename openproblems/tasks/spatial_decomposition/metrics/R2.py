9147b02ca8b353cd9127c5726e9bafb2f14a7404
Scott Gigante <scott.gigante@immunai.com>
2022-06-28 13:06:36 -0400

    fix R2

--- openproblems/tasks/spatial_decomposition/metrics/R2.py
+++ openproblems/tasks/spatial_decomposition/metrics/R2.py
@@ -6,8 +6,8 @@ import sklearn.metrics
 @metric(metric_name="R2", maximize=True)
 def R2(adata):
 
-    prop_true = adata.obsm["proportions_true"].values
-    prop_pred = adata.obsm["proportions_pred"].values
+    prop_true = adata.obsm["proportions_true"]
+    prop_pred = adata.obsm["proportions_pred"]
 
     r2_score = sklearn.metrics.r2_score(
         prop_true, prop_pred, sample_weight=None, multioutput="uniform_average"
