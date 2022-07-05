852a5ddb774dc8b2704e8e6a76a74cd9883529be
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-05 15:35:10 -0400

    Remove unused code

--- openproblems/tasks/denoising/methods/dca.py
+++ openproblems/tasks/denoising/methods/dca.py
@@ -1,31 +1,7 @@
 from ....tools.decorators import method
 from ....tools.utils import check_version
 
-# import numpy as np
 import scanpy as sc
-
-
-def remove_zeros(Xmat):
-    if scipy.sparse.issparse(Xmat):
-        Xmat = Xmat.tocsc()
-        # creates boolean list of missing
-        missing_bool = np.apply_along_axis(np.count_nonzero, 0, Xmat) == 0
-        # removes missing genes
-        Xmat[, (np.apply_along_axis(np.count_nonzero, 0, Xmat) == 0)]
-        # creates list of true (missing) indices
-        inds = list(compress(xrange(len(missing_bool)), missing_bool))
-    return Xmat, inds
-
-# from https://stackoverflow.com/questions/53870310
-
-
-def insert_at(arr, output_size, indices):
-    result = np.zeros(output_size)
-    existing_indices = [np.setdiff1d(np.arange(axis_size),
-                                     axis_indices, assume_unique=True)
-                        for axis_size, axis_indices in zip(output_size, indices)]
-    result[np.ix_(*existing_indices)] = arr
-    return result
 
 
 def _dca(adata, test=False, epochs=None):
