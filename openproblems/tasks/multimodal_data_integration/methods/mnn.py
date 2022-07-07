508b67398b5df902a7537069664016c3407b7e7b
Scott Gigante <scott.gigante@immunai.com>
2022-07-07 11:02:01 -0400

    use decorator in both methods

--- openproblems/tasks/multimodal_data_integration/methods/mnn.py
+++ openproblems/tasks/multimodal_data_integration/methods/mnn.py
@@ -30,14 +30,8 @@ def mnn_log_cpm(adata, test=False):
     return adata
 
 
-@method(
+@_mnn_method(
     method_name="Mutual Nearest Neighbors (log scran)",
-    paper_name="Batch effects in single-cell RNA-sequencing data are corrected by "
-    "matching mutual nearest neighbors",
-    paper_url="https://www.nature.com/articles/nbt.4091",
-    paper_year=2018,
-    code_url="https://github.com/LTLA/batchelor",
-    image="openproblems-r-extras",
 )
 def mnn_log_scran_pooling(adata, test=False):
     adata = log_scran_pooling(adata)
