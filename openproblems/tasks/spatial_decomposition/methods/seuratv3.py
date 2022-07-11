7a2af66d6fccbcda1247bd3dd49655142e534ca2
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-11 18:54:07 -0400

    Set n_pca in seuratv3.py

--- openproblems/tasks/spatial_decomposition/methods/seuratv3.py
+++ openproblems/tasks/spatial_decomposition/methods/seuratv3.py
@@ -4,8 +4,9 @@ from ....tools.utils import check_r_version
 from .._utils import split_sc_and_sp
 
 import pandas as pd
+from typing import Optional
 
-_seuratv3 = r_function("seuratv3.R", args="sce_sc, sce_sp")
+_seuratv3 = r_function("seuratv3.R", args="sce_sc, sce_sp, n_pcs")
 
 
 @method(
@@ -16,12 +17,16 @@ _seuratv3 = r_function("seuratv3.R", args="sce_sc, sce_sp")
     code_url="https://satijalab.org/seurat/archive/v3.2/spatial_vignette.html",
     image="openproblems-r-extras",
 )
-def seuratv3(adata, test=False):
+def seuratv3(adata, test: bool = False, n_pca: Optional[int] = None):
+    if test:
+        n_pca = n_pca or 10
+    else:  # pragma: nocover
+        n_pca = n_pca or 30
     # extract single cell reference data
     adata_sc, adata = split_sc_and_sp(adata)
     # proportions_true gets lost in translation
     proportions_true = adata.obsm["proportions_true"]
-    adata = _seuratv3(adata_sc, adata)
+    adata = _seuratv3(adata_sc, adata, n_pcs=n_pca)
     # get predicted cell type proportions from obs
     cell_type_names = pd.Index([x for x in adata.obs.columns if x.startswith("xCT_")])
     proportions_pred = adata.obs[cell_type_names].to_numpy()
