6b4d09ca232b305b9045f11baf004ea8146cbeef
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-13 11:00:37 -0400

    Shorten descriptors

--- openproblems/tasks/spatial_decomposition/datasets/pancreas.py
+++ openproblems/tasks/spatial_decomposition/datasets/pancreas.py
@@ -10,7 +10,7 @@ import scanpy as sc
 @dataset(
     "Pancreas (alpha=1)",
     data_url=load_pancreas.metadata["data_url"],
-    dataset_summary="Human pancreas cells aggregated from single-cell reference"
+    dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=1)",
 )
 def pancreas_alpha_1(test=False, n_obs=100):
@@ -27,7 +27,7 @@ def pancreas_alpha_1(test=False, n_obs=100):
 @dataset(
     "Pancreas (alpha=5)",
     data_url=load_pancreas.metadata["data_url"],
-    dataset_summary="Human pancreas cells aggregated from single-cell reference"
+    dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=5)",
 )
 def pancreas_alpha_5(test=False, n_obs=100):
@@ -43,7 +43,7 @@ def pancreas_alpha_5(test=False, n_obs=100):
 @dataset(
     "Pancreas (alpha=0.5)",
     data_url=load_pancreas.metadata["data_url"],
-    dataset_summary="Human pancreas cells aggregated from single-cell reference"
+    dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=0.5)",
 )
 def pancreas_alpha_0_5(test=False, n_obs=100):
