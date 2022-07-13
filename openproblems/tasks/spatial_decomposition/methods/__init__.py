91ead11d122e85fa66c99f259896ed9fc6d45b55
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-13 09:45:38 -0400

    Remove commented imports

--- openproblems/tasks/spatial_decomposition/methods/__init__.py
+++ openproblems/tasks/spatial_decomposition/methods/__init__.py
@@ -1,10 +1,7 @@
-# from .destvi import destvi_raw
 from .nmfreg import nmfreg
 from .nnls import nnls_scipy
 from .nusvr import nusvr_sklearn
 from .random import random_proportion_assignment
 from .rctd import rctd
 from .seuratv3 import seuratv3
-
-# from .stereoscope import stereoscope_raw
 from .vanillanmf import nmf
