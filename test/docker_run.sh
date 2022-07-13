5cc6f67c1fa00e01998fdcf43e55879083176d64
Scott Gigante <scott.gigante@immunai.com>
2022-07-13 12:32:17 -0400

    install test reqs

--- test/docker_run.sh
+++ test/docker_run.sh
@@ -8,9 +8,8 @@ CODEDIR=$(dirname "$WORKDIR")
 export PYTHONPATH="$WORKDIR"
 
 if [ ! -f ~/.install_complete ]; then
-  python3 -m pip install --upgrade "pip<=21.0"
-  python3 -m pip install --use-deprecated=legacy-resolver --upgrade-strategy=only-if-needed --no-cache-dir --editable "$CODEDIR"
-  python3 -m pip install --use-deprecated=legacy-resolver -U coverage
+  python3 -m pip install --upgrade pip
+  python3 -m pip install --upgrade-strategy=only-if-needed --no-cache-dir --editable "$CODEDIR"[test]
   FREEZE="$(python3 -m pip freeze)"
   if echo "$FREEZE" | grep -q annoy; then
     python3 -m pip install --force "$(echo "$FREEZE" | grep annoy)"
