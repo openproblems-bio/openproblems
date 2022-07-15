99bcd02f9f9f9c3f7c8efa758a9fcbf09ee4f63a
Robrecht Cannoodt <rcannood@gmail.com>
2022-07-15 14:31:58 +0200

    add sync folder component
    Former-commit-id: cb680ea41d63a536267c9a5013f50be86f218ee1

---
+++ src/common/sync_test_resources/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+echo ">> Run aws s3 sync"
+./$meta_functionality_name \
+  --input s3://openproblems-data/pancreas \
+  --output foo \
+  --quiet
+
+echo ">> Check whether the right files were copied"
+[ ! -f foo/raw_data.h5ad ] && echo csv should have been copied && exit 1
+
+echo ">> Test succeeded!"
---
+++ src/common/sync_test_resources/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## VIASH START
+par_input='s3://openproblems-data'
+par_output='resources_test'
+## VIASH END
+
+extra_params=( )
+
+if [ "$par_quiet" == "true" ]; then
+  extra_params+=( "--quiet" )
+fi
+if [ "$par_dryrun" == "true" ]; then
+  extra_params+=( "--dryrun" )
+fi
+if [ "$par_delete" == "true" ]; then
+  extra_params+=( "--delete" )
+fi
+
+if [ ! -z ${par_exclude+x} ]; then
+  IFS=":"
+  for var in $par_exclude; do
+    unset IFS
+    extra_params+=( "--exclude" "$var" )
+  done
+fi
+
+
+# Disable the use of the Amazon EC2 instance metadata service (IMDS).
+# see https://florian.ec/blog/github-actions-awscli-errors/
+# or https://github.com/aws/aws-cli/issues/5234#issuecomment-705831465
+export AWS_EC2_METADATA_DISABLED=true
+
+aws s3 sync "$par_input" "$par_output" --no-sign-request "${extra_params[@]}"
