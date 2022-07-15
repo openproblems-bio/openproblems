3e1ced2d45a778a8f32ff13257a15c2c4a9b6608
Robrecht Cannoodt <rcannood@gmail.com>
2022-07-15 14:55:17 +0200

    fix test
    Former-commit-id: 05172695e2912ab0a7a3dc879d194daf55a75a89

--- src/common/sync_test_resources/run_test.sh
+++ src/common/sync_test_resources/run_test.sh
@@ -5,7 +5,7 @@
 
 echo ">> Run aws s3 sync"
 ./$meta_functionality_name \
-  --input s3://openproblems-data/pancreas \
+  --input s3://openproblems-data/label_projection/pancreas \
   --output foo \
   --quiet
 
