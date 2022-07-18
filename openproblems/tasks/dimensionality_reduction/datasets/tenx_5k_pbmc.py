4d48fa5808dff2a56932e56c2773b06fc8e79e29
Luke Zappia <lazappi@users.noreply.github.com>
2022-07-18 11:48:22 +0200

    Add tenx_5k_pbmc description

--- openproblems/tasks/dimensionality_reduction/datasets/tenx_5k_pbmc.py
+++ openproblems/tasks/dimensionality_reduction/datasets/tenx_5k_pbmc.py
@@ -6,7 +6,10 @@ from ....tools.decorators import dataset
     "5k Peripheral blood mononuclear cells (PBMCs) from a healthy donor. "
     "10x Genomics; July 24, 2019.",
     data_url=load_tenx_5k_pbmc.metadata["data_url"],
-    dataset_summary="TODO",
+    dataset_summary=(
+        "5k Peripheral Blood Mononuclear Cells (PBMCs) from a healthy donor "
+        "produced by 10x Genomics in July 2019"
+    ),
 )
 def tenx_5k_pbmc(test=False):
     return load_tenx_5k_pbmc(test=test)
