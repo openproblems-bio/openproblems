54fb06d9e1bd33702bd5e62e31b0c79623d83bb0
giovp <giov.pll@gmail.com>
2022-07-19 21:50:08 +0200

    rctd

--- openproblems/tasks/spatial_decomposition/methods/__init__.py
+++ openproblems/tasks/spatial_decomposition/methods/__init__.py
@@ -2,6 +2,5 @@ from .nmfreg import nmfreg
 from .nnls import nnls_scipy
 from .nusvr import nusvr_sklearn
 from .random import random_proportion_assignment
-from .rctd import rctd
 from .seuratv3 import seuratv3
 from .vanillanmf import nmf
