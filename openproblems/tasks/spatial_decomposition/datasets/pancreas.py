a46cc32a13dc1f53c22022b2d0949bcae840ceb7
Scott Gigante <scott.gigante@immunai.com>
2022-07-21 08:59:39 -0400

    add dataset_reference

--- openproblems/tasks/spatial_decomposition/datasets/pancreas.py
+++ openproblems/tasks/spatial_decomposition/datasets/pancreas.py
@@ -10,6 +10,7 @@ import scanpy as sc
 @dataset(
     "Pancreas (alpha=1)",
     data_url=load_pancreas.metadata["data_url"],
+    data_reference=load_pancreas.metadata["data_reference"],
     dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=1)",
 )
@@ -27,6 +28,7 @@ def pancreas_alpha_1(test=False, n_obs=100):
 @dataset(
     "Pancreas (alpha=5)",
     data_url=load_pancreas.metadata["data_url"],
+    data_reference=load_pancreas.metadata["data_reference"],
     dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=5)",
 )
@@ -43,6 +45,7 @@ def pancreas_alpha_5(test=False, n_obs=100):
 @dataset(
     "Pancreas (alpha=0.5)",
     data_url=load_pancreas.metadata["data_url"],
+    data_reference=load_pancreas.metadata["data_reference"],
     dataset_summary="Human pancreas cells aggregated from single-cell"
     " (Dirichlet alpha=0.5)",
 )
