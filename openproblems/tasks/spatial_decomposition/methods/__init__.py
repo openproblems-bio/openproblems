0ee3bd8b663da4ac81cbe830629c46510fced7ac
Scott Gigante <scott.gigante@immunai.com>
2022-07-25 11:53:26 -0400

    Revert "rctd"
    This reverts commit 54fb06d9e1bd33702bd5e62e31b0c79623d83bb0.

--- openproblems/tasks/spatial_decomposition/methods/__init__.py
+++ openproblems/tasks/spatial_decomposition/methods/__init__.py
@@ -2,5 +2,6 @@ from .nmfreg import nmfreg
 from .nnls import nnls_scipy
 from .nusvr import nusvr_sklearn
 from .random import random_proportion_assignment
+from .rctd import rctd
 from .seuratv3 import seuratv3
 from .vanillanmf import nmf
