bf0c6a7c9ffb1a25b914334a70b74df1fd405b34
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-25 16:13:28 -0400

    set method_code_version on the right adata object

--- openproblems/tasks/spatial_decomposition/methods/destvi.py
+++ openproblems/tasks/spatial_decomposition/methods/destvi.py
@@ -23,15 +23,15 @@ def destvi(adata, test=False, max_epochs_sc=None, max_epochs_sp=None):
         max_epochs_sc = max_epochs_sc or 300
         max_epochs_sp = max_epochs_sp or 2500
 
-    adata_sc, adata_sp = split_sc_and_sp(adata)
+    adata_sc, adata = split_sc_and_sp(adata)
 
     CondSCVI.setup_anndata(adata_sc, labels_key="label")
     sc_model = CondSCVI(adata_sc, weight_obs=False)
     sc_model.train(max_epochs=max_epochs_sc)
-    DestVI.setup_anndata(adata_sp)
+    DestVI.setup_anndata(adata)
 
-    st_model = DestVI.from_rna_model(adata_sp, sc_model)
+    st_model = DestVI.from_rna_model(adata, sc_model)
     st_model.train(max_epochs=max_epochs_sp)
-    adata_sp.obsm["proportions_pred"] = st_model.get_proportions().to_numpy()
+    adata.obsm["proportions_pred"] = st_model.get_proportions().to_numpy()
     adata.uns["method_code_version"] = check_version("scvi-tools")
-    return adata_sp
+    return adata
