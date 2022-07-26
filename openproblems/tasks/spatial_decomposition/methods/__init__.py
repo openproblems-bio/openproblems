3b9fc96d635a952c80ff73e71de4a164b932b01d
giovp <giov.pll@gmail.com>
2022-07-26 16:54:34 +0200

    add stereoscope

--- openproblems/tasks/spatial_decomposition/methods/__init__.py
+++ openproblems/tasks/spatial_decomposition/methods/__init__.py
@@ -3,4 +3,5 @@ from .nnls import nnls_scipy
 from .nusvr import nusvr_sklearn
 from .random import random_proportion_assignment
 from .seuratv3 import seuratv3
+from .stereoscope import stereoscope
 from .vanillanmf import nmf
