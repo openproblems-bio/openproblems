9f94f99bd8406effb04dc1c4ef1a228e3faac7a7
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-26 14:05:10 -0400

    Split line

--- openproblems/tasks/dimensionality_reduction/datasets/mouse_blood_olssen_labelled.py
+++ openproblems/tasks/dimensionality_reduction/datasets/mouse_blood_olssen_labelled.py
@@ -6,7 +6,8 @@ from ....tools.decorators import dataset
     "Mouse myeloid lineage differentiation",
     data_url=load_olsson_2016_mouse_blood.metadata["data_url"],
     data_reference=load_olsson_2016_mouse_blood.metadata["data_reference"],
-    dataset_summary="Myeloid lineage differentiation from mouse blood. Sequenced by SMARTseq in 2016 by Olsson et al.",
+    dataset_summary="Myeloid lineage differentiation from mouse blood. "
+    "Sequenced by SMARTseq in 2016 by Olsson et al.",
 )
 def olsson_2016_mouse_blood(test=False):
     return load_olsson_2016_mouse_blood(test=test)
