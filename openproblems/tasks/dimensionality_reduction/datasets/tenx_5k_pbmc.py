f254b9d1febf3b0ce72ecc96a69adcb42162bdca
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-26 14:06:28 -0400

    Make dataset name concise

--- openproblems/tasks/dimensionality_reduction/datasets/tenx_5k_pbmc.py
+++ openproblems/tasks/dimensionality_reduction/datasets/tenx_5k_pbmc.py
@@ -3,13 +3,12 @@ from ....tools.decorators import dataset
 
 
 @dataset(
-    "5k Peripheral blood mononuclear cells (PBMCs) from a healthy donor. "
-    "10x Genomics; July 24, 2019.",
+    "5k Peripheral blood mononuclear cells",
     data_url=load_tenx_5k_pbmc.metadata["data_url"],
     data_reference=load_tenx_5k_pbmc.metadata["data_reference"],
     dataset_summary=(
-        "5k Peripheral Blood Mononuclear Cells (PBMCs) from a healthy donor "
-        "produced by 10x Genomics in July 2019"
+        "5k Peripheral Blood Mononuclear Cells (PBMCs) from a healthy donor. "
+        "Sequenced on 10X v3 chemistry in July 2019 by 10X Genomics."
     ),
 )
 def tenx_5k_pbmc(test=False):
