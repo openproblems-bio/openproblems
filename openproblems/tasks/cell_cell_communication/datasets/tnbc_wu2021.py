553614505e8456e141205fd41b31af024830adf4
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-28 12:54:43 -0400

    Proofread description

--- openproblems/tasks/cell_cell_communication/datasets/tnbc_wu2021.py
+++ openproblems/tasks/cell_cell_communication/datasets/tnbc_wu2021.py
@@ -3,13 +3,13 @@ from ....tools.decorators import dataset
 
 
 @dataset(
-    "~43k Cells from Triple Negative Breast Cancer",
+    "Triple negative breast cancer atlas",
     data_url=load_tnbc_data.metadata["data_url"],
     data_reference=load_tnbc_data.metadata["data_reference"],
     dataset_summary="A single-cell atlas of human breast cancers with inferred "
-    "cytokine activities as assumed benchmark truth. Cytokine activities were "
-    "estimated by fitting a multivariate linear model with cytokine-focused "
-    "signatures (see Dimitrov et al., 2022)",
+    "cytokine activities as assumed true cell-cell communication. Cytokine "
+    "activities were estimated by fitting a multivariate linear model with "
+    "cytokine-focused signatures (see Dimitrov et al., 2022).",
     image="openproblems-r-extras",
 )
 def tnbc_data(test=False):
