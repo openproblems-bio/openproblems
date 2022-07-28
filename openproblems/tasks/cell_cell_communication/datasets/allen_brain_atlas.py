bc4bf5c5d88bdf75f87859433d6a3f571c31b162
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-07-28 12:53:04 -0400

    Proofread description

--- openproblems/tasks/cell_cell_communication/datasets/allen_brain_atlas.py
+++ openproblems/tasks/cell_cell_communication/datasets/allen_brain_atlas.py
@@ -3,13 +3,13 @@ from ....tools.decorators import dataset
 
 
 @dataset(
-    "~15k Murine Brain cells",
+    "Mouse brain atlas",
     data_url=load_mouse_brain_atlas.metadata["data_url"],
     data_reference=load_mouse_brain_atlas.metadata["data_reference"],
     dataset_summary="A murine brain atlas with inferred spatially-adjacent "
-    "cell types as assumed benchmark truth. Adjacent cell types are those "
-    "with z-transformed deconvolution proportion correlations >= 1.645, "
-    "inferred on murine brain 10x Visium slides (see Dimitrov et al., 2022)",
+    "cell types as assumed benchmark truth. Adjacent cell types are inferred "
+    "from z-transformed deconvolution proportion correlations. Generated from "
+    "murine brain 10x Visium slides (see Dimitrov et al., 2022).",
     image="openproblems-r-extras",
 )
 def mouse_brain_atlas(test=False):
