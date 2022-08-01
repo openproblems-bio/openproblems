87561cd18e9703468fa90509c2fc5b7c8f16d018
Scott Gigante <scott.gigante@immunai.com>
2022-08-01 08:30:18 -0400

    add data_reference

--- openproblems/tasks/spatial_decomposition/datasets/destvi/generate.py
+++ openproblems/tasks/spatial_decomposition/datasets/destvi/generate.py
@@ -4,7 +4,9 @@ from openproblems.tools.decorators import dataset
 
 @dataset(
     "DestVI",
-    data_url="https://doi.org/10.1038/s41587-022-01272-8",
+    data_url="https://github.com/romain-lopez/DestVI-reproducibility/"
+    "blob/master/simulations/make_dataset.py",
+    data_reference="https://doi.org/10.1038/s41587-022-01272-8",
     dataset_summary="Synthetic dataset from DestVI manuscript",
     image="openproblems-python-extras",
 )
