c8bf7622e28fd3e89c47bb289aeda35f004c0129
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-08-02 17:50:08 -0400

    Fill in missing summaries (#529)
    * document denoising
    
    * fill in todos for label projection
    
    * fill in todos for multimodal data integration
    
    * fill in todos for regulatory effect prediction
    
    * fix missing
    
    * fix missing
    
    * Re-word label projection

--- openproblems/tasks/denoising/datasets/pbmc.py
+++ openproblems/tasks/denoising/datasets/pbmc.py
@@ -4,10 +4,13 @@ from . import utils
 
 
 @dataset(
-    "1k PBMCs from a Healthy Donor (10x/v3)",
+    "1k Peripheral blood mononuclear cells",
     data_url=load_tenx_1k_pbmc.metadata["data_url"],
     data_reference=load_tenx_1k_pbmc.metadata["data_reference"],
-    dataset_summary="TODO",
+    dataset_summary=(
+        "1k Peripheral Blood Mononuclear Cells (PBMCs) from a healthy donor. "
+        "Sequenced on 10X v3 chemistry in November 2018 by 10X Genomics."
+    ),
     image="openproblems-python-extras",
 )
 def pbmc(test=False):
