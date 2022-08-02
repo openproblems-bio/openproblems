1639fefa6f2ae88e4a52c0d4228aabe7c088a81f
Scott Gigante <scott.gigante@immunai.com>
2022-08-02 15:46:19 -0400

    fix missing

--- openproblems/tasks/dimensionality_reduction/datasets/human_blood_nestorowa2016.py
+++ openproblems/tasks/dimensionality_reduction/datasets/human_blood_nestorowa2016.py
@@ -3,10 +3,11 @@ from ....tools.decorators import dataset
 
 
 @dataset(
-    "Human blood (HSCs and differentiation thereof). Nestorowa, et al. Blood. 2016",
+    "Mouse hematopoietic stem cell differentiation",
     data_url=load_human_blood_nestorowa2016.metadata["data_url"],
     data_reference=load_human_blood_nestorowa2016.metadata["data_reference"],
-    dataset_summary="TODO",
+    dataset_summary="1.6k hematopoietic stem and progenitor cells from mouse bone "
+    "marrow. Sequenced by Smart-seq2.",
 )
 def human_blood_nestorowa2016(test=False):
     return load_human_blood_nestorowa2016(test=test)
