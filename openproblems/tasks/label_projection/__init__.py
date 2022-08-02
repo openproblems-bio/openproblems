5025828a37d6db8a54af9f59fb9f1c308699f65f
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-08-02 16:41:02 -0400

    Re-word label projection

--- openproblems/tasks/label_projection/__init__.py
+++ openproblems/tasks/label_projection/__init__.py
@@ -5,7 +5,7 @@ from . import methods
 from . import metrics
 
 _task_name = "Label Projection"
-_task_summary = "Classification of cell types given an annotated training set"
+_task_summary = "Classification of cell types from an annotated reference"
 
 DATASETS = utils.get_callable_members(datasets)
 METHODS = utils.get_callable_members(methods)
