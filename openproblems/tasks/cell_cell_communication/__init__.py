714d10c61f28396b651c96d22397417255e25d5e
dbdimitrov <d.boykoff@gmail.com>
2022-08-04 10:47:03 +0200

    explicitly import get_callable_members

--- openproblems/tasks/cell_cell_communication/__init__.py
+++ openproblems/tasks/cell_cell_communication/__init__.py
@@ -1,4 +1,4 @@
-from ... import utils
+from ...utils import get_callable_members
 from . import api
 from . import datasets
 from . import methods
@@ -7,6 +7,6 @@ from . import metrics
 _task_name = "Cell-Cell Communication Inference"
 _task_summary = "Detect intercellular communication events between cell types"
 
-DATASETS = utils.get_callable_members(datasets)
-METHODS = utils.get_callable_members(methods)
-METRICS = utils.get_callable_members(metrics)
+DATASETS = get_callable_members(datasets)
+METHODS = get_callable_members(methods)
+METRICS = get_callable_members(metrics)
