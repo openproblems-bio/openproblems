aa079b92dc62a5e0c9dda5df39a49b8f1f6cd0f0
Nikolay Markov <nikolai.markov@icloud.com>
2022-08-04 11:38:50 -0500

    Rename R variables according to lint

--- openproblems/tasks/label_projection/methods/seurat.R
+++ openproblems/tasks/label_projection/methods/seurat.R
@@ -4,13 +4,13 @@ library(Seurat)
 
 # Method body
 # Default and test parameters
-n.pcs <- 50
-k.score <- NULL
-k.filter <- NULL
+n_pcs <- 50
+k_score <- NULL
+k-filter <- NULL
 if (is_test) {
-  n.pcs <- 5
-  k.score <- 5
-  k.filter <- 20
+  n_pcs <- 5
+  k_score <- 5
+  k_filter <- 20
 }
 
 data <- as.Seurat(sce, counts = "X", data = NULL)
@@ -19,21 +19,21 @@ data <- SCTransform(data, assay = "originalexp", verbose = FALSE)
 reference <- data[, data$is_train]
 query <- data[, !data$is_train]
 
-reference <- RunPCA(reference, assay = "SCT", verbose = FALSE, n_pcs = n.pcs)
+reference <- RunPCA(reference, assay = "SCT", verbose = FALSE, n_pcs = n_pcs)
 anchors <- FindTransferAnchors(
   reference = reference,
   query = query,
   normalization.method = "SCT",
-  dims = 1:n.pcs,
-  k.score = k.score,
-  k.filter = k.filter,
+  dims = 1:n_pcs,
+  k.score = k_score,
+  k.filter = k_filter,
   verbose = FALSE
 )
 query <- TransferData(
   anchorset = anchors,
   reference = reference,
   query = query,
-  k.weight = k.score,
+  k.weight = k_score,
   refdata = list(
     labels = "labels"
   ),
