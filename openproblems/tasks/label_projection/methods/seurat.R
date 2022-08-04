ab01a38f1ea1f54a91504bb8a8e38e3345f71805
Nikolay Markov <nikolai.markov@icloud.com>
2022-08-03 22:59:49 -0500

    Add Seurat method to label projection, try 2

--- openproblems/tasks/label_projection/methods/seurat.R
+++ openproblems/tasks/label_projection/methods/seurat.R
@@ -3,28 +3,45 @@ library(SingleCellExperiment)
 library(Seurat)
 
 # Method body
-data <- as.Seurat(sce, counts = "X")
-data <- SCTransform(data, verbose = FALSE)
+# Default and test parameters
+n_pcs <- 50
+k.score <- NULL
+k.filter <- NULL
+if (is_test) {
+  n_pcs <- 5
+  k.score <- 5
+  k.filter <- 20
+}
+
+data <- as.Seurat(sce, counts = "X", data = NULL)
+data <- SCTransform(data, assay = "originalexp", verbose = FALSE)
+
 reference <- data[, data$is_train]
 query <- data[, !data$is_train]
+
+reference <- RunPCA(reference, assay = "SCT", verbose = FALSE, n_pcs = n_pcs)
 anchors <- FindTransferAnchors(
   reference = reference,
   query = query,
   normalization.method = "SCT",
-  reference.reduction = "spca",
-  dims = 1:50
+  dims = 1:n_pcs,
+  k.score = k.score,
+  k.filter = k.filter,
+  verbose = FALSE
 )
 query <- TransferData(
   anchorset = anchors,
   reference = reference,
   query = query,
+  k.weight = k.score,
   refdata = list(
-    labels = "labels",
-  )
+    labels = "labels"
+  ),
+  verbose = FALSE
 )
 query$labels_pred <- query$predicted.labels
 reference$labels_pred <- reference$labels
-sce <- merge(reference, query)
+sce <- as.SingleCellExperiment(merge(reference, query))
 
 # Return
 sce
--- openproblems/tasks/label_projection/methods/seurat.py
+++ openproblems/tasks/label_projection/methods/seurat.py
@@ -4,12 +4,12 @@ from ....tools.utils import check_r_version
 
 import functools
 
-_seurat = r_function("seurat.R")
+_seurat = r_function("seurat.R", args="sce, is_test")
 
 _seurat_method = functools.partial(
     method,
     paper_name="Integrated analysis of multimodal single-cell data",
-    paper_url="https://www.sciencedirect.com/science/article/pii/S0092867421005833",
+    paper_url="https://doi.org/10.1016/j.cell.2021.04.048",
     paper_year=2021,
     code_url="https://github.com/satijalab/seurat",
     image="openproblems-r-extras",
@@ -20,6 +20,6 @@ _seurat_method = functools.partial(
     method_name="Seurat reference mapping (SCTransform)",
 )
 def seurat(adata, test=False):
-    adata = _seurat(adata)
+    adata = _seurat(adata, test)
     adata.uns["method_code_version"] = check_r_version("Seurat")
     return adata
