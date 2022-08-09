2bba6065bf2a9bb249e126e7650692d88dc71820
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-08-09 11:48:28 +0200

    fix dataset_loader
    Former-commit-id: 112f8b84d33ddd806b1086fd320869526dcbd2b8

--- src/common/dataset_loader/download/run_example.sh
+++ src/common/dataset_loader/download/run_example.sh
@@ -1,4 +1,6 @@
-bin/viash run src/common/data_loader/config.vsh.yaml -- \
-  --output src/common/data_loader/resources/pancreas.h5ad \
+bin/viash run src/common/dataset_loader/download/config.vsh.yaml -- \
+  --output src/common/dataset_loader/download/resources/pancreas.h5ad \
   --url https://ndownloader.figshare.com/files/24539828 \
-  --name pancreas --lognorm_available adata.X
+  --name pancreas \
+  --obs_cell_type celltype \
+  --obs_batch tech
