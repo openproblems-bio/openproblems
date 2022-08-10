274537571db69b4eebd6e82d3efb4ccc0b78893d
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-08-10 11:47:33 -0400

    Allow setting of kwargs in method call

--- openproblems/tasks/label_projection/methods/seurat.py
+++ openproblems/tasks/label_projection/methods/seurat.py
@@ -3,8 +3,9 @@ from ....tools.decorators import method
 from ....tools.utils import check_r_version
 
 import functools
+from typing import Optional
 
-_seurat = r_function("seurat.R", args="sce, is_test")
+_seurat = r_function("seurat.R", args="sce, n_pcs, k_score=NULL, k_filter=NULL")
 
 _seurat_method = functools.partial(
     method,
@@ -19,7 +20,18 @@ _seurat_method = functools.partial(
 @_seurat_method(
     method_name="Seurat reference mapping (SCTransform)",
 )
-def seurat(adata, test=False):
-    adata = _seurat(adata, test)
+def seurat(adata, n_pcs: Optional[int] = None, k_score: Optional[int] = None, k_filter: Optiona[int] = None, test: bool = False):
+    kwargs = {}
+    if test:
+        kwargs["n_pcs"] = n_pcs or 5
+        kwargs["k_score"] = k_score or 5
+        kwargs["k_filter"] = k_filter or 20
+    else:  # pragma: nocover
+        kwargs["n_pcs"] = n_pcs or 50
+        if k_score is not None:
+            kwargs["k_score"] = k_score
+        if k_filter is not None:
+            kwargs["k_filter"] = k_filter
+    adata = _seurat(adata, **kwargs)
     adata.uns["method_code_version"] = check_r_version("Seurat")
     return adata
