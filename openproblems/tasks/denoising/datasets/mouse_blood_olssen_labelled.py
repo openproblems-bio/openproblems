4025a3675a02364400f012c7e137d582a8f88f56
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-08-15 20:58:46 -0400

    Create mouse_blood_olssen_labelled.py

---
+++ openproblems/tasks/denoising/datasets/mouse_blood_olssen_labelled.py
@@ -0,0 +1,13 @@
+from ....data.mouse_blood_olssen_labelled import load_olsson_2016_mouse_blood
+from ....tools.decorators import dataset
+
+
+@dataset(
+    "Mouse myeloid lineage differentiation",
+    data_url=load_olsson_2016_mouse_blood.metadata["data_url"],
+    data_reference=load_olsson_2016_mouse_blood.metadata["data_reference"],
+    dataset_summary="Myeloid lineage differentiation from mouse blood. "
+    "Sequenced by SMARTseq in 2016 by Olsson et al.",
+)
+def olsson_2016_mouse_blood(test=False):
+    return load_olsson_2016_mouse_blood(test=test)
