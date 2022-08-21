51948cd12e91712fcfc205c5c6e9a8abf421acb6
giovp <giov.pll@gmail.com>
2022-08-21 18:49:58 +0200

    add description of dataset

--- openproblems/tasks/spatial_decomposition/datasets/destvi/generate.py
+++ openproblems/tasks/spatial_decomposition/datasets/destvi/generate.py
@@ -7,7 +7,10 @@ from openproblems.tools.decorators import dataset
     data_url="https://github.com/romain-lopez/DestVI-reproducibility/"
     "blob/master/simulations/make_dataset.py",
     data_reference="https://doi.org/10.1038/s41587-022-01272-8",
-    dataset_summary="Synthetic dataset from DestVI manuscript",
+    dataset_summary="scRNA-seq is generated based on learn NB parameters"
+    "from the destVI manuscripts leveraging sparsePCA. Number of cells and"
+    "cell types present in each spatial spot is computed via combination of"
+    "kernel-based parametrization of a categorical distribution and the NB model.",
     image="openproblems-python-extras",
 )
 def destvi(test=False):
