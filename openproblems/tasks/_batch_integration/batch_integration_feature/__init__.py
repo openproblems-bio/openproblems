4a3c4e75d9d2b0f568beb03637012fc118496eb4
Daniel Strobl <danielstrobl@gmail.com>
2022-08-23 17:00:36 +0200

    try scaling fix

--- openproblems/tasks/_batch_integration/batch_integration_feature/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_feature/__init__.py
@@ -5,7 +5,7 @@ from . import methods
 from . import metrics
 
 _task_name = "Batch integration embed"
-_task_summary = "TODO"
+_task_summary = "Feature subtask of batch integration task"
 
 DATASETS = utils.get_callable_members(datasets)
 METHODS = utils.get_callable_members(methods)
