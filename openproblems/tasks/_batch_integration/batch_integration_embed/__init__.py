24cb9cd56b1bbc26fdbe3a980bd080f1a79c4ed9
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-08-23 16:19:40 -0400

    Fix task summary

--- openproblems/tasks/_batch_integration/batch_integration_embed/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_embed/__init__.py
@@ -5,7 +5,7 @@ from . import methods
 from . import metrics
 
 _task_name = "Batch integration embed"
-_task_summary = "Subtask of batch integration on embedding output"
+_task_summary = "Removing batch effects while preserving biological variation in single-cell data embeddings"
 
 DATASETS = utils.get_callable_members(datasets)
 METHODS = utils.get_callable_members(methods)
