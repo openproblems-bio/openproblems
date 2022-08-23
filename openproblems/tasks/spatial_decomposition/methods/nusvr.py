4c6541d67e923e00bb695b783712a08f50851323
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-08-23 16:41:40 -0400

    Speed up NuSVR (#540)
    * set max_iter
    
    * fix docs
    
    * run PCA before prediction
    
    * bugfix
    
    * use StandardScaler
    
    * bugfix
    
    * scale separately
    
    * double normalize
    
    Former-commit-id: c6fa547d60fccef44a5a02c64530e70b2633b54e

--- openproblems/tasks/spatial_decomposition/methods/nusvr.py
+++ openproblems/tasks/spatial_decomposition/methods/nusvr.py
@@ -3,31 +3,50 @@ from ....tools.utils import check_version
 from ..utils import normalize_coefficients
 from ..utils import obs_means
 from ..utils import split_sc_and_sp
+from typing import Optional
 
 import numpy as np
+import scanpy as sc
 
 
 @method(
     method_name="NuSVR",
-    paper_name="Probabilistic Outputs for Support Vector Machines and Comparisons to Regularized Likelihood Methods",  # noqa: E501
+    paper_name="Probabilistic Outputs for Support Vector Machines and Comparisons to"
+    " Regularized Likelihood Methods",
     paper_url="http://citeseer.ist.psu.edu/viewdoc/summary?doi=10.1.1.41.1639",
     paper_year=1999,
     code_url="https://scikit-learn.org/stable/modules/generated/sklearn.svm.NuSVR.html",
 )
-def nusvr_sklearn(adata, test=False):
-    from scipy.sparse import issparse
+def nusvr_sklearn(
+    adata,
+    test: bool = False,
+    n_pca: Optional[int] = None,
+    max_iter: Optional[int] = None,
+):
+    from sklearn.preprocessing import StandardScaler
     from sklearn.svm import NuSVR
 
-    adata_sc, adata = split_sc_and_sp(adata)
-    adata_means = obs_means(adata_sc, "label")
+    if test:
+        max_iter = max_iter or 100
+        n_pca = n_pca or 10
+    else:  # pragma: nocover
+        max_iter = max_iter or 10000
+        n_pca = n_pca or 50
 
-    X = adata_means.X.T
-    y = adata.X.T
-    if issparse(y):
-        y = y.toarray()
+    sc.pp.pca(adata, n_comps=n_pca)
+    adata_sc, adata = split_sc_and_sp(adata)
+    adata_pca_means = obs_means(adata_sc, "label", obsm="X_pca")
+
+    # whiten PCA
+    scale_op = StandardScaler()
+    y = scale_op.fit_transform(adata.obsm["X_pca"]).T
+    X = scale_op.transform(adata_pca_means.X).T
+    # normalize voxels / celltypes
+    X = StandardScaler().fit_transform(X)
+    y = StandardScaler().fit_transform(y)
     res = np.zeros((y.shape[1], X.shape[1]))  # (voxels,cells)
     for i in range(y.shape[1]):
-        model = NuSVR(kernel="linear")
+        model = NuSVR(kernel="linear", max_iter=max_iter)
         model.fit(X, y[:, i])
         res[i] = model.coef_
 
