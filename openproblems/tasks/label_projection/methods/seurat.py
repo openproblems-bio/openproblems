6ac23dffe0516196ac3afedbe9c8ad8f523d540a
Scott Gigante <scott.gigante@immunai.com>
2022-08-25 15:29:56 -0400

    k_score can't be NULL

--- openproblems/tasks/label_projection/methods/seurat.py
+++ openproblems/tasks/label_projection/methods/seurat.py
@@ -7,7 +7,7 @@ import functools
 import pathlib
 
 _seurat = r_function(
-    "seurat_wrapper.R", args="sce, n_pcs, k_score=NULL, k_filter=NULL, script_path"
+    "seurat_wrapper.R", args="sce, n_pcs, k_score, k_filter, script_path"
 )
 
 _seurat_method = functools.partial(
@@ -37,10 +37,8 @@ def seurat(
         kwargs["k_filter"] = k_filter or 20
     else:  # pragma: nocover
         kwargs["n_pcs"] = n_pcs or 50
-        if k_score is not None:
-            kwargs["k_score"] = k_score
-        if k_filter is not None:
-            kwargs["k_filter"] = k_filter
+        kwargs["k_score"] = k_score or 30
+        kwargs["k_filter"] = k_filter or 200
     adata = _seurat(
         adata,
         script_path=pathlib.Path(__file__).parent.joinpath("seurat.R").as_posix(),
