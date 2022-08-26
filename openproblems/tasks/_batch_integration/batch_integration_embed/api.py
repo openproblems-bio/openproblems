scaling fix

openproblems/tasks/_batch_integration/batch_integration_embed/api.py
@@ -26,13 +26,14 @@ def sample_dataset():
     """Create a simple dataset to use for testing methods in this task."""
     adata = load_sample_data()
 
-    adata.var.index = adata.var.gene_short_name
-    sc.pp.filter_genes
+    adata.var.index = adata.var.gene_short_name.astype(str)
     sc.pp.normalize_total(adata)
     sc.pp.log1p(adata)
     adata.obsm["X_uni"] = sc.pp.pca(adata.X)
     adata.obs["batch"] = np.random.choice(2, adata.shape[0], replace=True).astype(str)
     adata.obs["labels"] = np.random.choice(5, adata.shape[0], replace=True).astype(str)
+    adata.var_names_make_unique()
+    adata.obs_names_make_unique()
     return adata
 
 