3dd5172042806cd2d1dfa55e637919fb41338f69
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-08-30 17:54:37 -0400

    Update knn_smoothing.py

--- openproblems/tasks/denoising/methods/knn_smoothing.py
+++ openproblems/tasks/denoising/methods/knn_smoothing.py
@@ -17,6 +17,6 @@ import scprep
 )
 def knn_smoothing(adata, test=False):
     import knn_smooth
-
+    adata.uns["method_code_version"] = check_version("knn_smooth")
     adata.X = knn_smooth.knn_smoothing(adata.X)
     return adata
