f6d1047647487017466f393adf4cedd8b6902c61
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-08-30 14:59:39 -0400

    Update __init__.py

--- openproblems/tasks/denoising/methods/__init__.py
+++ openproblems/tasks/denoising/methods/__init__.py
@@ -2,5 +2,5 @@ from .alra import alra
 from .dca import dca
 from .magic import magic
 from .magic import magic_approx
-from .magic import knn_smoothing
+from .magic import knn_naive
 from .no_denoising import no_denoising
