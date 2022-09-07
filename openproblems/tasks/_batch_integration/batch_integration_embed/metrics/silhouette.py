2e55837da84249209df60b47156ca2d4a9df7911
MalteDLuecken <m.d.luecken@gmail.com>
2022-09-08 01:07:03 +0200

    add underscore

--- openproblems/tasks/_batch_integration/batch_integration_embed/metrics/silhouette.py
+++ openproblems/tasks/_batch_integration/batch_integration_embed/metrics/silhouette.py
@@ -3,7 +3,7 @@ from .....tools.decorators import metric
 """
 For the bio-conservation score, the ASW was computed on cell identity labels and
 scaled to a value between 0 and 1 using the equation:
-celltypeASW=(ASWC+1)/2,
+celltypeASW=(ASW_C+1)/2,
 
 where C denotes the set of all cell identity labels.
 For information about the batch silhouette score, check sil_batch."""
