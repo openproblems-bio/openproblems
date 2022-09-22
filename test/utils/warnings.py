324d07089f2686d20f092acb7fbb656684e76d6a
Scott Gigante <scott.gigante@immunai.com>
2022-09-22 12:11:26 -0400

    use pytest warnings filtering

--- test/utils/warnings.py
+++ test/utils/warnings.py
@@ -3,50 +3,7 @@ import warnings
 
 def ignore_warnings():
     """Ignore irrelevant warnings."""
-    warnings.simplefilter("error")
-    warnings.filterwarnings(
-        "always",
-        category=FutureWarning,
-        message="self.adata.X is loaded as dense",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        category=FutureWarning,
-        message="The `inplace` parameter in pandas.Categorical.reorder_categories"
-        " is deprecated",
-        module="anndata._core.anndata",
-    )
-
-    warnings.filterwarnings(
-        "ignore",
-        category=DeprecationWarning,
-        message="This package has been superseded by the `leidenalg` package",
-        module="scanpy.tools._louvain",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        category=DeprecationWarning,
-        message="Please use `spmatrix` from the `scipy.sparse` namespace",
-        module="anndata._core.merge",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        category=FutureWarning,
-        message="The `inplace` parameter in pandas.Categorical.reorder_categories",
-        module="anndata._core.anndata",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        category=DeprecationWarning,
-        message=r"The global conversion available with activate\(\) is deprecated",
-        module="rpy2.robjects.numpy2ri",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        category=DeprecationWarning,
-        message=r"The global conversion available with activate\(\) is deprecated",
-        module="rpy2.robjects.pandas2ri",
-    )
+    # warnings.simplefilter("error")
 
     try:
         import numba
