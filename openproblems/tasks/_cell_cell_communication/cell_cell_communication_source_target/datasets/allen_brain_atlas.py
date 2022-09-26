299f0d758a847ff70c2931b4e594b9c097ca4a8c
Daniel Dimitrov <50865230+dbdimitrov@users.noreply.github.com>
2022-09-26 23:43:58 +0200

    Task cell cell comm (#520)
    * add liana to requirenments
    
    * pre-commit
    
    * add datasets, metric, apis, structure, etc
    
    * pre-commit
    
    * shorten paper lines
    
    * pre-commit
    
    * add method scores via liana
    
    * write task readme + handle ... via Python
    
    * pre-commit
    
    * add ccc to tasks init
    
    * pre-commit
    
    * line in liana.R < 80 chars
    
    * pre-commit
    
    * remove redundant sentence from readme, connectome to <80 chars
    
    * pre-commit
    
    * add task title + test param to methods
    
    * readme to <80 chars per line
    
    * pre-commit
    
    * lower perms for tests, extend bench in sample data
    
    * remove test jupyter notebook
    
    * pre-commit
    
    * change image on ccc datasets
    
    * pre-commit
    
    * brain atlas issue resolved, dcRMatrix for scran bug fix
    
    * change dataset metadata + refer to assay X
    
    * refer to class in 1st position
    
    * Fix filtering
    
    * pre-commit
    
    * address PR suggestions
    
    * pre-commit
    
    * Update task description
    
    * pre-commit
    
    * Cleaner handling of RsparseMatrix
    
    * pre-commit
    
    * Proofread task description
    
    * pre-commit
    
    * move methods to liana.py
    
    * extend assumed truth text
    
    * pre-commit
    
    * Use same decorator partial for all methods
    
    * Proofread description
    
    * Proofread description
    
    * more explicit column names and descriptions
    
    * pre-commit
    
    * add blank line
    
    * add links to papers in README
    
    * refactor API
    
    * is int target_organism
    
    * extend dataset tests
    
    * change sample_method and add validity checks
    
    * pre-commit
    
    * soften readme further
    
    * pre-commit
    
    * use numbers.Integral not np.integer
    
    * use .cat.categories to avoid subsampling away all members of one label causing an error
    
    * excl labels causing test failure
    
    * add lr_resource and tests to it
    
    * pre-commit
    
    * split masks into sep lines
    
    * add not
    
    * refactor
    
    * test and import changes
    
    * minor changes to readme
    
    * explicitly import get_callable_members
    
    * change image in decorator & subsample further
    
    * add liana to GHA docker image
    
    * more explicit tests + fix typo
    
    * reformat
    
    * Add bioc requirements for liana to GHA container
    
    * OmnipathR 3.4.0
    
    * ComplexHeatmap
    
    * Install libgeos-dev
    
    * Install libgeos-dev if not available
    
    * OmnipathR from github
    
    * OmnipathR from github
    
    * simplify and rename
    
    * convert cell_cell_communication into subtasks
    
    * set resource on adata
    
    * add noise instead of filtering missing genes
    
    * easier to read assertionerror
    
    * remove double assert
    
    * need to update layers['counts']
    
    * asarray
    
    * add map_gene_symbols function
    
    * use map_gene_symbols with dummy file
    
    * test on pre-filtering var names
    
    * change test resource, minor fix to fix uniques in flatten_complex test
    
    * fix merge conflict
    
    * pre-commit
    
    * add dataset figshare info, remove alias=gene, delete temp file
    
    * pre-commit
    
    * flake8
    
    * Flake8
    
    * pre-commit
    
    * min subunit for cpdb
    
    * explicitly mention steady-state focus
    
    * pre-commit
    
    * update liana's tag
    
    * allow missing props for ligand & receptor symbols in data
    
    * fix api consistency error (var_names_all not set)
    
    * print prop_missing in error
    
    * make sure additive noise is the same dtype
    
    * Alphabetise README
    
    * add unit tests
    
    * updated tnbc dataset
    
    * subsample by celltype
    
    * use subsample_even
    
    * no need to import warnings
    
    * copy on gene subset
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
    Co-authored-by: Scott Gigante <scott.gigante@immunai.com>

---
+++ openproblems/tasks/_cell_cell_communication/cell_cell_communication_source_target/datasets/allen_brain_atlas.py
@@ -0,0 +1,21 @@
+from .....data.allen_brain_atlas import load_mouse_brain_atlas
+from .....tools.decorators import dataset
+from ..._common.utils import ligand_receptor_resource
+
+
+@dataset(
+    "Mouse brain atlas",
+    data_url=load_mouse_brain_atlas.metadata["data_url"],
+    data_reference=load_mouse_brain_atlas.metadata["data_reference"],
+    dataset_summary="A murine brain atlas with inferred spatially-adjacent "
+    "cell types as assumed benchmark truth. Adjacent cell types are inferred "
+    "from z-transformed deconvolution proportion correlations. Generated from "
+    "murine brain 10x Visium slides (see Dimitrov et al., 2022).",
+    image="openproblems-r-extras",
+)
+def mouse_brain_atlas(test=False):
+    adata = load_mouse_brain_atlas(test=test)
+    adata.uns["ligand_receptor_resource"] = ligand_receptor_resource(
+        adata.uns["target_organism"]
+    )
+    return adata
