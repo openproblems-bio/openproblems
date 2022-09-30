c97decf07adb2e3050561d6fa9ae46132be07bef
Daniel Strobl <50872326+danielStrobl@users.noreply.github.com>
2022-09-30 14:49:17 +0200

    Use scran log-normalized layer for batch integration instead of counts (#576)
    * use normalized data for batch integration
    
    * pre-commit
    
    * fix name of scran layer in dataloaders
    
    * use log_scran_pooling function
    
    * adapt readme
    
    * pre-commit
    
    * Adapt to discussed setup
    
    * pre-commit
    
    * remove unused import
    
    * review
    
    * change to log_normalized
    
    * update readme
    
    * pre-commit
    
    * adapt embedding subtask
    
    * pre-commit
    
    * clarify pre-integration data
    
    * correct comment in dataloaders
    
    * remove layer X equals counts assertion
    
    * add log_normalized to sample dataloader
    
    * adapt api in embedding task
    
    * pre-commit
    
    * api
    
    * check default layer api adaption
    
    * fix testing
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- openproblems/tasks/denoising/__init__.py
+++ openproblems/tasks/denoising/__init__.py
@@ -7,6 +7,8 @@ from . import metrics
 _task_name = "Denoising"
 _task_summary = "Removing noise in sparse single-cell RNA-sequencing count data"
 
+DEFAULT_LAYER = "counts"
+
 DATASETS = utils.get_callable_members(datasets)
 METHODS = utils.get_callable_members(methods)
 METRICS = utils.get_callable_members(metrics)
--- test/test_0_tasks.py
+++ test/test_0_tasks.py
@@ -27,6 +27,9 @@ class TestTask(unittest.TestCase):
         assert isinstance(self.task._task_summary, str)
         assert len(self.task._task_summary) > TASK_SUMMARY_MINLEN
         assert len(self.task._task_summary) < TASK_SUMMARY_MAXLEN
+        assert hasattr(self.task, "DEFAULT_LAYER")
+        assert isinstance(self.task.DEFAULT_LAYER, str)
+        assert self.task.DEFAULT_LAYER in ["counts", "log_normalized"]
         assert hasattr(self.task, "api")
         assert isinstance(self.task.api, MODULE)
         for list_name in ["DATASETS", "METHODS", "METRICS"]:
