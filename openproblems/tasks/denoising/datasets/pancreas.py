9ee02ba4e984c4ab40ec2da2ae59f2b98a8bc919
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-10-04 10:16:07 -0400

    Update pancreas.py

--- openproblems/tasks/denoising/datasets/pancreas.py
+++ openproblems/tasks/denoising/datasets/pancreas.py
@@ -1,5 +1,5 @@
-from .....data.pancreas import load_pancreas
-from .....tools.decorators import dataset
+from ....data.pancreas import load_pancreas
+from ....tools.decorators import dataset
 
 # import scanpy as sc
 
