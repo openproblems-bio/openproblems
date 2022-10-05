d09e00027da669a929524f7d005678625b9872a8
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-10-05 12:59:43 -0400

    Update pancreas.py

--- openproblems/tasks/denoising/datasets/pancreas.py
+++ openproblems/tasks/denoising/datasets/pancreas.py
@@ -10,7 +10,7 @@ from . import utils
     dataset_summary="Human pancreatic islet scRNA-seq data from 6 datasets "
     "across technologies (CEL-seq, CEL-seq2, Smart-seq2, inDrop, Fluidigm C1, "
     "and SMARTER-seq).",
-    image="openproblems",
+    image="openproblems-python-extras",
 )
 def pancreas(test=False):
     adata = load_pancreas(test, integer_only=True)
