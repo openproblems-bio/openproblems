3d47f14355b0e63e4ae01cdfed060977a061c42a
Scott Gigante <scott.gigante@immunai.com>
2022-10-18 10:38:49 -0400

    fix imports

--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/__init__.py
@@ -1,4 +1,5 @@
-from .baseline import celltype_integration
+from .baseline import batch_random_integration
+from .baseline import celltype_random_integration
 from .baseline import no_integration
 from .baseline import random_integration
 from .bbknn import bbknn_full_scaled
