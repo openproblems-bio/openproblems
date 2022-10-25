1d0045f2f0719f1e92d0b3cae81e25796eeb7ac0
Scott Gigante <scott.gigante@immunai.com>
2022-10-25 13:00:10 -0400

    use just one batch of pancreas data

--- openproblems/data/pancreas.py
+++ openproblems/data/pancreas.py
@@ -1,6 +1,5 @@
 from . import utils
 
-import anndata as ad
 import numpy as np
 import os
 import scanpy as sc
@@ -12,17 +11,19 @@ URL = "https://ndownloader.figshare.com/files/36086813"
 
 
 @utils.loader(data_url=URL, data_reference="https://doi.org/10.1038/s41592-021-01336-8")
-def load_pancreas(test=False, integer_only=False):
+def load_pancreas(test=False, keep_techs=None):
     """Download pancreas data from figshare."""
     if test:
         # load full data first, cached if available
-        adata = load_pancreas(test=False, integer_only=integer_only)
+        adata = load_pancreas(
+            test=False,
+            keep_techs=["celseq", "inDrop4", "smarter"]
+            if keep_techs is None
+            else keep_techs,
+        )
 
         keep_celltypes = adata.obs["celltype"].dtype.categories[[0, 3]]
-        keep_techs = adata.obs["tech"].dtype.categories[[0, -3, -2]]
-        keep_tech_idx = adata.obs["tech"].isin(keep_techs)
-        keep_celltype_idx = adata.obs["celltype"].isin(keep_celltypes)
-        adata = adata[keep_tech_idx & keep_celltype_idx].copy()
+        adata = adata[adata.obs["celltype"].isin(keep_celltypes)].copy()
 
         # Subsample pancreas data
         adata = adata[:, :500].copy()
@@ -52,36 +53,15 @@ def load_pancreas(test=False, integer_only=False):
         scprep.io.download.download_url(URL, filepath)
         adata = sc.read(filepath)
 
+    if keep_techs is not None:
+        adata = adata[adata.obs["tech"].isin(keep_techs)].copy()
+
     # NOTE: adata.X contains log-normalized data, so we're moving it
     adata.layers["log_normalized"] = adata.X
     adata.X = adata.layers["counts"]
     del adata.layers["counts"]
 
-    if integer_only:
-        adata = _get_pancreas_integer(adata)
-
     # Ensure there are no cells or genes with 0 counts
     utils.filter_genes_cells(adata)
-
-    return adata
-
-
-def _get_pancreas_integer(adata: ad.AnnData):
-    """Transform counts to integer.
-
-    For some platforms the pancreas data set only have processed counts.
-    Here we grab those with integer counts.
-    See https://github.com/theislab/scib-reproducibility/tree/main/notebooks/data_preprocessing/pancreas # noqa: E501
-    """
-    is_int = ["smartseq2"]
-    is_int += ["inDrop{}".format(x) for x in range(1, 5)]
-
-    keep = np.zeros(len(adata)).astype(bool)
-
-    for tech in is_int:
-        idx = adata.obs.tech.values == tech
-        keep = keep | idx
-
-    adata = adata[keep, :].copy()
 
     return adata
