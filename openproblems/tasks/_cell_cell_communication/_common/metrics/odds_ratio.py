9ccafe8d2162316a931e767d35035b930eb6593a
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-10-28 16:33:08 -0400

    better error handling

--- openproblems/tasks/_cell_cell_communication/_common/metrics/odds_ratio.py
+++ openproblems/tasks/_cell_cell_communication/_common/metrics/odds_ratio.py
@@ -25,17 +25,16 @@ def odds_ratio(adata, top_prop=0.05):
     fn = np.sum(bot.response == 1)
     tn = np.sum(bot.response == 0)
 
-    # GT dependent
-    if np.sum(bot.response) == 0:  # comparison is impossible
-        return np.inf
-
-    if tp == top_n:  # best case scenario (all are true predictions)
-        tp = top_n - 1
-        fp = 1
-    if tp == 0:  # worst case scenario (no true predictions)
-        tp = 1
-        fp = top_n - 1
-
-    oddsratio = (tp / fp) / (fn / tn)
+    numerator = tp * tn
+    denominator = fp * fn
+    if denominator == 0:
+        if numerator == 0:
+            # undefined
+            oddsratio = np.nan
+        else:
+            # perfect score
+            oddsratio = np.inf
+    else:
+        oddsratio = numerator / denominator
 
     return oddsratio
