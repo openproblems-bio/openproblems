2e097eec220ccade9d166fb02c7b8eeb3e27d105
Scott Gigante <scott.gigante@immunai.com>
2022-11-08 12:23:18 -0500

    test dataset API on full datasets

--- test/test_3_datasets.py
+++ test/test_3_datasets.py
@@ -45,7 +45,7 @@ def _assert_not_bytes(X):
         (staticmethod(dataset), task, test, utils.TEMPDIR.name)
         for task in utils.git.list_modified_tasks()
         for dataset in task.DATASETS
-        for test in [True]
+        for test in [True, False]
     ],
     class_name_func=utils.name.name_test,
 )
