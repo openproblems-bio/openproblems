e0b913d786fcd438f081888643d1cf03b2edbaac
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-08 15:58:00 +0100

    refactor normalisation methods
    Former-commit-id: d93b94b190ea8d44294927322d7b9b2977e4b6cb

---
+++ src/label_projection/data_processing/normalize_log_cpm/script.py
@@ -0,0 +1,26 @@
+import scanpy as sc
+
+## VIASH START
+par = {
+    'input': "resources_test/label_projection/pancreas/dataset_subsampled.h5ad",
+    'output': "output.h5ad"
+}
+meta = {
+    "functionality_name": "normalize_log_cpm"
+}
+## VIASH END
+
+print(">> Load data")
+adata = sc.read_h5ad(par['input'])
+
+print(">> Normalize data")
+norm = sc.pp.normalize_total(adata, target_sum=1e6, key_added="size_factors", layer="counts", inplace=False)
+lognorm = sc.pp.log1p(norm["X"])
+
+print(">> Store output in adata")
+adata.layers["lognorm"] = lognorm
+adata.obs["norm_factor"] = norm["norm_factor"]
+adata.uns["normalization_method"] = meta["functionality_name"].removeprefix("normalize_")
+
+print(">> Write data")
+adata.write(par['output'], compression="gzip")
