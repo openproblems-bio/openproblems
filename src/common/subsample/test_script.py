8c2f2d95ca42c7803365cb7f833c98ddb9e01a6e
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-09 14:00:50 +0100

    fixes to tests
    Former-commit-id: 42d4bfb1f23b0645dce30cbf7456f9950e1abb13

--- src/common/subsample/test_script.py
+++ src/common/subsample/test_script.py
@@ -9,34 +9,44 @@ meta = {
 ### VIASH END
 
 input_path = f"{meta['resources_dir']}/pancreas/dataset.h5ad"
-output_path = "toy_data.h5ad"
+input = sc.read_h5ad(input_path)
 
 print(">> Running script as test for even")
+output_path = "output.h5ad"
 out = subprocess.check_output([
     meta["executable"],
     "--input", input_path,
     "--output", output_path,
-    "--even"
+    "--even",
+    "--seed", "123"
 ]).decode("utf-8")
 
 print(">> Checking whether file exists")
 assert path.exists(output_path)
 
 print(">> Check that test output fits expected API")
-adata = sc.read_h5ad(output_path)
-assert (495, 467) == adata.layers["counts"].shape, "processed result data shape {}".format(adata.layers["counts"].shape)
+output = sc.read_h5ad(output_path)
+
+assert input.n_obs >= output.n_obs
+assert input.n_vars == output.n_vars
+
+
 
 print(">> Runing script as test for specific batch and celltype categories")
+output2_path = "output.h5ad"
 out = subprocess.check_output([
     meta["executable"],
     "--input", input_path,
     "--keep_celltype_categories", "acinar:beta",
     "--keep_batch_categories", "celseq:inDrop4:smarter",
-    "--output", output_path
+    "--output", output_path,
+    "--seed", "123"
 ]).decode("utf-8")
 print(">> Checking whether file exists")
-assert path.exists(output_path)
+assert path.exists(output2_path)
 
 print(">> Check that test output fits expected API")
-adata = sc.read_h5ad(output_path)
-assert (500, 443) == adata.layers["counts"].shape, "processed result data shape {}".format(adata.layers["counts"].shape)
+output2 = sc.read_h5ad(output2_path)
+
+assert input.n_obs >= output2.n_obs
+assert input.n_vars == output2.n_vars
