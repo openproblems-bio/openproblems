a131c8f8750c708ba1063210b5084663f03074e4
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-09 11:38:13 +0100

    undo dataset renaming
    Former-commit-id: 05335885283d4030dd5a9d1cce28a4d630210ced

--- src/label_projection/resources_test_scripts/pancreas.sh
+++ src/label_projection/resources_test_scripts/pancreas.sh
@@ -22,19 +22,19 @@ mkdir -p $DATASET_DIR
 # censor dataset
 bin/viash run src/label_projection/data_processing/censoring/config.vsh.yaml -- \
     --input $RAW_DATA \
-    --output_train $DATASET_DIR/pancreas_cpm_train.h5ad \
-    --output_test $DATASET_DIR/pancreas_cpm_test.h5ad \
-    --output_solution $DATASET_DIR/pancreas_cpm_solution.h5ad \
+    --output_train $DATASET_DIR/dataset_cpm_train.h5ad \
+    --output_test $DATASET_DIR/dataset_cpm_test.h5ad \
+    --output_solution $DATASET_DIR/dataset_cpm_solution.h5ad \
     --seed 123
 
 # run one method
 bin/viash run src/label_projection/methods/knn_classifier/config.vsh.yaml -- \
-    --input_train $DATASET_DIR/pancreas_cpm_train.h5ad \
-    --input_test $DATASET_DIR/pancreas_cpm_test.h5ad \
-    --output $DATASET_DIR/pancreas_cpm_knn.h5ad
+    --input_train $DATASET_DIR/dataset_cpm_train.h5ad \
+    --input_test $DATASET_DIR/dataset_cpm_test.h5ad \
+    --output $DATASET_DIR/dataset_cpm_knn.h5ad
 
 # # run one metric
 # bin/viash run src/label_projection/metric/accuracy/config.vsh.yaml -- \
-#     --input_prediction $DATASET_DIR/pancreas_cpm_knn.h5ad \
-#     --input_solution $DATASET_DIR/pancreas_cpm_solution.h5ad \
-#     --output $DATASET_DIR/pancreas_cpm_knn_accuracy.h5ad
+#     --input_prediction $DATASET_DIR/dataset_cpm_knn.h5ad \
+#     --input_solution $DATASET_DIR/dataset_cpm_solution.h5ad \
+#     --output $DATASET_DIR/dataset_cpm_knn_accuracy.h5ad
