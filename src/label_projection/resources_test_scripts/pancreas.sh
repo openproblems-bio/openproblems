7001d7a162f5a63fe180544a6335b417a717c10e
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-10 12:56:34 +0100

    add multiple normalisation methods to one dataset
    Former-commit-id: dc7d674876b3b6bf150cbe61303a2e3adaaad6a8

--- src/label_projection/resources_test_scripts/pancreas.sh
+++ src/label_projection/resources_test_scripts/pancreas.sh
@@ -9,7 +9,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-RAW_DATA=resources_test/common/pancreas/dataset_cpm.h5ad
+RAW_DATA=resources_test/common/pancreas/dataset.h5ad
 DATASET_DIR=resources_test/label_projection/pancreas
 
 if [ ! -f $RAW_DATA ]; then
@@ -22,19 +22,19 @@ mkdir -p $DATASET_DIR
 # censor dataset
 bin/viash run src/label_projection/data_processing/censoring/config.vsh.yaml -- \
     --input $RAW_DATA \
-    --output_train $DATASET_DIR/dataset_cpm_train.h5ad \
-    --output_test $DATASET_DIR/dataset_cpm_test.h5ad \
-    --output_solution $DATASET_DIR/dataset_cpm_solution.h5ad \
+    --output_train $DATASET_DIR/train.h5ad \
+    --output_test $DATASET_DIR/test.h5ad \
+    --output_solution $DATASET_DIR/solution.h5ad \
     --seed 123
 
 # run one method
 bin/viash run src/label_projection/methods/knn_classifier/config.vsh.yaml -- \
-    --input_train $DATASET_DIR/dataset_cpm_train.h5ad \
-    --input_test $DATASET_DIR/dataset_cpm_test.h5ad \
-    --output $DATASET_DIR/dataset_cpm_knn.h5ad
+    --input_train $DATASET_DIR/train.h5ad \
+    --input_test $DATASET_DIR/test.h5ad \
+    --output $DATASET_DIR/knn.h5ad
 
 # run one metric
 bin/viash run src/label_projection/metrics/accuracy/config.vsh.yaml -- \
-    --input_prediction $DATASET_DIR/dataset_cpm_knn.h5ad \
-    --input_solution $DATASET_DIR/dataset_cpm_solution.h5ad \
-    --output $DATASET_DIR/dataset_cpm_knn_accuracy.h5ad
+    --input_prediction $DATASET_DIR/knn.h5ad \
+    --input_solution $DATASET_DIR/solution.h5ad \
+    --output $DATASET_DIR/knn_accuracy.h5ad
