2c537596a55ec9b2a0aba2c841b8821d8f6df3d2
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-16 13:11:14 +0100

    fix pca operations
    Former-commit-id: f9e2cc0f4df7ff0733a6486bfa205084fed0c75f

--- src/label_projection/methods/mlp/script.py
+++ src/label_projection/methods/mlp/script.py
@@ -7,9 +7,10 @@ import scipy.sparse
 
 ## VIASH START
 par = {
-    'input_train': 'resources_test/label_projection/pancreas/train.h5ad',
-    'input_test': 'resources_test/label_projection/pancreas/test.h5ad',
+    'input_train': 'resources/label_projection/openproblems_v1/pancreas.split_dataset.output_train.h5ad',
+    'input_test': 'resources/label_projection/openproblems_v1/pancreas.split_dataset.output_test.h5ad',
     'output': 'output.h5ad',
+    'layer_input': 'counts'
 }
 meta = {
     'functionality_name': 'foo',
@@ -19,13 +20,14 @@ meta = {
 print("Load input data")
 input_train = ad.read_h5ad(par['input_train'])
 input_test = ad.read_h5ad(par['input_test'])
+input_layer = par["layer_input"]
 
 print("Set up classifier pipeline")
 def pca_op(adata_train, adata_test, n_components=100):
-    is_sparse = scipy.sparse.issparse(adata_train.X)
+    is_sparse = scipy.sparse.issparse(adata_train.layers[input_layer])
 
     min_components = min(
-        [adata_train.shape[0], adata_test.shape[0], adata_train.shape[1]]
+        [adata_train.n_obs, adata_test.n_obs, adata_train.n_vars]
     )
     if is_sparse:
         min_components -= 1
@@ -47,8 +49,6 @@ pipeline = sklearn.pipeline.Pipeline(
         ("regression", classifier),
     ]
 )
-
-input_layer = par["layer_input"]
 
 print("Fit to train data")
 pipeline.fit(input_train.layers[input_layer], input_train.obs["label"].astype(str))
