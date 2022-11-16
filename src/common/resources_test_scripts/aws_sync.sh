90309bd03cd3da9b052af899d92d90a508443238
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-16 14:47:57 +0100

    add initial analysis script
    Former-commit-id: 8923077ef8b8eb3be53fabdada7fcc4d58636743

--- src/common/resources_test_scripts/aws_sync.sh
+++ src/common/resources_test_scripts/aws_sync.sh
@@ -4,3 +4,4 @@ echo "Run the command in this script manually"
 exit 1
 
 aws s3 sync "resources_test" "s3://openproblems-data/resources_test" --exclude */temp_* --delete --dryrun
+aws s3 sync "resources" "s3://openproblems-data/resources" --exclude */temp_* --delete --dryrun
