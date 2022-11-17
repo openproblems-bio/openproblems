10755729fee11184be77f90d0cf27b734893dfcd
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2022-11-17 15:38:16 -0500

    Update pancreas.py

--- openproblems/tasks/denoising/datasets/pancreas.py
+++ openproblems/tasks/denoising/datasets/pancreas.py
@@ -9,7 +9,7 @@ from . import utils
     data_reference=load_pancreas.metadata["data_reference"],
     dataset_summary="Human pancreatic islet scRNA-seq data from 6 datasets "
     "across technologies (CEL-seq, CEL-seq2, Smart-seq2, inDrop, Fluidigm C1, "
-    "and SMARTER-seq). Here we just use the inDrop1 batch, which includes",
+    "and SMARTER-seq). Here we just use the inDrop1 batch, which includes"
     "16382 cells × 18771 genes.",
     image="openproblems-python-extras",
 )
