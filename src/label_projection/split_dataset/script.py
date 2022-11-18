ab771a7f25044b4ef6542fb1ae966f020d9d55ae
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-18 20:48:20 +0100

    refactor split dataset component to be a little more generic
    Former-commit-id: 5dbbdcd679aa17e4ca1d14c9f542a2f08a60407f

--- src/label_projection/split_dataset/script.py
+++ src/label_projection/split_dataset/script.py
@@ -1,10 +1,13 @@
+import re
+import yaml
+import random
 import numpy as np
 import anndata as ad
-import random
+# Todo: throw error when not all slots are available?
 
 ## VIASH START
 par = {
-    'input': 'work/b5/46e5081b30a46ab67d074d4c23eb71/zebrafish.h5ad',
+    'input': 'resources_test/common/pancreas/dataset.h5ad',
     'method': 'batch',
     'seed': None,
     'obs_batch': 'batch',
@@ -14,21 +17,69 @@ par = {
     'output_solution': 'solution.h5ad'
 }
 meta = {
-    'resources_dir': 'src/label_projection/split'
+    'resources_dir': 'src/label_projection/split_dataset',
+    'config': 'src/label_projection/split_dataset/.config.vsh.yaml'
 }
 ## VIASH END
 
+# read the .config.vsh.yaml to find out which output slots need to be copied to which output file
+def read_slots(par, meta):
+    # read output spec from yaml
+    with open(meta["config"], "r") as file:
+        config = yaml.safe_load(file)
+
+    output_struct_slots = {}
+
+    # fetch info on which slots should be copied to which file
+    for arg in config["functionality"]["arguments"]:
+        if re.match("--output_", arg["name"]):
+            file = re.sub("--output_", "", arg["name"])
+            
+            struct_slots = arg['info']['slots']
+            out = {}
+            for (struct, slots) in struct_slots.items():
+                out[struct] = { slot['name'] : slot['name'] for slot in slots }
+            
+            # rename source keys
+            if 'obs' in out:
+                if 'label' in out['obs']:
+                    out['obs']['label'] = par['obs_label']
+                if 'batch' in out['obs']:
+                    out['obs']['batch'] = par['obs_batch']
+
+            output_struct_slots[file] = out
+
+    return output_struct_slots
+
+# create new anndata objects according to api spec
+def subset_anndata(adata_sub, slot_info):
+    structs = ["layers", "obs", "var", "uns", "obsp", "obsm", "varp", "varm"]
+    kwargs = {}
+
+    for struct in structs:
+        slot_mapping = slot_info.get(struct, {})
+        data = {dest : getattr(adata_sub, struct)[src] for (dest, src) in slot_mapping.items()}
+        if len(data) > 0:
+            if struct in ['obs', 'var']:
+                data = pd.concat(data, axis=1)
+            kwargs[struct] = data
+        elif struct in ['obs', 'var']:
+            # if no columns need to be copied, we still need an 'obs' and a 'var' 
+            # to help determine the shape of the adata
+            kwargs[struct] = getattr(adata_sub, struct).iloc[:,[]]
+
+    return ad.AnnData(**kwargs)
+
+# set seed if need be
 if par["seed"]:
     print(f">> Setting seed to {par['seed']}")
     random.seed(par["seed"])
 
 print(">> Load data")
 adata = ad.read_h5ad(par["input"])
-
 print("adata:", adata)
 
 print(f">> Process data using {par['method']} method")
-
 if par["method"] == "batch":
     batch_info = adata.obs[par["obs_batch"]]
     batch_categories = batch_info.dtype.categories
@@ -38,33 +89,27 @@ elif par["method"] == "random":
     train_ix = np.random.choice(adata.n_obs, round(adata.n_obs * 0.8), replace=False)
     is_test = [ not x in train_ix for x in range(0, adata.n_obs) ]
 
-# create new anndata objects according to api spec
-def subset_anndata(adata_sub, layers, obs, uns):
-    return ad.AnnData(
-        layers={key: adata_sub.layers[key] for key in layers},
-        obs=adata_sub.obs[obs.values()].rename({v:n for n,v in obs.items()}, axis=1),
-        var=adata.var.drop(adata.var.columns, axis=1),
-        uns={key: adata_sub.uns[key] for key in uns}
-    )
+# subset the different adatas
+print(">> Figuring which data needs to be copied to which output file")
+slot_info_per_output = read_slots(par, meta)
+
+print(">> Creating train data")
 output_train = subset_anndata(
-    adata_sub = adata[[not x for x in is_test]], 
-    layers=["counts", "normalized"], 
-    obs={"label": par["obs_label"], "batch": par["obs_batch"]}, 
-    uns=["dataset_id"]
+    adata_sub=adata[[not x for x in is_test]], 
+    slot_info=slot_info_per_output['train']
 )
+
+print(">> Creating test data")
 output_test = subset_anndata(
-    adata[is_test], 
-    layers=["counts", "normalized"], 
-    obs={"batch": par["obs_batch"]}, # do NOT copy label to test obs!
-    uns=["dataset_id"]
+    adata_sub=adata[is_test],
+    slot_info=slot_info_per_output['test']
 )
+
+print(">> Creating solution data")
 output_solution = subset_anndata(
-    adata[is_test], 
-    layers=["counts", "normalized"],
-    obs={"label": par["obs_label"], "batch": par["obs_batch"]},
-    uns=["dataset_id"]
+    adata_sub=adata[is_test],
+    slot_info=slot_info_per_output['solution']
 )
-# TODO: use .viash_config.yaml to define these subsets
 
 print(">> Writing data")
 output_train.write_h5ad(par["output_train"])
