58b8f6b6c6e366c4151ff7992a0bd996974518e6
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-22 06:44:27 +0100

    refactor scanvi based on docs
    Former-commit-id: ba57510c8b93facee5bab6a7d4d9903c0527b72d

--- src/label_projection/methods/scanvi/script.py
+++ src/label_projection/methods/scanvi/script.py
@@ -1,6 +1,9 @@
 
 import anndata as ad
-import scvi
+import scarches as sca
+
+# followed procedure from here:
+# https://scarches.readthedocs.io/en/latest/scanvi_surgery_pipeline.html
 
 ## VIASH START
 par = {
@@ -14,46 +17,52 @@ meta = {
 }
 ## VIASH END
 
-print("Load input data")
+print("Load input data", flush=True)
 input_train_orig = ad.read_h5ad(par['input_train'])
 input_test_orig = ad.read_h5ad(par['input_test'])
 
-print("Subsetting to HVG")
-input_train = input_train_orig[:,input_train_orig.var['hvg']]
-input_test = input_test_orig[:,input_test_orig.var['hvg']]
+if par["hvg"]:
+    print("Subsetting to HVG", flush=True)
+    input_train = input_train_orig[:,input_train_orig.var['hvg']]
+    input_test = input_test_orig[:,input_test_orig.var['hvg']]
+else:
+    input_train = input_train_orig
+    input_test = input_test_orig
 
-print("Concatenating train and test data")
+print("Concatenating train and test data", flush=True)
 input_train.obs['is_test'] = False
 input_test.obs['is_test'] = True
 input_test.obs['label'] = "Unknown"
 adata = ad.concat([input_train, input_test], merge = "same")
 
-print("Setting up adata object")
-adata.obs["scanvi_label"] = adata.obs["label"].to_numpy()
-scvi.model.SCVI.setup_anndata(
+print("Create SCANVI model and train it on fully labelled reference dataset", flush=True)
+sca.models.SCVI.setup_anndata(
     adata, 
-    batch_key="batch",
-    labels_key="scanvi_label",
+    batch_key="batch", 
+    labels_key="label",
     layer="normalized"
 )
 
-print("Train SCVI model")
-train_kwargs = dict(
-    train_size=0.9,
-    early_stopping=True,
+vae = sca.models.SCVI(
+    adata,
+    n_layers=2,
+    encode_covariates=True,
+    deeply_inject_covariates=False,
+    use_layer_norm="both",
+    use_batch_norm="none",
 )
-scvi_model = scvi.model.SCVI(adata)
-scvi_model.train(**train_kwargs)
 
-print("Train SCANVI model")
-model = scvi.model.SCANVI.from_scvi_model(scvi_model, unlabeled_category="Unknown")
-model.train(**train_kwargs)
+print("Create the SCANVI model instance with ZINB loss", flush=True)
+scanvae = sca.models.SCANVI.from_scvi_model(vae, unlabeled_category = "Unknown")
 
-print("Make predictions")
-preds = model.predict(adata)
+print("Train SCANVI model", flush=True)
+scanvae.train()
+
+print("Make predictions", flush=True)
+preds = scanvae.predict(adata)
 input_test_orig.obs["label_pred"] = preds[adata.obs['is_test'].values]
 
-print("Write output to file")
+print("Write output to file", flush=True)
 input_test_orig.uns["method_id"] = meta["functionality_name"]
 input_test_orig.write_h5ad(par['output'], compression="gzip")
 
