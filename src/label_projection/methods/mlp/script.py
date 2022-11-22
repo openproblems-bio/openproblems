fa303512e426c13870d32bdf01e1c1a346e58dd3
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-22 07:20:01 +0100

    simplify sklearn-based components
    Former-commit-id: 171658eb6d7c3a85ac716335f7da4535a1315c55

--- src/label_projection/methods/mlp/script.py
+++ src/label_projection/methods/mlp/script.py
@@ -1,9 +1,5 @@
 import anndata as ad
 from sklearn.neural_network import MLPClassifier
-import sklearn.pipeline
-import sklearn.preprocessing
-import sklearn.decomposition
-import scipy.sparse
 
 ## VIASH START
 par = {
@@ -20,41 +16,16 @@ meta = {
 print("Load input data")
 input_train = ad.read_h5ad(par['input_train'])
 input_test = ad.read_h5ad(par['input_test'])
-input_layer = "normalized"
 
-print("Set up classifier pipeline")
-def pca_op(adata_train, adata_test, n_components=100):
-    is_sparse = scipy.sparse.issparse(adata_train.layers[input_layer])
-
-    min_components = min(
-        [adata_train.n_obs, adata_test.n_obs, adata_train.n_vars]
-    )
-    if is_sparse:
-        min_components -= 1
-    n_components = min([n_components, min_components])
-    if is_sparse:
-        pca_op = sklearn.decomposition.TruncatedSVD
-    else:
-        pca_op = sklearn.decomposition.PCA
-    return pca_op(n_components=n_components)
-
+print("Fit to train data")
 classifier = MLPClassifier(
     max_iter=par["max_iter"], 
     hidden_layer_sizes=tuple(par["hidden_layer_sizes"])
 )
-pipeline = sklearn.pipeline.Pipeline(
-    [
-        ("pca", pca_op(input_train, input_test, n_components=100)),
-        ("scaler", sklearn.preprocessing.StandardScaler(with_mean=True)),
-        ("regression", classifier),
-    ]
-)
-
-print("Fit to train data")
-pipeline.fit(input_train.layers[input_layer], input_train.obs["label"].astype(str))
+classifier.fit(input_train.obsm["X_pca"], input_train.obs["label"].astype(str))
 
 print("Predict on test data")
-input_test.obs["label_pred"] = pipeline.predict(input_test.layers[input_layer])
+input_test.obs["label_pred"] = classifier.predict(input_test.obsm["X_pca"])
 
 print("Write output to file")
 input_test.uns["method_id"] = meta["functionality_name"]
