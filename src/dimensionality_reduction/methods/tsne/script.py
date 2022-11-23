df36c5be51f56fe6602e35855d44487b827173e5
jacorvar <jacorvar@gmail.com>
2022-11-23 13:17:15 +0100

    recalculate pca from hvg
    Former-commit-id: 2fb9da453b4a7470248785d9a01c1f13942d1b98

--- src/dimensionality_reduction/methods/tsne/script.py
+++ src/dimensionality_reduction/methods/tsne/script.py
@@ -12,20 +12,28 @@ meta = {
 }
 ## VIASH END
 
+print("Load input data")
 adata = ad.read_h5ad(par['input'])
 
-# t-SNE
-sc.tl.tsne(adata, use_rep="X_pca", n_pcs=par['n_pca'])
+print('Select top 1000 high variable genes')
+n_genes = 1000
+idx = adata.var['hvg_score'].to_numpy().argsort()[::-1][:n_genes]
+
+print('Apply PCA with 50 dimensions')
+adata.obsm['X_pca_hvg'] = sc.tl.pca(adata.layers['normalized'][:, idx], n_comps=par['n_pca'], svd_solver="arpack")
+
+print('Run t-SNE')
+sc.tl.tsne(adata, use_rep="X_pca_hvg", n_pcs=par['n_pca'])
 adata.obsm["X_emb"] = adata.obsm["X_tsne"].copy()
 
 # Update .uns
 adata.uns['method_id'] = 'tsne'
-adata.uns['normalization_id'] = 'pca'
+adata.uns['normalization_id'] = 'log_cpm'
 #del(adata.uns["pca_variance"])
 #del(adata.uns["tsne"])
 # Update .obsm/.varm 
 #del(adata.obsm["X_tsne"])
 #del(adata.varm["pca_loadings"])
 
-# Write output
+print("Write output to file")
 adata.write_h5ad(par['output'], compression="gzip")
