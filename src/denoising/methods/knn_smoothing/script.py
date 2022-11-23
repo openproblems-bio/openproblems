392cd2261c68331c90c9e99bb31b62fe3610c8af
KaiWaldrant <kai.waldrant@outlook.com>
2022-11-23 17:29:04 +0100

    add unit testing to methods and control_methods
    Former-commit-id: 2b44c77acaadc5bcbd1bc4d90ad1667bd6eadbf0

--- src/denoising/methods/knn_smoothing/script.py
+++ src/denoising/methods/knn_smoothing/script.py
@@ -1,10 +1,10 @@
-import knn_smooth #NOTE: not a python package but just a py script on github
+import knn_smooth
 import numpy as np
 import anndata as ad
 
 ## VIASH START
 par = {
-    'input_train': 'resources/label_projection/openproblems_v1/pancreas.split_dataset.output_train.h5ad',
+    'input_train': 'output/denoising/pancreas.split_data.output_train.h5ad',
     'output': 'output_knn.h5ad',
 }
 meta = {
@@ -17,9 +17,8 @@ input_train = ad.read_h5ad(par["input_train"])
 
 print("process data")
 X = input_train.layers["counts"].transpose().toarray()
-output_denoised = input_train.copy()
-output_denoised.layers["counts"] = (knn_smooth.knn_smoothing(X, k=10)).transpose()
+input_train.layers["denoised"] = (knn_smooth.knn_smoothing(X, k=10)).transpose()
 
 print("Writing data")
-output_denoised.uns["method_id"] = par["functionality_name"]
-output_denoised.write_h5ad(par["output"], compression="gzip")
+input_train.uns["method_id"] = meta["functionality_name"]
+input_train.write_h5ad(par["output"], compression="gzip")
