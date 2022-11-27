da437b5e9c993051cfa8d5ff86aad5a2eddc6922
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-11-27 14:07:38 -0500

    Scale and hvg trample uns

--- openproblems/tasks/_batch_integration/batch_integration_graph/methods/_utils.py
+++ openproblems/tasks/_batch_integration/batch_integration_graph/methods/_utils.py
@@ -4,6 +4,8 @@ def hvg_batch(adata, batch_key, target_genes, adataOut):
     if adata.n_vars < 2000:
         return adata
     else:
+        # uns and var get trampled
+        organism = adata.uns["organism"]
         var = adata.var.copy()
         adata = hvg_batch(
             adata,
@@ -13,13 +15,17 @@ def hvg_batch(adata, batch_key, target_genes, adataOut):
             adataOut=adataOut,
         )
         adata.var = var.loc[adata.var.index]
+        adata.uns["organism"] = organism
         return adata
 
 
 def scale_batch(adata, batch_key):
     from scib.preprocessing import scale_batch
 
+    # uns and var get trampled
+    organism = adata.uns["organism"]
     var = adata.var.copy()
     adata = scale_batch(adata, batch_key)
     adata.var = var.loc[adata.var_names]
+    adata.uns["organism"] = organism
     return adata
