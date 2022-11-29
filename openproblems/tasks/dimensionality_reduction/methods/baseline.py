8d4f4e1ff3082b3e526f576156ec7190e7550a51
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-11-29 09:46:31 -0500

    Add spectral baseline to dimensionality reduction (#705)
    * add spectral baseline
    
    * import
    
    Former-commit-id: 8cfae942ab0d317ce9fa1647c0648ed93dfe3e46

--- openproblems/tasks/dimensionality_reduction/methods/baseline.py
+++ openproblems/tasks/dimensionality_reduction/methods/baseline.py
@@ -41,3 +41,31 @@ def high_dim_pca(adata, n_comps: Optional[int] = None, test=False):
     adata.obsm["X_emb"] = adata.obsm["X_pca"]
     adata.uns["method_code_version"] = check_version("openproblems")
     return adata
+
+
+@method(
+    method_name="High-dimensional Laplacian Eigenmaps",
+    paper_name="High-dimensional Laplacian Eigenmaps (baseline)",
+    paper_url="https://openproblems.bio",
+    paper_year=2022,
+    code_url="https://github.com/openproblems-bio/openproblems",
+    is_baseline=True,
+)
+def high_dim_spectral(adata, n_comps: Optional[int] = None, test=False):
+    # We wanted to use all features, but output must be dense
+    # so this is a close approximation
+    import umap
+    import umap.spectral
+
+    if test:
+        n_comps = n_comps or 10
+    else:  # pragma: nocover
+        n_comps = n_comps or 200
+
+    graph = umap.UMAP(transform_mode="graph").fit_transform(adata.X)
+    adata.obsm["X_emb"] = umap.spectral.spectral_layout(
+        adata.X, graph, n_comps, random_state=None
+    )
+
+    adata.uns["method_code_version"] = check_version("openproblems")
+    return adata
