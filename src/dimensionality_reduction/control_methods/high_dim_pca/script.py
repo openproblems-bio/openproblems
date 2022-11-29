dc1d9d3402676decce24e9374469f385f83ecdd1
jacorvar <jacorvar@gmail.com>
2022-11-29 14:22:27 +0100

    set 500 as default n_pca and fix bug
    Former-commit-id: 82de54bd9f59649f2624259ad269e68f363306d2

--- src/dimensionality_reduction/control_methods/high_dim_pca/script.py
+++ src/dimensionality_reduction/control_methods/high_dim_pca/script.py
@@ -5,6 +5,7 @@ import scanpy as sc
 par = {
     'input': 'resources_test/common/pancreas/dataset.h5ad',
     'output': 'output.h5ad',
+    'n_pca': 500,
 }
 meta = {
     'functionality_name': 'foo',
@@ -15,8 +16,7 @@ print("Load input data")
 adata = ad.read_h5ad(par['input'])
 
 print('Create high dimensionally PCA embedding')
-sc.pp.pca(adata.layers['counts'], n_comps=min(min(adata.shape), par['n_pca']))
-adata.obsm["X_emb"] = adata.obsm["X_pca"]
+adata.obsm["X_emb"] = sc.pp.pca(adata.layers['counts'], n_comps=min(min(adata.shape) - 1, par['n_pca']))
 
 # Update .uns
 adata.uns['method_id'] = 'high_dim_pca'
