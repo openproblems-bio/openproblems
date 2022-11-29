abb030a3cea5d2bb7807208afc2425891f01defa
jacorvar <jacorvar@gmail.com>
2022-11-29 21:23:17 +0100

    add rmse unittest
    Former-commit-id: 258a77d47cec6867bce6405a69894980c592458d

--- src/dimensionality_reduction/metrics/rmse/test.py
+++ src/dimensionality_reduction/metrics/rmse/test.py
@@ -2,22 +2,27 @@ import anndata as ad
 import subprocess
 from os import path
 
-input_path = meta["resources_dir"] + "/input/dataset.h5ad"
-output_path = "output.h5ad"
+## VIASH START
+meta = {
+    'executable': './target/docker/dimensionality_reduction/umap',
+    'resources_dir': './resources_test/common/pancreas',
+}
+## VIASH END
+
+input_path = meta["resources_dir"] + "/input/reduced.h5ad"
+output_path = "score.h5ad"
 n_pca = 50
 cmd = [
     meta['executable'],
     "--input", input_path,
     "--output", output_path,
-    "--n_pca", str(n_pca)
 ]
 
 print(">> Checking whether input file exists")
 assert path.exists(input_path)
 
 print(">> Running script as test")
-out = subprocess.run(cmd)
-# out = subprocess.run(cmd, check=True, capture_output=True, text=True)
+out = subprocess.run(cmd, check=True, capture_output=True, text=True)
 
 print(">> Checking whether output file exists")
 assert path.exists(output_path)
@@ -33,10 +38,10 @@ print("output:", output)
 print(">> Checking whether metrics were added")
 assert "metric_ids" in output.uns
 assert "metric_values" in output.uns
-assert meta['functionality_name'] in output.uns["metric_id"]
+assert meta['functionality_name'] in output.uns["metric_ids"]
 
 print(">> Checking whether metrics are float")
-assert isinstance(adata.uns['metric_values'][adata.uns['metric_ids'].index(meta['functionality_name'])], float)
+assert isinstance(output.uns['metric_values'][meta['functionality_name']], float)
 
 print(">> Checking whether data from input was copied properly to output")
 assert input.n_obs == output.n_obs
