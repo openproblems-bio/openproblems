7479e32c3cdd3c5bc4829314747d12c4d52cbe15
jacorvar <jacorvar@gmail.com>
2022-11-29 15:00:55 +0100

    add rmse unit test
    Former-commit-id: 51ab83cfc345fb4d054259b37f5d506b4a737e01

---
+++ src/dimensionality_reduction/metrics/rmse/test.py
@@ -0,0 +1,45 @@
+import anndata as ad
+import subprocess
+from os import path
+
+input_path = meta["resources_dir"] + "/input/dataset.h5ad"
+output_path = "output.h5ad"
+n_pca = 50
+cmd = [
+    meta['executable'],
+    "--input", input_path,
+    "--output", output_path,
+    "--n_pca", str(n_pca)
+]
+
+print(">> Checking whether input file exists")
+assert path.exists(input_path)
+
+print(">> Running script as test")
+out = subprocess.run(cmd)
+# out = subprocess.run(cmd, check=True, capture_output=True, text=True)
+
+print(">> Checking whether output file exists")
+assert path.exists(output_path)
+
+print(">> Reading h5ad files")
+input = ad.read_h5ad(input_path)
+output = ad.read_h5ad(output_path)
+
+print("input:", input)
+
+print("output:", output)
+
+print(">> Checking whether metrics were added")
+assert "metric_ids" in output.uns
+assert "metric_values" in output.uns
+assert meta['functionality_name'] in output.uns["metric_id"]
+
+print(">> Checking whether metrics are float")
+assert isinstance(adata.uns['metric_values'][adata.uns['metric_ids'].index(meta['functionality_name'])], float)
+
+print(">> Checking whether data from input was copied properly to output")
+assert input.n_obs == output.n_obs
+assert input.uns["dataset_id"] == output.uns["dataset_id"]
+
+print("All checks succeeded!")
