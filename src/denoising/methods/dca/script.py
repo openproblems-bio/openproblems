0faad918df2d231eb54b838537d935e30577298a
Robrecht Cannoodt <rcannood@gmail.com>
2022-12-01 12:48:36 +0100

    make sure dca isn't using the .X accidentally
    Former-commit-id: 6d6358b51a2064012a4fc48f215fa33f259c50b5

--- src/denoising/methods/dca/script.py
+++ src/denoising/methods/dca/script.py
@@ -1,6 +1,5 @@
 import anndata as ad
 from dca.api import dca
-from scipy import sparse
 
 ## VIASH START
 par = {
@@ -16,11 +15,16 @@ meta = {
 print("load input data")
 input_train = ad.read_h5ad(par['input_train'])
 
-print("running dca")
-mod = dca(input_train, epochs=par["epochs"], copy=True)
+print("move layer to X")
+input_train.X = input_train.layers["counts"]
+del input_train.layers["counts"]
 
-print("Writing data")
-input_train.layers["denoised"] = mod.layers["counts"]
+print("running dca")
+dca(input_train, epochs=par["epochs"])
+
+print("moving X back to layer")
+input_train.layers["denoised"] = input_train.X
+del input_train.X
 
 print("Writing data")
 input_train.uns["method_id"] = meta["functionality_name"]
