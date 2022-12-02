4d2375c339daeff11216a65f1d317f7e6f31cd0e
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-12-02 14:41:47 +0100

    update test resource generation
    Former-commit-id: 3b49b11aa7b3f224df5b682ed5323fc4f957a984

---
+++ src/batch_integration/resources_test_scripts/pancreas.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#make sure the following command has been executed
+#bin/viash_build -q 'batch_integration|common'
+
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+RAW_DATA=resources_test/common/pancreas/dataset.h5ad
+DATASET_DIR=resources_test/batch_integration
+
+if [ ! -f $RAW_DATA ]; then
+    echo "Error! Could not find raw data"
+    exit 1
+fi
+
+mkdir -p $DATASET_DIR
+
+# subset data
+echo subset data...
+bin/viash run src/batch_integration/datasets/subsample/config.vsh.yaml -- \
+    --input $RAW_DATA \
+    --output $DATASET_DIR/pancreas/subsample.h5ad \
+    --label celltype \
+    --batch tech
+
+# process dataset
+echo process data...
+bin/viash run src/batch_integration/datasets/preprocessing/config.vsh.yaml -- \
+    --input $DATASET_DIR/pancreas/subsample.h5ad \
+    --output $DATASET_DIR/pancreas/processed.h5ad \
+    --label celltype \
+    --batch tech \
+    --hvgs 100
+
+# run methods
+echo run methods...
+bin/viash run src/batch_integration/graph/methods/bbknn/config.vsh.yaml -- \
+    --input $DATASET_DIR/pancreas/processed.h5ad \
+    --output $DATASET_DIR/graph/methods/bbknn.h5ad
+
+bin/viash run src/batch_integration/graph/methods/combat/config.vsh.yaml -- \
+    --input $DATASET_DIR/pancreas/processed.h5ad \
+    --output $DATASET_DIR/graph/methods/combat.h5ad
+# TODO: embedding method
+
+# run one metric
+echo run metrics...
