8b47fc6e7dcbe7ca9c82ccbbc2e73bb1c3ebc14b
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2022-12-02 21:30:26 -0500

    Don't upgrade

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ install_requires = [
     "scprep>=1.2.1",
     "scipy>=1.8,<1.10",
     "scanpy>=1.6",
-    "louvain==0.8.*",
-    "python-igraph==0.10.*",
+    "louvain==0.7.*",
+    "python-igraph<0.10.*",
     "decorator<5.0",  # pinned in #324
     "memory-profiler==0.60",
     "colorama==0.4.*",
