0c46e8112f6036703a7179355b16f26edc0bb6be
KaiWaldrant <kai.waldrant@outlook.com>
2022-12-05 11:38:20 +0100

    update test
    Former-commit-id: eb97a5cd1f091a6eae10c2c8e4e03326d7e89995

--- src/common/list_git_shas/test.py
+++ src/common/list_git_shas/test.py
@@ -1,20 +1,26 @@
 import subprocess
 from os import path
+import json
 
-input_path = meta["resources_dir"] + "src/common"
+input_path = meta["resources_dir"] + "/openproblems-v2"
 output_path = "output.json"
 
 cmd = [
     meta['executable'],
-    "--input_train", input_path,
+    "--input", input_path,
     "--output", output_path
 ]
 
 print(">> Running script as test")
-out = subprocess.run(cmd, capture_output=True, text=True)
+out = subprocess.run(cmd, check=True, capture_output=True, text=True)
 
 
 print(">> Checking whether output file exists")
 assert path.exists(output_path)
+
+print(">> Reading json file")
+with open(output_path) as f:
+    out = json.load(f)
+    print(out[0])
 
 print("All checks succeeded!")
