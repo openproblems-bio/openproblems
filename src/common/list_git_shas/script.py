de7de87cdd94a346c7206624e782ce357f332dd0
Robrecht Cannoodt <rcannood@gmail.com>
2022-12-05 08:08:27 +0100

    small fixes to git sha
    Former-commit-id: a2a58850a90fb3c5326d5e691d396cd68bf2625d

--- src/common/list_git_shas/script.py
+++ src/common/list_git_shas/script.py
@@ -3,60 +3,61 @@ import os
 import json
 
 ## VIASH START
-
 par = {
-    'input': '/home/kai/Documents/openroblems/openproblems-v2/src/denoising',
+    'input': '.',
     'output': 'output/output.json',
     'show_history': True
 }
 meta = {
     'functionality_name': 'dca',
 }
-
 ## VIASH STOP
 
-print(par['show_history'])
+# to do: what to do with untracked files?
 
 output = []
 
-def get_git_file_info(fp, format="none", history=0):
-    
-    if history:
-        cmd = [
-        "git",
-        'log',
-        "--no-merges",
-        f"--pretty=format:%H",
-        "--",
-        fp
-    ]
-    else:    
-        cmd = [
-            "git",
-            'log',
-            "-n",
-            "1",
-            f"--pretty=format:{format}",
-            "--",
-            fp
-        ]
-    out = subprocess.run(cmd, capture_output=True, text=True).stdout
+def git_ls_files(directory):
+    cmd = ["git", "ls-files"]
+    cmd_out = subprocess.run(cmd, capture_output=True, text=True, cwd=directory).stdout
+    out = [ line for line in cmd_out.split("\n") if line != "" ]
     return out
+
+def get_git_file_info(file, full_history=False):
+    # construct command
+    cmd = ["git", "log", "--no-merges", "--pretty=%H\t%ci"]
+
+    if not full_history:
+        cmd.extend(["-n", "1"])
+
+    cmd.extend(["--", file])
     
+    # run command
+    out = subprocess.run(cmd, capture_output=True, text=True).stdout
 
-for root, dirs, files in os.walk(par['input']):
-        for file in files:
-            git_file= {}
-            fp = os.path.join(root,file)
-            abs_fp = fp.replace(par['input']+"/","")
-            git_file['path'] = abs_fp
-            git_file['last_modified'] = get_git_file_info(fp, "%ci")
-            git_file['sha'] = get_git_file_info(fp, "%H")
-            if "show_history" in par:
-                if par['show_history']:
-                    git_file['history_sha'] = get_git_file_info(fp,history=1).split("\n")
+    # split output
+    split = [line.split("\t") for line in out.split("\n") if "\t" in line]
 
-            output.append(git_file)
+    return split
+
+
+
+for relative_path in git_ls_files(par['input']):
+    # construct path
+    path = os.path.join(par["input"], relative_path)
+
+    # get git file info
+    git_file_info = get_git_file_info(path, full_history=par["show_history"])
+    last = git_file_info[len(git_file_info)-1]
+    out = {
+        "path": relative_path,
+        "last_modified": last[1],
+        "sha": last[0]
+    }
+    if par['show_history']:
+        out['history_sha'] = [info[0] for info in git_file_info]
+
+    output.append(out)
 
 with open(par['output'], 'w') as f:
     json.dump(output, f, indent=4)
