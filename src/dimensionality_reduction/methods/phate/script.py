fa4039b77a66cbaf8fea715513850ffc063fc84e
jacorvar <jacorvar@gmail.com>
2022-12-07 10:59:46 +0100

    fix phate arguments/preferred_normalization key
    Former-commit-id: 3cb053b6e9d82313f61c3cbc1b5f6b47e2a42bcc

--- src/dimensionality_reduction/methods/phate/script.py
+++ src/dimensionality_reduction/methods/phate/script.py
@@ -25,19 +25,18 @@ gamma = 0 if par['g0'] else 1
 print('... with gamma=' + str(gamma) + ' and...')
 phate_op = PHATE(n_pca=par['n_pca'], verbose=False, n_jobs=-1, gamma=gamma)
 
-if par['log_cpm']:
+with open(meta['config'], 'r') as config_file:
+    config = yaml.safe_load(config_file)
+input.uns['normalization_id'] = config['functionality']['info']['preferred_normalization']
+
+if input.uns['normalization_id'] == 'sqrt_cpm':
+    print('... using sqrt-CPM data')
+    input.obsm["X_emb"] = phate_op.fit_transform(input.layers['normalized'])
+elif input.uns['normalization_id'] == 'log_cpm':
     print('... using logCPM data')
     n_genes = 1000
     idx = input.var['hvg_score'].to_numpy().argsort()[::-1][:n_genes]
     input.obsm["X_emb"] = phate_op.fit_transform(input.layers['normalized'][:, idx])
-    input.uns['normalization_id'] = 'log_cpm'
-else:
-    print('... using sqrt-CPM data')
-    input.layers['sqrt_cpm'] = sc.transform.sqrt(input.layers['normalized'].expm1())
-    input.obsm["X_emb"] = phate_op.fit_transform(input.layers['sqrt_cpm'])
-    with open(meta['config'], 'r') as config_file:
-        config = yaml.safe_load(config_file)
-    input.uns['normalization_id'] = config['functionality']['info']['preferred_normalization']
 
 print("Delete layers and var")
 del input.layers
