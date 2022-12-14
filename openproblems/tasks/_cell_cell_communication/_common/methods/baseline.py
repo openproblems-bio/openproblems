6785b2462165b984f7e1f9ccc350912cbbf163b7
Daniel Dimitrov <50865230+dbdimitrov@users.noreply.github.com>
2022-12-14 10:03:18 +0100

    Ccc aggregate + ligand-target subtask fix (#752)
    * fix issue with random ligand-target, add aggregations
    
    * pre-commit
    
    * replace tag with commit
    
    * retry with release
    
    * add basilisk.utils to requirements
    
    * add dir.expiry to r-extras
    
    * pre-commit
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
    Former-commit-id: a49ae996dc5f34402b1fa5cab9b3a3936126a413

--- openproblems/tasks/_cell_cell_communication/_common/methods/baseline.py
+++ openproblems/tasks/_cell_cell_communication/_common/methods/baseline.py
@@ -14,22 +14,24 @@ import pandas as pd
     is_baseline=True,
 )
 def random_events(adata, test=False, n_events=1000):
-    adata.uns["ccc_pred"] = pd.DataFrame(
+    rng = np.random.default_rng(seed=1)
+
+    ccc_pred = pd.DataFrame(
         {
-            "ligand": np.random.choice(
+            "ligand": rng.choice(
                 adata.uns["ligand_receptor_resource"]["ligand_genesymbol"], n_events
             ),
             "receptor": np.random.choice(
                 adata.uns["ligand_receptor_resource"]["receptor_genesymbol"], n_events
             ),
-            "source": np.random.choice(adata.obs["label"].cat.categories, n_events),
-            "target": np.random.choice(adata.obs["label"].cat.categories, n_events),
-            "score": np.random.uniform(0, 1, n_events),
+            "source": rng.choice(adata.obs["label"].cat.categories, n_events),
+            "target": rng.choice(adata.obs["label"].cat.categories, n_events),
+            "score": rng.uniform(0, 1, n_events),
         }
     )
-    adata.uns["ccc_pred"] = adata.uns["ccc_pred"].loc[
-        ~adata.uns["ccc_pred"][adata.uns["merge_keys"]].duplicated()
-    ]
+    ccc_pred = ccc_pred.loc[~ccc_pred[adata.uns["merge_keys"]].duplicated()]
+
+    adata.uns["ccc_pred"] = ccc_pred
     adata.uns["method_code_version"] = check_version("openproblems")
     return adata
 
