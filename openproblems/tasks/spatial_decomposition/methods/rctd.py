54f6e3f024df68f1a4ab02bbebc4f0673677240b
Scott Gigante <scott.gigante@immunai.com>
2022-12-17 09:00:22 +1100

    fix rctd filtering

--- openproblems/tasks/spatial_decomposition/methods/rctd.py
+++ openproblems/tasks/spatial_decomposition/methods/rctd.py
@@ -30,21 +30,29 @@ def rctd(
     else:  # pragma: nocover
         fc_cutoff = fc_cutoff or 0.5
         fc_cutoff_reg = fc_cutoff_reg or 0.75
-    # exctract single cell reference data
+    # extract single cell reference data
     adata_sc, adata = split_sc_and_sp(adata)
+    labels = np.unique(adata_sc.obs["labels"])
 
     # set spatial coordinates for the single cell data
     adata_sc.obsm["spatial"] = np.ones((adata_sc.shape[0], 2))
+    # remove rare cell types to prevent RCTD error
+    celltype_counts = adata_sc.obs["label"].value_counts()
+    adata_sc = adata_sc[
+        ~adata_sc.obs["label"].isin(celltype_counts[celltype_counts < 25].index)
+    ].copy()
     # run RCTD
     adata = _rctd(
         adata_sc, adata, fc_cutoff, fc_cutoff_reg, max_cores=multiprocessing.cpu_count()
     )
 
     # get predicted cell type proportions from obs
-    cell_type_names = [x for x in adata.obs.columns if x.startswith("xCT")]
+    cell_type_names = [f"xCT_{label}" for label in labels]
 
     # add proportions
-    adata.obsm["proportions_pred"] = adata.obs[cell_type_names].to_numpy()
+    adata.obsm["proportions_pred"] = (
+        adata.obs.reindex(cell_type_names, axis=1).fillna(0).to_numpy()
+    )
 
     adata.uns["method_code_version"] = check_r_version("spacexr")
 
