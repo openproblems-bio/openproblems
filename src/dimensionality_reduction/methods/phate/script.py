11fb44ee7bd094542d06b45f441e580e5b560c43
Robrecht Cannoodt <rcannood@gmail.com>
2022-12-22 14:07:37 +0100

    add variants to phate
    Former-commit-id: 11475877bae6e421be8ec24962437969e88f4ac7

--- src/dimensionality_reduction/methods/phate/script.py
+++ src/dimensionality_reduction/methods/phate/script.py
@@ -1,14 +1,13 @@
 import anndata as ad
 from phate import PHATE
-import scprep as sc
-import yaml
 
 ## VIASH START
 par = {
-    'input': 'resources_test/common/pancreas/train.h5ad',
+    'input': 'resources_test/dimensionality_reduction/pancreas/train.h5ad',
     'output': 'reduced.h5ad',
     'n_pca': 50,
-    'g0': False,
+    'gamma': 1,
+    'num_hvg_genes': None
 }
 meta = {
     'functionality_name': 'phate',
@@ -16,33 +15,28 @@ meta = {
 }
 ## VIASH END
 
-print("Load input data")
+print("Load input data", flush=True)
 input = ad.read_h5ad(par['input'])
 
-print("Run PHATE...")
-gamma = 0 if par['g0'] else 1
-print('... with gamma=' + str(gamma) + ' and...')
-phate_op = PHATE(n_pca=par['n_pca'], verbose=False, n_jobs=-1, gamma=gamma)
+print("Run PHATE", flush=True)
+phate_op = PHATE(n_pca=par['n_pca'], verbose=False, n_jobs=-1, gamma=par['gamma'])
+X_mat = input.layers['normalized']
 
-with open(meta['config'], 'r') as config_file:
-    config = yaml.safe_load(config_file)
-input.uns['normalization_id'] = config['functionality']['info']['preferred_normalization']
+if par["num_hvg_genes"]:
+    print("Subsetting to hvg genes", flush=True)
+    num_features = par["num_hvg_genes"]
+    hvg_idx = input.var['hvg_score'].to_numpy().argsort()[::-1][:num_features]
+    X_mat = X_mat[:, hvg_idx]
 
-if input.uns['normalization_id'] == 'sqrt_cpm':
-    print('... using sqrt-CPM data')
-    input.obsm["X_emb"] = phate_op.fit_transform(input.layers['normalized'])
-elif input.uns['normalization_id'] == 'log_cpm':
-    print('... using logCPM data')
-    n_genes = 1000
-    idx = input.var['hvg_score'].to_numpy().argsort()[::-1][:n_genes]
-    input.obsm["X_emb"] = phate_op.fit_transform(input.layers['normalized'][:, idx])
+# store embedding
+input.obsm["X_emb"] = phate_op.fit_transform(X_mat)
 
-print("Delete layers and var")
+print("Delete layers and var", flush=True)
 del input.layers
 del input.var
 
-print('Add method')
+print('Add method', flush=True)
 input.uns['method_id'] = meta['functionality_name']
 
-print("Write output to file")
+print("Write output to file", flush=True)
 input.write_h5ad(par['output'], compression="gzip")
