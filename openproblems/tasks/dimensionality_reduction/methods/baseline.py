255b23e086c38d921b979c683fb9d9d439de5969
Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
2023-01-04 17:32:40 +1100

    fix normalization in baselines (#760)

--- openproblems/tasks/dimensionality_reduction/methods/baseline.py
+++ openproblems/tasks/dimensionality_reduction/methods/baseline.py
@@ -1,4 +1,6 @@
 from ....tools.decorators import method
+from ....tools.normalize import log_cpm
+from ....tools.normalize import log_cpm_hvg
 from ....tools.utils import check_version
 
 import numpy as np
@@ -27,6 +29,45 @@ def random_features(adata, test=False):
     is_baseline=True,
 )
 def true_features(adata, test=False):
+    adata.obsm["X_emb"] = adata.X
+    if test:
+        adata.obsm["X_emb"] = adata.obsm["X_emb"][:, :100]
+
+    adata.obsm["X_emb"] = adata.obsm["X_emb"].toarray()
+    adata.uns["method_code_version"] = check_version("openproblems")
+    return adata
+
+
+@method(
+    method_name="True Features (logCPM)",
+    paper_name="True Features (baseline)",
+    paper_url="https://openproblems.bio",
+    paper_year=2022,
+    code_url="https://github.com/openproblems-bio/openproblems",
+    is_baseline=True,
+)
+def true_features_log_cpm(adata, test=False):
+    adata = log_cpm(adata)
+    adata.obsm["X_emb"] = adata.X
+    if test:
+        adata.obsm["X_emb"] = adata.obsm["X_emb"][:, :100]
+
+    adata.obsm["X_emb"] = adata.obsm["X_emb"].toarray()
+    adata.uns["method_code_version"] = check_version("openproblems")
+    return adata
+
+
+@method(
+    method_name="True Features (logCPM, 1kHVG)",
+    paper_name="True Features (baseline)",
+    paper_url="https://openproblems.bio",
+    paper_year=2022,
+    code_url="https://github.com/openproblems-bio/openproblems",
+    is_baseline=True,
+)
+def true_features_log_cpm_hvg(adata, test=False):
+    adata = log_cpm_hvg(adata)
+    adata = adata[:, adata.var["highly_variable"]].copy()
     adata.obsm["X_emb"] = adata.X
     if test:
         adata.obsm["X_emb"] = adata.obsm["X_emb"][:, :100]
