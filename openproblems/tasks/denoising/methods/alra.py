0380cb3945c2506c7a85cfef349864be98da578d
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2023-01-09 21:42:12 -0500

    Update alra.py

--- openproblems/tasks/denoising/methods/alra.py
+++ openproblems/tasks/denoising/methods/alra.py
@@ -69,8 +69,6 @@ def alra_log(adata, test=False):
     import scprep
 
     # libsize and log norm
-    # add pseudocount
-    adata.obsm["train_norm"] = adata.obsm["train"] + 1
     # lib norm
     adata.obsm["train_norm"], libsize = scprep.normalize.library_size_normalize(
         adata.obsm["train_norm"], rescale=1, return_library_size=True
@@ -98,7 +96,6 @@ def alra_log(adata, test=False):
     # transform back into original space
     Y = scprep.utils.matrix_transform(Y, np.exp)
     Y = scprep.utils.matrix_vector_elementwise_multiply(Y, libsize, axis=0)
-    Y = Y - 1
     adata.obsm["denoised"] = Y
 
     adata.uns["method_code_version"] = "1.0.0"
