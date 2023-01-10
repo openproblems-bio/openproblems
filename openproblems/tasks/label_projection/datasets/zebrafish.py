0d865ae745e98d313a444b3f2479f7a62eb3b24c
Nikolay Markov <nikolai.markov@icloud.com>
2023-01-09 18:44:45 -0600

    Add label pr dataset dimensions & cell type counts

--- openproblems/tasks/label_projection/datasets/zebrafish.py
+++ openproblems/tasks/label_projection/datasets/zebrafish.py
@@ -10,7 +10,8 @@ import numpy as np
     data_reference=load_zebrafish.metadata["data_reference"],
     dataset_summary="90k cells from zebrafish embryos throughout the first day of "
     "development, with and without a knockout of chordin, an important developmental "
-    "gene. Split into train/test by laboratory.",
+    "gene. Split into train/test by laboratory. "
+    "Dimensions: 26022 cells, 25258 genes. 24 cell types (avg. 1084±1156 cells per cell type).",
 )
 def zebrafish_labs(test=False):
     adata = load_zebrafish(test=test)
@@ -26,7 +27,8 @@ def zebrafish_labs(test=False):
     data_reference=load_zebrafish.metadata["data_reference"],
     dataset_summary="90k cells from zebrafish embryos throughout the first day of "
     "development, with and without a knockout of chordin, an important developmental "
-    "gene. Split into train/test randomly.",
+    "gene. Split into train/test randomly. "
+    "Dimensions: 26022 cells, 25258 genes. 24 cell types (avg. 1084±1156 cells per cell type).",
 )
 def zebrafish_random(test=False):
     adata = load_zebrafish(test=test)
