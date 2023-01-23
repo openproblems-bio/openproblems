4fa435f748d0a7eee5393370aca2d6bf74414f17
Nikolay Markov <nikolai.markov@icloud.com>
2023-01-23 11:52:54 -0600

    Label projection describe datasets (#776)
    * Rename zebrafish_labels to zebrafish_labs
    
    * Add label pr dataset dimensions & cell type counts
    
    * pre-commit
    
    * pre-commit
    
    * Fix flake 88 line length
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
    Former-commit-id: e5f0bc663de6d5130a21aca0e5a46fa27fb2c966

--- openproblems/tasks/label_projection/datasets/__init__.py
+++ openproblems/tasks/label_projection/datasets/__init__.py
@@ -4,5 +4,5 @@ from .pancreas import pancreas_batch
 from .pancreas import pancreas_random
 from .pancreas import pancreas_random_label_noise
 from .tabula_muris_senis import tabula_muris_senis_lung_random
-from .zebrafish import zebrafish_labels
+from .zebrafish import zebrafish_labs
 from .zebrafish import zebrafish_random
