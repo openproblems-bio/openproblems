b444fa6c9fd2293c0fb56d77511af4e248da0754
Daniel Strobl <50872326+danielStrobl@users.noreply.github.com>
2023-01-27 17:10:42 +0100

    add metrics from graph subtask to embedding task

--- openproblems/tasks/_batch_integration/batch_integration_embed/metrics/__init__.py
+++ openproblems/tasks/_batch_integration/batch_integration_embed/metrics/__init__.py
@@ -4,3 +4,8 @@ from .kBET import kBET
 from .pcr import pcr
 from .sil_batch import silhouette_batch
 from .silhouette import silhouette
+
+from ...batch_integration_graph.metrics.ari import ari
+from ...batch_integration_graph.metrics.graph_connectivity import graph_connectivity
+from ...batch_integration_graph.metrics.iso_label_f1 import isolated_labels_f1
+from ...batch_integration_graph.metrics.nmi import nmi
