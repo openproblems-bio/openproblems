091bfb86363c4c0f3f108fd6b5dcbbddabfcdd83
Daniel Strobl <50872326+danielStrobl@users.noreply.github.com>
2023-02-01 22:14:21 +0100

    add new dataloaders (#792)
    * add new dataloaders
    
    * task dataloaders
    
    * address comments
    
    * correct docstring
    
    * change dataset name
    
    * remove whitespace
    
    * add to init
    
    * pre-commit
    
    * filter celltypes
    
    * pre-commit
    
    * filter celltypes
    
    * pre-commit
    
    * update urllib requirement
    
    * pre-commit
    
    * urllib
    
    * Use scprep 1.2.2
    
    * remove immune human mouse for now
    
    * Remove urllib dep
    
    * Remove broken import
    
    ---------
    
    Co-authored-by: Scott Gigante <84813314+scottgigante-immunai@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Former-commit-id: c366d94fd90827516b8eaa15a5ad9b0cdec7531a

---
+++ openproblems/tasks/_batch_integration/_common/datasets/lung.py
@@ -0,0 +1,40 @@
+from .....data.lung import load_lung
+from .....tools.decorators import dataset
+from ..utils import filter_celltypes
+from typing import Optional
+
+
+@dataset(
+    dataset_name="Lung (Viera Braga et al.)",
+    data_url=load_lung.metadata["data_url"],
+    data_reference=load_lung.metadata["data_reference"],
+    dataset_summary="Human lung scRNA-seq data from 3 datasets with 32,472 cells."
+    "From Vieira Braga et al. Technologies: 10X and Drop-seq.",
+    image="openproblems",
+)
+def lung_batch(test: bool = False, min_celltype_count: Optional[int] = None):
+    import scanpy as sc
+
+    adata = load_lung(test)
+    adata.uns["organism"] = "human"
+    adata.obs["labels"] = adata.obs["cell_type"]
+    # No need to rename batch column as it already exists
+
+    adata = filter_celltypes(adata, min_celltype_count=min_celltype_count)
+
+    sc.pp.filter_genes(adata, min_counts=1)
+    sc.pp.filter_genes(adata, min_cells=1)
+
+    adata.X = adata.layers["log_normalized"]
+
+    sc.tl.pca(
+        adata,
+        svd_solver="arpack",
+        return_info=True,
+    )
+    adata.obsm["X_uni_pca"] = adata.obsm["X_pca"]
+
+    sc.pp.neighbors(adata, use_rep="X_uni_pca", key_added="uni")
+
+    adata.var_names_make_unique()
+    return adata
