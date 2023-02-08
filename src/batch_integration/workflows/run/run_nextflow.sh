0d05e44db58a5316fdfdb3363bf19f1d00172930
Robrecht Cannoodt <rcannood@gmail.com>
2023-02-08 10:34:51 +0100

    Move pipeline, add includes to config
    Co-authored-by: Kai Waldrant <KaiWaldrant@users.noreply.github.com>
    Co-authored-by: Michaela Mueller <mumichae@users.noreply.github.com>
    
    Former-commit-id: 633b8818151a7bc89bca6de590c172560fc27660

---
+++ src/batch_integration/workflows/run/run_nextflow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Run this prior to executing this script:
+# bin/viash_build -q 'batch_integration'
+
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+set -xe
+
+DATASET_DIR=resources_test/batch_integration/pancreas
+
+# run benchmark
+export NXF_VER=22.04.5
+
+  # -profile docker \
+nextflow run . \
+  -main-script src/batch_integration/workflows/run/main.nf \
+  -profile docker \
+  -c src/wf_utils/labels_ci.config \
+  -resume \
+  --id pancreas \
+  --input $DATASET_DIR/processed.h5ad \
+  --output scores.tsv \
+  --publish_dir $DATASET_DIR/
