81bb3c2f19112d158f235f541e9d13ad4d3587d3
Wesley Lewis <59123674+wes-lewis@users.noreply.github.com>
2023-02-19 14:31:50 -0500

    add reverse order/regular order
    Try to match the magic code format for decorators and implementation of reverse norm

--- openproblems/tasks/denoising/methods/alra.py
+++ openproblems/tasks/denoising/methods/alra.py
@@ -8,8 +8,9 @@ _alra = r_function("alra.R")
 log = logging.getLogger("openproblems")
 
 
-@method(
-    method_name="ALRA (sqrt norm, reversed normalization)",
+method_name="ALRA (sqrt norm, reversed normalization)",
+_alra_method = functools.partial(
+    method,
     paper_name="Zero-preserving imputation of scRNA-seq data using "
     "low-rank approximation",
     paper_reference="linderman2018zero",
@@ -17,19 +18,36 @@ log = logging.getLogger("openproblems")
     code_url="https://github.com/KlugerLab/ALRA",
     image="openproblems-r-extras",
 )
-def alra_sqrt(adata, test=False):
+
+
+def _alra(adata, normtype = "log", reverse_norm_order=False, test=False):
     import numpy as np
     import rpy2.rinterface_lib.embedded
     import scprep
+    
+    if normtype == "sqrt":
+        norm_fn = np.sqrt
+        denorm_fn = np.square
+    elif normtype == "log":
+        norm_fn = np.log1p
+        denorm_fn = np.expm1
+    else:
+        raise NotImplementedError
+    
+    X = adata.obsm["train"]
+    if reverse_norm_order:
+        # inexplicably, this sometimes performs better
+        adata.obsm["train"] = scprep.utils.matrix_transform(adata.obsm["train"], norm_fn)
+        adata.obsm["train"], libsize = scprep.normalize.library_size_normalize(
+            adata.obsm["train"], rescale=1, return_library_size=True
+        )
+    else:
+        adata.obsm["train"], libsize = scprep.normalize.library_size_normalize(
+            adata.obsm["train"], rescale=1, return_library_size=True
+        )
+        adata.obsm["train"] = scprep.utils.matrix_transform(X, norm_fn)
 
-    # libsize and sqrt norm
-    adata.obsm["train_norm"] = scprep.utils.matrix_transform(
-        adata.obsm["train"], np.sqrt
-    )
-    adata.obsm["train_norm"], libsize = scprep.normalize.library_size_normalize(
-        adata.obsm["train_norm"], rescale=1, return_library_size=True
-    )
-    adata.obsm["train_norm"] = adata.obsm["train_norm"].tocsr()
+    adata.obsm["train"] = adata.obsm["train"].tocsr()
     # run alra
     # _alra takes sparse array, returns dense array
     Y = None
@@ -53,50 +71,26 @@ def alra_sqrt(adata, test=False):
     adata.uns["method_code_version"] = "1.0.0"
     return adata
 
+@_alra_method(
+    method_name="ALRA (sqrt norm, reversed normalization)",
+)
+def alra_sqrt_reversenorm(adata, test=False):
+    return _alra(adata, normtype = "log", reverse_norm_order=True, test=False)
 
-@method(
+@_alra_method(
+    method_name="ALRA (log norm, reversed normalization)",
+)
+def alra_log_reversenorm(adata, test=False):
+    return _alra(adata, normtype = "log", reverse_norm_order=True, test=False)
+
+@_alra_method(
+    method_name="ALRA (sqrt norm)",
+)
+def alra_sqrt(adata, test=False):
+    return _alra(adata, normtype = "log", reverse_norm_order=False, test=False)
+
+@_alra_method(
     method_name="ALRA (log norm)",
-    paper_name="Zero-preserving imputation of scRNA-seq data using "
-    "low-rank approximation",
-    paper_reference="linderman2018zero",
-    paper_year=2018,
-    code_url="https://github.com/KlugerLab/ALRA",
-    image="openproblems-r-extras",
 )
 def alra_log(adata, test=False):
-    import numpy as np
-    import rpy2.rinterface_lib.embedded
-    import scprep
-
-    # libsize and log norm
-    # lib norm
-    adata.obsm["train_norm"], libsize = scprep.normalize.library_size_normalize(
-        adata.obsm["train"], rescale=1, return_library_size=True
-    )
-    # log
-    adata.obsm["train_norm"] = scprep.utils.matrix_transform(
-        adata.obsm["train_norm"], np.log1p
-    )
-    # to csr
-    adata.obsm["train_norm"] = adata.obsm["train_norm"].tocsr()
-    # run alra
-    # _alra takes sparse array, returns dense array
-    Y = None
-    attempts = 0
-    while Y is None:
-        try:
-            Y = _alra(adata)
-        except rpy2.rinterface_lib.embedded.RRuntimeError:  # pragma: no cover
-            if attempts < 10:
-                attempts += 1
-                log.warning(f"alra.R failed (attempt {attempts})")
-            else:
-                raise
-
-    # transform back into original space
-    Y = scprep.utils.matrix_transform(Y, np.expm1)
-    Y = scprep.utils.matrix_vector_elementwise_multiply(Y, libsize, axis=0)
-    adata.obsm["denoised"] = Y
-
-    adata.uns["method_code_version"] = "1.0.0"
-    return adata
+    return _alra(adata, normtype = "log", reverse_norm_order=False, test=False)
