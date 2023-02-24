0ddb5a455b70aa5c064944d105ea74dc4d77ed27
Scott Gigante <scott.gigante@immunai.com>
2023-02-24 11:47:54 -0500

    just subsample it

--- openproblems/tasks/dimensionality_reduction/datasets/zebrafish.py
+++ openproblems/tasks/dimensionality_reduction/datasets/zebrafish.py
@@ -14,7 +14,12 @@ from .._utils import ranking_matrix
     "(avg. 1084±1156 cells per cell type).",
 )
 def zebrafish_labs(test=False):
+    import scanpy as sc
+
     adata = load_zebrafish(test=test)
+    if not test:
+        # this dataset is too big
+        sc.pp.subsample(adata, n_obs=25000)
     adata.uns["n_genes"] = adata.shape[1]
     adata = log_cp10k(adata)
     adata.obsm["X_ranking"] = ranking_matrix(adata.X)
