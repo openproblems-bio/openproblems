c55df0688490243863387cc311291a826512a323
Robrecht Cannoodt <rcannood@gmail.com>
2023-03-17 15:24:09 +0100

    remove deprecated component
    Former-commit-id: 4ea622803c4a387227bcb9b1e78137a4d0ad2ad7

--- src/common/dataset_loader/download/README.md
+++
@@ -1,9 +0,0 @@
-# Universal Data Loader
-
-Download data from URL and ensure that matrices are stored as expected.
-
-## API
-Downloaded datasets contain:
-
-* `adata.X`: raw counts
-* `adata.layers['lognorm']`: log-normalized counts, only if available
--- src/common/dataset_loader/download/run_example.sh
+++
@@ -1,6 +0,0 @@
-bin/viash run src/common/dataset_loader/download/config.vsh.yaml -- \
-  --output src/common/dataset_loader/download/resources/pancreas.h5ad \
-  --url https://ndownloader.figshare.com/files/24539828 \
-  --name pancreas \
-  --obs_cell_type celltype \
-  --obs_batch tech
