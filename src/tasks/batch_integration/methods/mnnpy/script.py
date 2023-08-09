9164b8a8d1ee850eb24b218922235d0c23d511d4
Daniel Strobl <50872326+danielStrobl@users.noreply.github.com>
2023-08-09 14:15:45 +0200

    add mnn (#210)
    * add mnn
    
    * Update config.vsh.yaml
    
    add documentation
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    see PR https://github.com/chriscainx/mnnpy#48
    
    * process comments
    
    * Update src/tasks/batch_integration/methods/mnn/script.py
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>
    
    * rename method
    
    * update descriptions
    
    ---------
    
    Co-authored-by: Kai Waldrant <kai@data-intuitive.com>
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>
    Former-commit-id: febb220b9bf3df79dd8daaee35f8026a602fd8f0

---
+++ src/tasks/batch_integration/methods/mnnpy/script.py
@@ -0,0 +1,31 @@
+import anndata as ad
+from scib.integration import mnn
+
+## VIASH START
+par = {
+    'input': 'resources_test/batch_integration/pancreas/unintegrated.h5ad',
+    'output': 'output.h5ad',
+    'hvg': True,
+}
+meta = {
+    'functionality_name': 'foo',
+    'config': 'bar'
+}
+## VIASH END
+
+print('Read input', flush=True)
+adata = ad.read_h5ad(par['input'])
+
+if par['hvg']:
+    print('Select HVGs', flush=True)
+    adata = adata[:, adata.var['hvg']]
+
+print('Run mnn', flush=True)
+adata.X = adata.layers['normalized']
+adata.layers['corrected_counts'] = mnn(adata, batch='batch').X
+
+del adata.X
+
+print("Store outputs", flush=True)
+adata.uns['method_id'] = meta['functionality_name']
+adata.write_h5ad(par['output'], compression='gzip')
