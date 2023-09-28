e54c64fe80d6217d503d299ec84a8ee527a08262
Michaela Müller <51025211+mumichae@users.noreply.github.com>
2023-09-28 21:17:21 +0200

    Add more methods (#223)
    * add scalex feature and embed
    
    * change to base_python image
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>
    Former-commit-id: 96598b8e4fe591f01071ebe5c18dd50553c5edf3

---
+++ src/tasks/batch_integration/methods/scalex_feature/script.py
@@ -0,0 +1,41 @@
+import anndata as ad
+import scanpy as sc
+import scalex
+
+## VIASH START
+par = {
+    'input': 'resources_test/batch_integration/pancreas/unintegrated.h5ad',
+    'output': 'output.h5ad',
+    'hvg': True,
+}
+meta = {
+    'functionality_name' : 'foo',
+    'config': 'bar'
+}
+## VIASH END
+
+
+
+print('Read input', flush=True)
+adata = ad.read_h5ad(par['input'])
+
+print('Run SCALEX', flush=True)
+adata.X = adata.layers['normalized']
+adata = scalex.SCALEX(
+    adata,
+    batch_key="batch",
+    ignore_umap=True,
+    impute=adata.obs["batch"].cat.categories[0],
+    processed=True,
+    max_iteration=40,
+    min_features=None,
+    min_cells=None,
+    n_top_features=0,
+    outdir=None,
+    gpu=0,
+)
+adata.layers['corrected_counts'] = adata.layers["impute"]
+
+print("Store outputs", flush=True)
+adata.uns['method_id'] = meta['functionality_name']
+adata.write_h5ad(par['output'], compression='gzip')
