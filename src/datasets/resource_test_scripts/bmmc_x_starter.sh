6d3c15f90cb73593c8a32818e11b63d1675ceed5
Kai Waldrant <kai@data-intuitive.com>
2023-10-12 14:42:30 +0200

    uncomment
    Former-commit-id: 02df606f650e4798ccb41fc2cd6b2d0a963bd2f6

--- src/datasets/resource_test_scripts/bmmc_x_starter.sh
+++ src/datasets/resource_test_scripts/bmmc_x_starter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# NEURIPS2021_URL="https://github.com/openproblems-bio/neurips2021_multimodal_viash/raw/main/resources_test/common"
-# DATASET_DIR="resources_test/common"
+NEURIPS2021_URL="https://github.com/openproblems-bio/neurips2021_multimodal_viash/raw/main/resources_test/common"
+DATASET_DIR="resources_test/common"
 
-# SUBDIR="$DATASET_DIR/bmmc_cite_starter"
-# mkdir -p "$SUBDIR"
-# wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_rna.h5ad" \
-#   -O "$SUBDIR/dataset_rna.h5ad"
-# wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_mod2.h5ad" \
-#   -O "$SUBDIR/dataset_adt.h5ad"
+SUBDIR="$DATASET_DIR/bmmc_cite_starter"
+mkdir -p "$SUBDIR"
+wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_rna.h5ad" \
+  -O "$SUBDIR/dataset_rna.h5ad"
+wget "$NEURIPS2021_URL/openproblems_bmmc_cite_starter/openproblems_bmmc_cite_starter.output_mod2.h5ad" \
+  -O "$SUBDIR/dataset_adt.h5ad"
 
 cat > "$SUBDIR/state.yaml" << HERE
 id: bmmc_cite_starter
@@ -16,12 +16,12 @@ output_dataset_rna: !file dataset_rna.h5ad
 output_dataset_other_mod: !file dataset_adt.h5ad
 HERE
 
-# SUBDIR="$DATASET_DIR/bmmc_multiome_starter"
-# mkdir -p "$SUBDIR"
-# wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_rna.h5ad" \
-#   -O "$SUBDIR/dataset_rna.h5ad"
-# wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_mod2.h5ad" \
-#   -O "$SUBDIR/dataset_atac.h5ad"
+SUBDIR="$DATASET_DIR/bmmc_multiome_starter"
+mkdir -p "$SUBDIR"
+wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_rna.h5ad" \
+  -O "$SUBDIR/dataset_rna.h5ad"
+wget "$NEURIPS2021_URL/openproblems_bmmc_multiome_starter/openproblems_bmmc_multiome_starter.output_mod2.h5ad" \
+  -O "$SUBDIR/dataset_atac.h5ad"
 
 cat > "$SUBDIR/state.yaml" << HERE
 id: bmmc_multiome_starter
@@ -29,5 +29,5 @@ output_dataset_rna: !file dataset_rna.h5ad
 output_dataset_other_mod: !file dataset_atac.h5ad
 HERE
 
-# # run task process dataset components
-# src/tasks/predict_modality/resources_test_scripts/bmmc_x_starter.sh
+# run task process dataset components
+src/tasks/predict_modality/resources_test_scripts/bmmc_x_starter.sh
