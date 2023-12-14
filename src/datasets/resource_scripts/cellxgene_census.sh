c04a453647f3eac5656369acae51322dc2a31859
Robrecht Cannoodt <rcannood@gmail.com>
2023-12-14 10:05:36 +0100

    Add more cellxgene datasets (#308)
    * add cellxgene datasets
    
    Co-authored-by: Michaela Müller <51025211+mumichae@users.noreply.github.com>
    
    * fix yaml formatting
    
    ---------
    
    Co-authored-by: Michaela Müller <51025211+mumichae@users.noreply.github.com>
    Former-commit-id: 6cdba41787f894aa4d1ff6f4b8d00d866fefb62e

--- src/datasets/resource_scripts/cellxgene_census.sh
+++ src/datasets/resource_scripts/cellxgene_census.sh
@@ -2,16 +2,16 @@
 
 cat > "/tmp/params.yaml" << 'HERE'
 param_list:
-  - id: cxg_mm_pancreas_atlas
+  - id: cxg_mouse_pancreas_atlas
     species: mus_musculus
     census_version: "2023-07-25"
     obs_value_filter: "dataset_id == '49e4ffcc-5444-406d-bdee-577127404ba8'"
     obs_batch: donor_id
-    dataset_name: Mouse pancreatic islet atlas
-    dataset_url: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE211799
-    dataset_reference: hrovatin2023delineating
+    dataset_name: Mouse Pancreatic Islet Atlas
     dataset_summary: Mouse pancreatic islet scRNA-seq atlas across sexes, ages, and stress conditions including diabetes
     dataset_description: To better understand pancreatic β-cell heterogeneity we generated a mouse pancreatic islet atlas capturing a wide range of biological conditions. The atlas contains scRNA-seq datasets of over 300,000 mouse pancreatic islet cells, of which more than 100,000 are β-cells, from nine datasets with 56 samples, including two previously unpublished datasets. The samples vary in sex, age (ranging from embryonic to aged), chemical stress, and disease status (including T1D NOD model development and two T2D models, mSTZ and db/db) together with different diabetes treatments. Additional information about data fields is available in anndata uns field 'field_descriptions' and on https://github.com/theislab/mm_pancreas_atlas_rep/blob/main/resources/cellxgene.md.
+    dataset_url: https://cellxgene.cziscience.com/collections/296237e2-393d-4e31-b590-b03f74ac5070
+    dataset_reference: hrovatin2023delineating
     dataset_organism: mus_musculus
 
 normalization_methods: [log_cp10k, sqrt_cp10k, l1_sqrt]
