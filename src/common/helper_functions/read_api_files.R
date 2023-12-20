58b0de725ec7d5749aceb233f278976e10bd2711
Robrecht Cannoodt <rcannood@gmail.com>
2023-12-20 20:29:27 +0100

    update bibtex and helper functions (#323)
    Former-commit-id: 6f6acccfdb7068a6c198b41e0d67d47552488b3b

--- src/common/helper_functions/read_api_files.R
+++ src/common/helper_functions/read_api_files.R
@@ -344,3 +344,77 @@ render_task_graph <- function(task_api, root = .task_graph_get_root(task_api)) {
     §```
     §"), symbol = "§")
 }
+
+
+
+# Recursive function to process each property with indentation
+.render_example_process_property <- function(prop, prop_name = NULL, indent_level = 0) {
+  if (is.null(prop_name)) {
+    prop_name <- ""
+  }
+
+  out <- c()
+
+  # define helper variables
+  indent_spaces <- strrep(" ", indent_level)
+  next_indent_spaces <- strrep(" ", indent_level + 2)
+
+  # add comment if available
+  if ("description" %in% names(prop)) {
+    comment <- gsub("\n", paste0("\n", indent_spaces, "# "), stringr::str_trim(prop$description))
+    out <- c(out, indent_spaces, "# ", comment, "\n")
+  }
+
+  # add variable
+  out <- c(out, indent_spaces, prop_name, ": ")
+
+  if (prop$type == "object" && "properties" %in% names(prop)) {
+    # Handle object with properties
+    prop_names <- setdiff(names(prop$properties), "additionalProperties")
+    sub_props <- unlist(lapply(prop_names, function(sub_prop_name) {
+      prop_out <- .render_example_process_property(
+        prop$properties[[sub_prop_name]],
+        sub_prop_name,
+        indent_level + 2
+      )
+      c(prop_out, "\n")
+    }))
+    c(out, "\n", sub_props[-length(sub_props)])
+  } else if (prop$type == "array") {
+    if (is.list(prop$items) && "properties" %in% names(prop$items)) {
+      # Handle array of objects
+      array_items_yaml <- unlist(lapply(names(prop$items$properties), function(item_prop_name) {
+        prop_out <- .render_example_process_property(
+          prop$items$properties[[item_prop_name]],
+          item_prop_name,
+          indent_level + 4
+        )
+        c(prop_out, "\n")
+      }))
+      c(out, "\n", next_indent_spaces, "- ", array_items_yaml[-1])
+    } else {
+      # Handle simple array
+      c(out, "[ ... ]")
+    }
+  } else {
+    c(out, "...")
+  }
+}
+
+# Function for rendering an example yaml based on a JSON schema
+render_example <- function(json_schema) {
+  if (!"properties" %in% names(json_schema)) {
+    return("")
+  }
+  text <-
+    unlist(lapply(names(json_schema$properties), function(prop_name) {
+      out <- .render_example_process_property(
+        json_schema$properties[[prop_name]],
+        prop_name,
+        0
+      )
+      c(out, "\n")
+    }))
+
+  paste(text, collapse = "")
+}
