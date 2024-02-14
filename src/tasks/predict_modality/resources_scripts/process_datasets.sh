48b71bf0cd7ad32b886c57a8401494e35d05b039
Kai Waldrant <kai@data-intuitive.com>
2024-02-14 15:23:51 +0100

    [pred_mod] Update process datasets nf-tower script (#373)
    * add thumbnail for PM task
    
    * update readme
    
    * catch cases where description is not defined
    
    * refactor process_datasets nf-tower script
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>
    Former-commit-id: fe7b02e7b8b35ed2d9a6dd20cbc983e5b0828a37

--- src/tasks/predict_modality/resources_scripts/process_datasets.sh
+++ src/tasks/predict_modality/resources_scripts/process_datasets.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 cat > /tmp/params.yaml << 'HERE'
-param_list:
-  - id: predict_modality_process_datasets
-    settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad"}'
-  - id: predict_modality_process_datasets_swap
-    settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad", "swap": true}'
-
+id: predict_modality_process_datasets
 input_states: s3://openproblems-data/resources/datasets/**/state.yaml
+settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad"}'
 rename_keys: 'input_mod1:output_mod1,input_mod2:output_mod2'
 output_state: "$id/state.yaml"
 publish_dir: s3://openproblems-data/resources/predict_modality/datasets
@@ -33,3 +29,23 @@ tw launch https://github.com/openproblems-bio/openproblems-v2.git \
   --entry-name auto \
   --config /tmp/nextflow.config \
   --labels predict_modality,process_datasets
+
+cat > /tmp/params.yaml << 'HERE'
+id: predict_modality_process_datasets_swap
+input_states: s3://openproblems-data/resources/datasets/**/state.yaml
+settings: '{"output_train_mod1": "$id/train_mod1.h5ad", "output_train_mod2": "$id/train_mod2.h5ad", "output_test_mod1": "$id/test_mod1.h5ad", "output_test_mod2": "$id/test_mod2.h5ad", "swap": true}'
+rename_keys: 'input_mod1:output_mod1,input_mod2:output_mod2'
+output_state: "$id/state.yaml"
+publish_dir: s3://openproblems-data/resources/predict_modality/datasets
+HERE
+
+tw launch https://github.com/openproblems-bio/openproblems-v2.git \
+  --revision main_build \
+  --pull-latest \
+  --main-script target/nextflow/predict_modality/workflows/process_datasets/main.nf \
+  --workspace 53907369739130 \
+  --compute-env 1pK56PjjzeraOOC2LDZvN2 \
+  --params-file /tmp/params.yaml \
+  --entry-name auto \
+  --config /tmp/nextflow.config \
+  --labels predict_modality,process_datasets,swap_mod
