76958e609de56df4c54137954e4708666bb5657a
Kai Waldrant <kai@data-intuitive.com>
2024-10-18 16:23:49 +0200

    update updata_task_table script

--- src/backend/update_tasks_table/script.py
+++ src/backend/update_tasks_table/script.py
@@ -20,19 +20,19 @@ par = {
 
 
 # authenticate with the Github API
-print(">> Authenticating with Github")
+print(">> Authenticating with Github", flush=True)
 auth = Auth.Token(par["github_auth"])
 g = Github(auth=auth)
 
 
 # authenticate with the Supabase API
-print(">> Authenticating with Supabase")
+print(">> Authenticating with Supabase", flush=True)
 url: str = par["supabase_url"]
 key: str = par["supabase_key"]
 supabase: Client = create_client(url, key)
 
 # Get tasks already in db
-print(">> Getting tasks from db")
+print(">> Getting tasks from db" flush=True)
 tasks = supabase.table("tasks").select("task_name").execute()
 
 task_names=[]
@@ -40,11 +40,11 @@ for task in tasks.data:
     task_names.append(task["task_name"])
 
 # get repos from Openproblems-bio organization
-print(">> Getting repos from Openproblems-bio organization")
-repos = g.get_organization("openproblems-bio").get_repos()
+print(">> Getting repos from Openproblems-bio organization", flush=True)
+repos = g.get_organization(par["github_org"]).get_repos()
 
 # Create rows for tasks that are not in db
-print(">> Creating rows for tasks that are not in db")
+print(">> Creating rows for tasks that are not in db", flush=True)
 rows= []
 for repo in repos:
   if "task_" in repo.name:
@@ -56,11 +56,11 @@ for repo in repos:
 
 # Add rows to db tasks table
 if len(rows) > 0:
-  print(">> Adding rows to db tasks table")
+  print(">> Adding rows to db tasks table", flush=True)
   response = (
     supabase.table("tasks")
     .insert(rows)
     .execute()
   )
 else:
-  print(">> No new tasks to add to db")
+  print(">> No new tasks to add to db", flush=True)
