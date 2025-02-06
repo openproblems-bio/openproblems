fdbdff86df7d41368829269bbe9975c6f37b7aa9
Luke Zappia <luke@data-intuitive.com>
2025-02-06 11:21:10 +0100

    Update get_dataset_info to new schema

--- src/reporting/get_dataset_info/script.R
+++ src/reporting/get_dataset_info/script.R
@@ -1,53 +1,61 @@
-requireNamespace("jsonlite", quietly = TRUE)
-requireNamespace("yaml", quietly = TRUE)
-library(purrr, warn.conflicts = FALSE)
-library(rlang, warn.conflicts = FALSE)
-
 ## VIASH START
 par <- list(
-  input = "resources_test/openproblems/task_results_v3/raw/dataset_uns.yaml",
-  output = "resources_test/openproblems/task_results_v3/processed/dataset_info.json"
+  input = "resources_test/openproblems/task_results_v4/raw/dataset_uns.yaml",
+  output = "resources_test/openproblems/task_results_v4/processed/dataset_info.json"
 )
 ## VIASH END
 
-datasets <- yaml::yaml.load_file(par$input)
+cat("====== Get dataset info ======\n")
 
-# transform into format expected by website
-outputs <- map(datasets, function(dataset) {
-  # ↑ the 'dataset' object could be used as the new format
+`%||%` <- rlang::`%||%`
 
-  # TODO: it'd be nice if the s3 path was also included in the dataset info
+cat("\n>>> Reading input files...\n")
+cat("Reading dataset uns from '", par$input, "'...\n", sep = "")
+dataset_uns <- yaml::yaml.load_file(
+  par$input,
+  # Read file sizes as floats to avoid issues with big integers
+  handlers = list(int = \(x) {as.numeric(x)})
+)
 
-  # construct v1 format
-  out <- list(
-    "dataset_id" = dataset$dataset_id,
-    "dataset_name" = dataset$dataset_name,
-    "dataset_summary" = dataset$dataset_summary,
-    "dataset_description" = dataset$dataset_description %||% NA_character_,
-    "data_reference" = dataset$dataset_reference %||% NA_character_,
-    "data_url" = dataset$dataset_url %||% NA_character_,
-    "date_created" = dataset$date_created %||% NA_character_,
-    "file_size" = dataset$file_size %||% NA_character_
+cat(
+  "\n>>> Processing ", length(dataset_uns), " datasets...\n",
+  sep = ""
+)
+dataset_info_json <- purrr::map(dataset_uns, function(.dataset) {
+  cat("Processing dataset uns '", .dataset$dataset_id, "'\n", sep = "")
+
+  references <- if (is.list(.dataset$dataset_reference)) {
+    list(
+      doi = .dataset$dataset_reference$doi %||% character(0),
+      bibtex = .dataset$dataset_reference$bibtex %||% character(0)
+    )
+  } else {
+    .dataset$dataset_reference
+  }
+
+  list(
+    name = jsonlite::unbox(.dataset$dataset_id),
+    label = jsonlite::unbox(.dataset$dataset_name),
+    commit = jsonlite::unbox(.dataset$dataset_commit %||% "missing-sha"),
+    summary = jsonlite::unbox(.dataset$dataset_summary),
+    description = jsonlite::unbox(.dataset$dataset_description),
+    source_url = jsonlite::unbox(.dataset$dataset_url),
+    common_dataset_names = .dataset$common_dataset_id,
+    modalities = jsonlite::unbox(.dataset$dataset_modality),
+    organisms = .dataset$dataset_organism,
+    references = references,
+    date_created = jsonlite::unbox(.dataset$date_created),
+    file_size_mb = jsonlite::unbox(.dataset$file_size / 1048576)
   )
-
-  if (!is.null(dataset[["common_dataset_id"]])) {
-    out[["common_dataset_id"]] <- dataset[["common_dataset_id"]]
-  }
-
-  # show warning when certain data is missing and return null?
-  for (n in names(out)) {
-    if (is.null(out[[n]])) {
-      out_as_str <- jsonlite::toJSON(out, auto_unbox = TRUE, pretty = TRUE)
-      stop("missing value for value '", n, "' in ", out_as_str)
-    }
-  }
-
-  out
 })
 
+cat("\n>>> Writing output files...\n")
+cat("Writing dataset info to '", par$output, "'...\n", sep = "")
 jsonlite::write_json(
-  outputs,
+  dataset_info_json,
   par$output,
-  auto_unbox = TRUE,
-  pretty = TRUE
+  pretty = TRUE,
+  null = "null"
 )
+
+cat("\n>>> Done!\n")
