aa841e1b4c7dbadf169b36b4c2ad4c47301d6784
Olga Novitskaia <novitskaia.olga.s@gmail.com>
2025-05-23 10:36:48 +0000

    an example script for running the data processing pipeline is added

--- src/datasets/resource_scripts/op3_loader.sh
+++ src/datasets/resource_scripts/op3_loader.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
-
+#It is an example how to set parameters and execute processing for the op3 dataset.
 set -e
 
-params_file="/tmp/datasets_openproblems_neurips2022_params.yaml"
-
-## TODO: fill in the parameters below
-echo "TODO: fill in the parameters below" && exit 1
+params_file="/tmp/datasets_op3.yaml"
 
 cat > "$params_file" << 'HERE'
 param_list:
-  - id: openproblems_neurips2022/pbmc_cite
-    input: https://ftp.ncbi.nlm.nih.gov/geo/series/GSE279nnn/GSE279945/suppl/GSE279945_sc_counts_processed.h5ad
-    dataset_name: OpenProblems NeurIPS2022 CITE-Seq
-    dataset_organism: homo_sapiens
-    dataset_summary: Single-cell CITE-Seq (GEX+ADT) data collected from bone marrow mononuclear cells of 12 healthy human donors.
-    dataset_description: "Single-cell CITE-Seq data collected from bone marrow mononuclear cells of 12 healthy human donors using the 10X 3 prime Single-Cell Gene Expression kit with Feature Barcoding in combination with the BioLegend TotalSeq B Universal Human Panel v1.0. The dataset was generated to support Multimodal Single-Cell Data Integration Challenge at NeurIPS 2022. Samples were prepared using a standard protocol at four sites. The resulting data was then annotated to identify cell types and remove doublets. The dataset was designed with a nested batch layout such that some donor samples were measured at multiple sites with some donors measured at a single site."
-
-dataset_url: "https://www.kaggle.com/competitions/open-problems-multimodal/data"
-dataset_reference: lance2024predicting
-normalization_methods: [log_cp10k, sqrt_cp10k, l1_sqrt]
-output_mod1: '$id/dataset_mod1.h5ad'
-output_mod2: '$id/dataset_mod2.h5ad'
-output_meta_mod1: '$id/dataset_metadata_mod1.yaml'
-output_meta_mod2: '$id/dataset_metadata_mod2.yaml'
+  - id: op3
+    input: ../data/sample.h5ad
+    dataset_name: "OP3: single-cell multimodal dataset in PBMCs for perturbation prediction benchmarking"
+    dataset_summary: "The Open Problems Perurbation Prediction (OP3) dataset with small molecule perturbations in PBMCs"
+    dataset_description: "The OP3 dataset is to-date the largest single-cell small molecule perturbation dataset in primary tissue with multiple donor replicates."
+    dataset_url: "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE279nnn/GSE279945/suppl/GSE279945_sc_counts_processed.h5ad"
+    dataset_reference: GSE279945
+normalization_methods: 
+  - log_cp10k
+output_dataset: '$id/dataset.h5ad'
+do_subsample: True
+output_meta: '$id/dataset_metadata.yaml'
 output_state: '$id/state.yaml'
-publish_dir: s3://openproblems-data/resources/datasets/multimodal
+output_raw: force_null
+output_normalized: force_null
+output_pca: force_null
+output_hvg: force_null
+output_knn: force_null
+output_state: '$id/state.yaml'
+publish_dir: s3://openproblems-data/resources/datasets/datasets_op3
 HERE
 
 cat > /tmp/nextflow.config << HERE
 process {
   withName:'.*publishStatesProc' {
-    memory = '16GB'
-    disk = '100GB'
+    memory = '100GB'
+    disk = '1000GB'
   }
 }
 HERE
 
-tw launch https://github.com/openproblems-bio/openproblems.git \
-  --revision main_build \
-  --pull-latest \
-  --main-script target/nextflow/datasets/workflows/scrnaseq/op3_loader/main.nf \
-  --workspace 53907369739130 \
-  --params-file "$params_file" \
-  --config /tmp/nextflow.config \
-  --labels op3_loader,dataset_loader \
+#tw launch https://github.com/openproblems-bio/openproblems.git \
+#  --revision main_build \
+#  --pull-latest \
+#  --main-script target/nextflow/datasets/workflows/scrnaseq/op3_loader/main.nf \
+#  --workspace 53907369739130 \
+#  --params-file "$params_file" \
+#  --labels op3_loader,dataset_loader \
+#  --config /tmp/nextflow.config
+#
+set -x
+nextflow run . \
+  -main-script target/nextflow/datasets/workflows/scrnaseq/process_op3/main.nf \
+  -profile docker \
+  -resume \
+  -params-file "$params_file" \
+  -config common/nextflow_helpers/labels_ci.config
