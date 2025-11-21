0fc92415b72a0a7bc247a2fcc5a0bb424460db38
Luke Zappia <lazappi@users.noreply.github.com>
2025-11-21 13:54:02 +0100

    Handle metric direction in `process_task_results` (#936)
    * Handle minimized metrics in generate_qc
    
    * Handle minimized metrics in render_report
    
    * Fix logo export for report
    
    * Adjust how metric direction is handled

--- src/reporting/generate_qc/script.R
+++ src/reporting/generate_qc/script.R
@@ -243,7 +243,8 @@ check_metric_scaling <- function(
   results_long,
   metric,
   control_methods,
-  task_name
+  task_name,
+  maximize
 ) {
   `%||%` <- rlang::`%||%`
 
@@ -272,6 +273,8 @@ check_metric_scaling <- function(
     dplyr::left_join(control_range, by = "dataset_name") |>
     dplyr::mutate(
       scaled_value = (metric_value - control_min) / (control_max - control_min),
+      # Reverse metric values if lower is better
+      scaled_value = ifelse(maximize, scaled_value, 1 - scaled_value),
       outside = scaled_value < 0 | scaled_value > 1,
       pct_outside = dplyr::case_when(
         scaled_value < 0 ~ 0 - scaled_value,
@@ -381,7 +384,8 @@ check_method_metric_scaling <- function(
   scaled_metrics,
   method,
   task_name,
-  metric_name
+  metric_name,
+  maximize
 ) {
   method_scaled_metrics <- scaled_metrics |>
     dplyr::filter(method_name == method)
@@ -773,8 +777,10 @@ controls_metrics <- purrr::map(seq_len(nrow(metric_controls)), function(.idx) {
 })
 
 cat("\n>>> Checking metric scaling...\n")
+metric_maximize <- purrr::map_lgl(metric_info, "maximize") |>
+  purrr::set_names(metric_names)
 scaling <- purrr::map(metric_names, function(.metric) {
-  check_metric_scaling(results_long, .metric, control_methods, task_name)
+  check_metric_scaling(results_long, .metric, control_methods, task_name, maximize = metric_maximize[[.metric]])
 }) |>
   purrr::list_flatten()
 
